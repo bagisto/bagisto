b6fda43b38e845cd588c813352d71f8a6292768c
rahul shukla <rahulshukla.symfony517@webkul.com>
2018-10-11 15:23:29 +0530

    conflict

--- packages/Webkul/Shop/src/Resources/views/customers/account/partials/sidemenu.blade.php
+++ packages/Webkul/Shop/src/Resources/views/customers/account/partials/sidemenu.blade.php
@@ -1,7 +1,13 @@
 <ul class="account-side-menu">
+    <div class="side-menu-title" id="side-menu-title">
+        Menu
+    </div>
+
     @foreach($menu->items as $key=>$value)
 
         <li><a href="{{ $value['url'] }}">{{ $value['name'] }}</a></li>
 
     @endforeach
 </ul>
+
+
