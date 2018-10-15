8784fffe9280244b661bad160d2fd2c8abde99ee
prashant-webkul <prashant.singh852@webkul.com>
2018-10-15 17:19:28 +0530

    before merge

--- packages/Webkul/Shop/src/Resources/views/products/list/card.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/list/card.blade.php
@@ -34,14 +34,16 @@
             @if($product->type == "configurable")
                 <a href="{{ route('cart.add.configurable', $product->url_key) }}" class="btn btn-lg btn-primary addtocart">{{ __('shop::app.home.product-card.add-to-cart') }}</a>
             @else
-                <form action="{{route('cart.add', $product->id)}}" method="POST">
-                    @csrf
-                    <input type="hidden" name="product" value="{{ $product->id }}">
-                    <input type="hidden" name="quantity" value="1">
-                    <input type="hidden" value="false" name="is_configurable">
-                    <button class="btn btn-lg btn-primary addtocart">{{ __('shop::app.home.product-card.add-to-cart') }}</button>
-                </form>
-                @include('shop::products.wishlist')
+                <div class="cart-wish-wrap">
+                    <form action="{{route('cart.add', $product->id)}}" method="POST">
+                        @csrf
+                        <input type="hidden" name="product" value="{{ $product->id }}">
+                        <input type="hidden" name="quantity" value="1">
+                        <input type="hidden" value="false" name="is_configurable">
+                        <button class="btn btn-lg btn-primary addtocart">{{ __('shop::app.home.product-card.add-to-cart') }}</button>
+                    </form>
+                    @include('shop::products.wishlist')
+                </div>
             @endif
         @endif
     </div>
