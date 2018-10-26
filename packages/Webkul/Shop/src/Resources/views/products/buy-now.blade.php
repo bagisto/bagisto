e5e2bf338ee3684d3b0d4d778b3974621f1053ae
prashant-webkul <prashant.singh852@webkul.com>
2018-10-26 16:47:54 +0530

    Disabled buy now until next merge

--- packages/Webkul/Shop/src/Resources/views/products/buy-now.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/buy-now.blade.php
@@ -1,3 +1,3 @@
-<a href="{{ route('shop.product.buynow', $product->id)}}" class="btn btn-lg btn-primary buynow" style="text-align: center;">
+{{-- <a href="{{ route('shop.product.buynow', $product->id)}}" class="btn btn-lg btn-primary buynow" style="text-align: center;">
     {{ __('shop::app.products.buy-now') }}
-</a>
+</a> --}}
