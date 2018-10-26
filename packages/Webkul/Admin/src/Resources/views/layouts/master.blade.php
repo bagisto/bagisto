8e2d45e498d62bbe799cb75a94ecaa192743c1e8
prashant-webkul <prashant.singh852@webkul.com>
2018-10-26 19:59:24 +0530

    TinyMCE integration and Slider Content Fixes

--- packages/Webkul/Admin/src/Resources/views/layouts/master.blade.php
+++ packages/Webkul/Admin/src/Resources/views/layouts/master.blade.php
@@ -56,6 +56,24 @@
         <script type="text/javascript" src="{{ asset('vendor/webkul/admin/assets/js/admin.js') }}"></script>
         <script type="text/javascript" src="{{ asset('vendor/webkul/ui/assets/js/ui.js') }}"></script>
 
+        <script type="text/javascript" src="{{ asset('js/tinyMCE/tinymce.min.js') }}">
+            tinymce.init({
+            selector: 'textarea',
+            height: 500,
+            menubar: false,
+            plugins: [
+                'advlist autolink lists link image charmap print preview anchor textcolor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table contextmenu paste code help wordcount'
+            ],
+            toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+            content_css: [
+                '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+                '//www.tinymce.com/css/codepen.min.css']
+            });
+        </script>
+
+
         @stack('scripts')
 
         <div class="modal-overlay"></div>
