d2836b50abf170e52a6298445ce37e0963dca94c
Prashant Singh <prashant.singh852@webkul.com>
2019-03-12 13:39:32 +0530

    One way to hide the side drawer

--- packages/Webkul/Admin/src/Resources/views/layouts/nav-aside.blade.php
+++ packages/Webkul/Admin/src/Resources/views/layouts/nav-aside.blade.php
@@ -29,25 +29,31 @@
         @endif
     </ul>
 
-    {{-- <div class="close-nav-aside">
+    <div class="close-nav-aside">
         <i class="icon angle-left-icon close-icon"></i>
-    </div> --}}
+    </div>
+
+    <div class="open-nav-aside">
+        <i class="icon angle-left-icon close-icon"></i>
+    </div>
 </div>
 
-{{-- @push('scripts')
+@push('scripts')
     <script>
         $(document).ready(function() {
             $(".close-nav-aside").on('click', function(e) {
                 $('.content-wrapper').css("margin-left", "0px");
                 $('.aside-nav').css('display', 'none');
                 $('.open-nav-aside').css('display', 'block');
+                $('.close-nav-aside').css('display', 'none');
             });
 
             $(".open-nav-aside").on('click', function(e) {
                 $('.content-wrapper').css("margin-left", "305px");
                 $('.aside-nav').css('display', '');
                 $('.open-nav-aside').css('display', 'none');
+                $('.close-nav-aside').css('display', 'block');
             });
         });
     </script>
-@endpush --}}
+@endpush
