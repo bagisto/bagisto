c9f51db884e377cb331e0396d39099e9980eca58
Prashant Singh <prashant.singh852@webkul.com>
2019-03-31 03:18:12 +0530

    corrected discount cust grp table class name

--- database/migrations/2019_03_28_120843_create_discount_customer_groups_table.php
+++ database/migrations/2019_03_28_120843_create_discount_customer_groups_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDiscountCustomerGroupTable extends Migration
+class CreateDiscountCustomerGroupsTable extends Migration
 {
     /**
      * Run the migrations.
