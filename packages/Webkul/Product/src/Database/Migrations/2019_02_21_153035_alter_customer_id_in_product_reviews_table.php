8e7c3c7f22c7e979ac8974564c0b31cca54eee1f
rahul shukla <rahulshukla.symfony517@webkul.com>
2019-04-09 12:12:35 +0530

    restore migration

--- packages/Webkul/Product/src/Database/Migrations/2019_02_21_153035_alter_customer_id_in_product_reviews_table.php
+++ packages/Webkul/Product/src/Database/Migrations/2019_02_21_153035_alter_customer_id_in_product_reviews_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterProductReviewsTable extends Migration
+class AlterCustomerIdInProductReviewsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,7 @@ class AlterProductReviewsTable extends Migration
     public function up()
     {
         Schema::table('product_reviews', function (Blueprint $table) {
-            $table->string('name')->default('');
-            $table->dropForeign('product_reviews_customer_id_foreign');
+            $table->integer('customer_id')->nullable()->change();
         });
     }
 
@@ -26,6 +25,8 @@ class AlterProductReviewsTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('product_reviews', function (Blueprint $table) {
+            //
+        });
     }
 }
