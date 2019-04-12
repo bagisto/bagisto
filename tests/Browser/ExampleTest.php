3fe81527586e34eb33f6f3bc3122653b3412f2f2
Prashant Singh <prashant.singh852@webkul.com>
2019-04-12 18:20:33 +0530

    added php tooltip.js dependency and laravel dusk for browser tests profiling

---
+++ tests/Browser/ExampleTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ExampleTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/customer/login')
+                    ->assertSee('login');
+        });
+    }
+}
