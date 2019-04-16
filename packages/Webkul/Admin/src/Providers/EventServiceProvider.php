53ebbf5a1e5e34dce2938540be6bff7dcf1b55bf
Prashant Singh <prashant.singh852@webkul.com>
2019-04-16 14:47:28 +0530

    Removed product grid events, product grid table is now obselete and maybe used in future for someother purposes. In result we will have a faster product creation and a new grid will be added with product flat table

--- packages/Webkul/Admin/src/Providers/EventServiceProvider.php
+++ packages/Webkul/Admin/src/Providers/EventServiceProvider.php
@@ -23,9 +23,5 @@ class EventServiceProvider extends ServiceProvider
         Event::listen('checkout.order.save.after', 'Webkul\Admin\Listeners\Order@updateProductInventory');
 
         Event::listen('products.datagrid.sync', 'Webkul\Admin\Listeners\Product@sync');
-
-        Event::listen('catalog.product.create.after', 'Webkul\Admin\Listeners\Product@afterProductCreated');
-
-        Event::listen('catalog.product.update.after', 'Webkul\Admin\Listeners\Product@afterProductUpdate');
     }
 }
