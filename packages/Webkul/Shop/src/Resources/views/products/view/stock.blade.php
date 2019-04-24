8b0787ad5332483ba1e3fe1278ed8b5efcced383
rahul shukla <rahulshukla.symfony517@webkul.com>
2019-04-24 14:45:57 +0530

    issue #835

--- packages/Webkul/Shop/src/Resources/views/products/view/stock.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/view/stock.blade.php
@@ -1,7 +1,13 @@
 {!! view_render_event('bagisto.shop.products.view.stock.before', ['product' => $product]) !!}
 
-<div class="stock-status {{ $product->type != 'configurable' && ! $product->haveSufficientQuantity(1) ? '' : 'active' }}">
-    {{ $product->type != 'configurable' && ! $product->haveSufficientQuantity(1) ? __('shop::app.products.out-of-stock') : __('shop::app.products.in-stock') }}
-</div>
+@if ($product->type == 'simple')
+    <div class="stock-status {{! $product->haveSufficientQuantity(1) ? '' : 'active' }}">
+        {{ $product->haveSufficientQuantity(1) ? __('shop::app.products.in-stock') : __('shop::app.products.out-of-stock') }}
+    </div>
+@else
+    <div class="stock-status in-stock" id="in-stock" style="display: none;">
+        {{ __('shop::app.products.in-stock') }}
+    </div>
+@endif
 
 {!! view_render_event('bagisto.shop.products.view.stock.after', ['product' => $product]) !!}
