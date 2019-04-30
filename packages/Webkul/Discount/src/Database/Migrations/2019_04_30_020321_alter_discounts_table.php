ece4f315e4b2886832a14d4dc2739381377a3a4c
Prashant Singh <prashant.singh852@webkul.com>
2019-05-01 03:23:54 +0530

    migrations for discount almost to be finished, final revision left

---
+++ packages/Webkul/Discount/src/Database/Migrations/2019_04_30_020321_alter_discounts_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterDiscountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('discounts', function (Blueprint $table) {
+            $table->string('name');
+            $table->string('type');
+            $table->text('description')->nullable();
+            $table->boolean('status')->default(0);
+            $table->dateTime('starts_from');
+            $table->dateTime('ends_till');
+            $table->integer( 'priority')->unsigned();
+            $table->boolean('ends_subsequent')->default(false);
+        });
+
+        Schema::table('discount_channels', function (Blueprint $table) {
+            $table->integer('discount_id')->unsigned();
+            $table->foreign('discount_id')->references('id')->on('discounts')->onDelete('cascade');
+            $table->integer('channel_id')->unsigned();
+            $table->foreign('channel_id')->references('id')->on('channels')->onDelete('cascade');
+        });
+
+        Schema::table('discount_customer_group', function (Blueprint $table) {
+            $table->integer('discount_id')->nullable();
+            $table->foreign('discount_id')->references('id')->on('discounts')->onDelete('cascade');
+            $table->integer('customer_group_id')->unsigned();
+            $table->foreign('customer_group_id')->references('id')->on('customer_groups')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('discounts', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->dropColumn('type');
+            $table->dropColumn('description');
+            $table->dropColumn( 'status');
+            $table->dropColumn( 'starts_from');
+        });
+
+        Schema::dropIfExists('discount_channels');
+
+        Schema::dropIfExists('discount_customer_group');
+    }
+}
---
+++ packages/Webkul/Discount/src/Database/Migrations/2019_05_01_020921_add_column_in_discount_rules_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnInDiscountRulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('discount_rules', function (Blueprint $table) {
+            Schema::table('discount_rules', function (Blueprint $table) {
+                $table->json('conditions');
+                $table->json('actions');
+                $table->integer('discount_id')->unsigned();
+                $table->foreign('discount_id')->references('id')->on('dicounts');
+                $table->integer('coupon_id')->unsigned()->nullable();
+                $table->foreign('coupon_id')->references('id')->on('coupons');
+                $table->json('label')->nullable();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('discount_rules', function (Blueprint $table) {
+            $table->dropColumn('conditions');
+            $table->dropColumn('actions');
+            $table->dropForeign('discount_id');
+            $table->dropColumn('discount_id');
+            $table->dropForeign('coupon_id');
+            $table->dropColumn('coupon_id');
+            $table->dropColumn('label');
+        });
+    }
+}
