4c101aa646cabe9ab35d7c5fa16dbb923644d8e1
prateek srivastava <prateek.srivastava781@webkul.com>
2019-04-30 12:55:11 +0530

    admin mail notification, review price

---
+++ packages/Webkul/Admin/src/Mail/NewAdminNotification.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Webkul\Admin\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+/**
+ * New Admin Mail class
+ *
+ * @author    Jitendra Singh <jitendra@webkul.com>
+ * @copyright 2018 Webkul Software Pvt Ltd (http://www.webkul.com)
+ */
+class NewAdminNotification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * The order instance.
+     *
+     * @var Order
+     */
+    public $order;
+
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->to(env('ADMIN_MAIL_TO'))
+                ->subject(trans('shop::app.mail.order.subject'))
+                ->view('shop::emails.sales.new-admin-order');
+    }
+}
