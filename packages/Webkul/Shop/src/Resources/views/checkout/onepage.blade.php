3486983b1d6b9f032ff8719345312534be573eec
rahul shukla <rahulshukla.symfony517@webkul.com>
2019-05-08 17:52:54 +0530

    IE completed & redirect customer route

--- packages/Webkul/Shop/src/Resources/views/checkout/onepage.blade.php
+++ packages/Webkul/Shop/src/Resources/views/checkout/onepage.blade.php
@@ -116,7 +116,27 @@
         var shippingHtml = '';
         var paymentHtml = '';
         var reviewHtml = '';
-        var summaryHtml = Vue.compile(`<?php echo view('shop::checkout.total.summary', ['cart' => $cart])->render(); ?>`);
+        var summaryHtml = Vue.compile(
+            "<div class='order-summary'><h3>"
+                + '{{ __('shop::app.checkout.total.order-summary') }}'  +
+            "</h3><div class='item-detail'><label>"
+                +  '{{ intval($cart->items_qty) }}  {{ __('shop::app.checkout.total.sub-total') }}  {{ __('shop::app.checkout.total.price') }}' +
+            "</label><label class='right'>"
+                + '{{ core()->currency($cart->base_sub_total) }}' +
+            "</label></div><?php if($cart->base_tax_total): ?><div class='item-detail'><label>"
+                + '{{ __('shop::app.checkout.total.tax') }}' +
+            "</label><label class='right'>"
+                + '{{ core()->currency($cart->base_tax_total) }}' +
+            "</label></div><?php endif; ?><?php if($cart->selected_shipping_rate): ?><div class='item-detail'><label>"
+                + '{{ __('shop::app.checkout.total.delivery-charges') }}' +
+            "</label><label class='right'>"
+                + '{{ core()->currency($cart->selected_shipping_rate->base_price) }}' +
+            "</label></div><?php endif; ?><div class='payble-amount'><label>"
+                    + '{{ __('shop::app.checkout.total.grand-total') }}' +
+            "</label><label class='right'>"
+                + '{{ core()->currency($cart->base_grand_total) }}' +
+            "</label></div></div>"
+        );
         var customerAddress = null;
 
         @auth('customer')
