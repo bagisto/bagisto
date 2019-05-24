308b3be1d81f2bfe6582afedc89802d07b53c17b
rahul shukla <rahulshukla.symfony517@webkul.com>
2019-05-24 11:53:11 +0530

    installer file deletion after install

--- packages/Webkul/Product/src/Database/Migrations/2019_05_23_113407_add_remaining_column_in_product_flat_table.php
+++ packages/Webkul/Product/src/Database/Migrations/2019_05_23_113407_add_remaining_column_in_product_flat_table.php
@@ -14,25 +14,25 @@ class AddRemainingColumnInProductFlatTable extends Migration
     public function up()
     {
         Schema::table('product_flat', function (Blueprint $table) {
-            if (! Schema::hasColumn('short_description'))
+            if (! Schema::hasColumn('short_description')) {
                 $table->text('short_description')->nullable();
             }
-            if (! Schema::hasColumn('meta_title'))
+            if (! Schema::hasColumn('meta_title')) {
                 $table->text('meta_title')->nullable();
             }
-            if (! Schema::hasColumn('meta_keywords'))
+            if (! Schema::hasColumn('meta_keywords')) {
                 $table->text('meta_keywords')->nullable();
             }
-            if (! Schema::hasColumn('meta_description'))
+            if (! Schema::hasColumn('meta_description')) {
                 $table->text('meta_description')->nullable();
             }
-            if (! Schema::hasColumn('width'))
+            if (! Schema::hasColumn('width')) {
                 $table->decimal('width', 12, 4)->nullable();
             }
-            if (! Schema::hasColumn('height'))
+            if (! Schema::hasColumn('height')) {
                 $table->decimal('height', 12, 4)->nullable();
             }
-            if (! Schema::hasColumn('depth'))
+            if (! Schema::hasColumn('depth')) {
                 $table->decimal('depth', 12, 4)->nullable();
             }
         });
