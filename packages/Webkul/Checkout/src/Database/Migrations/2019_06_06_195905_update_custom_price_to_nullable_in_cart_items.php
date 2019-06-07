f924e7ed019e49ff59ae3aa7d03388b5a0e8c1aa
rahul shukla <rahulshukla.symfony517@webkul.com>
2019-06-07 10:06:00 +0530

    custom price nullable in cart items

---
+++ packages/Webkul/Checkout/src/Database/Migrations/2019_06_06_195905_update_custom_price_to_nullable_in_cart_items.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateCustomPriceToNullableInCartItems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cart_items', function (Blueprint $table) {
+            $table->decimal('custom_price', 12,4)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
