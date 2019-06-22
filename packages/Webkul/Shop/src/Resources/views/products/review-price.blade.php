bddb039ce6184a1f358a1ec521eea0bc45fbed0d
Prashant Singh <prashant.singh852@webkul.com>
2019-06-22 17:06:16 +0530

    Fixed a bug related to product review write in showprice after login

--- packages/Webkul/Shop/src/Resources/views/products/review-price.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/review-price.blade.php
@@ -1,13 +1,20 @@
-<div class="product-price mt-10">
-    @inject ('priceHelper', 'Webkul\Product\Helpers\Price')
+@php
+    $status = core()->getConfigData('ShowPriceAfterLogin.settings.settings.enableordisable');
+    $function = core()->getConfigData('ShowPriceAfterLogin.settings.settings.selectfunction');
+@endphp
 
-    @if ($product->type == 'configurable')
-        <span class="pro-price">{{ core()->currency($priceHelper->getMinimalPrice($product)) }}</span>
-    @else
-        @if ($priceHelper->haveSpecialPrice($product))
-            <span class="pro-price">{{ core()->currency($priceHelper->getSpecialPrice($product)) }}</span>
+@if(($status && ! auth()->guard('customer')->check()) && $function == 'hide-price-buy-cart-guest')
+    <div class="product-price mt-10">
+        @inject ('priceHelper', 'Webkul\Product\Helpers\Price')
+
+        @if ($product->type == 'configurable')
+            <span class="pro-price">{{ core()->currency($priceHelper->getMinimalPrice($product)) }}</span>
         @else
-            <span class="pro-price">{{ core()->currency($product->price) }}</span>
+            @if ($priceHelper->haveSpecialPrice($product))
+                <span class="pro-price">{{ core()->currency($priceHelper->getSpecialPrice($product)) }}</span>
+            @else
+                <span class="pro-price">{{ core()->currency($product->price) }}</span>
+            @endif
         @endif
-    @endif
-</div>
+    </div>
+@endif
