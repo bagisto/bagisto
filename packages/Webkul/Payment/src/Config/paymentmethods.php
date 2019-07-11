7ffbee95ad647c19baf027001c90dfdd12223544
Prashant Singh <prashant.singh852@webkul.com>
2019-07-11 10:16:58 +0530

    Shipping methods and payment methods description converted to translation string

--- packages/Webkul/Payment/src/Config/paymentmethods.php
+++ packages/Webkul/Payment/src/Config/paymentmethods.php
@@ -3,7 +3,7 @@ return [
     'cashondelivery' => [
         'code' => 'cashondelivery',
         'title' => 'Cash On Delivery',
-        'description' => 'Cash On Delivery',
+        'description' => 'shop::app.checkout.onepage.cash-desc',
         'class' => 'Webkul\Payment\Payment\CashOnDelivery',
         'active' => true,
         'sort' => 1
@@ -12,7 +12,7 @@ return [
     'moneytransfer' => [
         'code' => 'moneytransfer',
         'title' => 'Money Transfer',
-        'description' => 'Money Transfer',
+        'description' => 'shop::app.checkout.onepage.money-desc',
         'class' => 'Webkul\Payment\Payment\MoneyTransfer',
         'active' => true,
         'sort' => 2
@@ -21,7 +21,7 @@ return [
     'paypal_standard' => [
         'code' => 'paypal_standard',
         'title' => 'Paypal Standard',
-        'description' => 'Paypal Standard',
+        'description' => 'shop::app.checkout.onepage.paypal-desc',
         'class' => 'Webkul\Paypal\Payment\Standard',
         'sandbox' => true,
         'active' => true,
