06a4f1216b8131f7cf66831297d14c58bfda83cf
Prashant Singh <prashant.singh852@webkul.com>
2019-08-09 19:29:28 +0530

    Dead code removal

--- config/services.php
+++ config/services.php
@@ -42,23 +42,5 @@ return [
         'model' => App\User::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
-    ],
-
-    'facebook' => [
-        'client_id'     => env('FACEBOOK_CLIENT_ID'),
-        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect'      => env('FACEBOOK_URL'),
-    ],
-
-    'twitter' => [
-        'client_id'     => env('TWITTER_CLIENT_ID'),
-        'client_secret' => env('TWITTER_CLIENT_SECRET'),
-        'redirect'      => env('TWITTER_URL'),
-    ],
-
-    'google' => [
-        'client_id'     => env('GOOGLE_CLIENT_ID'),
-        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect'      => env('GOOGLE_URL'),
-    ],
+    ]
 ];
