8c75c20e1d77738978319f7b37e052cb8e5b02f0
Sam <samparker801@gmail.com>
2019-09-03 23:22:51 +0530

    fixed issue #1371

--- packages/Webkul/Admin/src/Listeners/Order.php
+++ packages/Webkul/Admin/src/Listeners/Order.php
@@ -8,7 +8,7 @@ use Webkul\Admin\Mail\NewAdminNotification;
 use Webkul\Admin\Mail\NewInvoiceNotification;
 use Webkul\Admin\Mail\NewShipmentNotification;
 use Webkul\Admin\Mail\NewInventorySourceNotification;
-
+use Webkul\Admin\Mail\CancelOrderNotification;
 /**
  * Order event handler
  *
@@ -77,5 +77,16 @@ class Order {
      */
     public function updateProductInventory($order)
     {
+    }
+
+     /*
+     * @param mixed $order
+     * */
+    public function sendCancelOrderMail($order){
+        try{
+            Mail::queue(new CancelOrderNotification($order));
+        }catch (\Exception $e){
+            Log::error('Error occured when sending email '.$e->getMessage());
+        }
     }
 }
