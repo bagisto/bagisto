9c9727a90c763f66d0faa819289279ec9712521d
benzel <dabenzel.philip@gmail.com>
2019-10-14 22:20:01 +0200

    Create passwords.php

---
+++ resources/lang/de/passwords.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'password' => 'Die Passwörter müssen aus mindestens sechs Zeichen bestehen und mit der Passwortbestätigung übereinstimmen.',
+    'reset' => 'Das Passwort wurde zurückgesetzt!',
+    'sent' => 'Wir haben dir deinen Link zum Zurücksetzen des Passworts per E-Mail geschickt!',
+    'token' => 'Das Token zum Zurücksetzen des Passworts ist ungültig.',
+    'user' => "Wir können keinen Benutzer mit dieser E-Mail-Adresse finden.",
+];
