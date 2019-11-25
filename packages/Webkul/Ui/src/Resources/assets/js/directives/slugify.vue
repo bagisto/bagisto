5eaebbd3d46ab07d64a357159954f80a68daca0f
Herbert Maschke <thyseus@gmail.com>
2019-11-25 15:16:21 +0100

    improve generation of slugs

--- packages/Webkul/Ui/src/Resources/assets/js/directives/slugify.vue
+++ packages/Webkul/Ui/src/Resources/assets/js/directives/slugify.vue
@@ -1,12 +1,15 @@
 <script>
     export default {
         bind(el, binding, vnode) {
-            let handler = function(e) {
-                setTimeout(function() { 
-                    e.target.value = e.target.value.toString().toLowerCase()
-                        .replace(/[^\w- ]+/g,'')
-                        .trim()
-                        .replace(/ +/g,'-');
+            let handler = function (e) {
+                setTimeout(function () {
+                    e.target.value = e.target.value
+                        .toString()
+                        .toLowerCase()
+                        .replace(/[^\w- ]+/g, '')
+                        .replace(/ +/g, '-')
+                        .replace('![-\s]+!u', '-')
+                        .trim();
                 }, 100);
             }
 
