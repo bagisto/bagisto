7380870ea13d4cd580753567d66e7f044a390bd5
jitendra <jitendra@webkul.com>
2019-12-18 17:25:25 +0530

    Issue #1875 fixed

--- packages/Webkul/Rule/src/Helpers/Validator.php
+++ packages/Webkul/Rule/src/Helpers/Validator.php
@@ -24,14 +24,46 @@ class Validator
             if (! $condition['attribute'] || ! $condition['value'])
                 continue;
 
-            if ($entity instanceof \Webkul\Checkout\Contracts\Cart && strpos($condition['attribute'], 'cart|') === false)
-                continue;
-
             $totalConditionCount++;
 
             $attributeValue = $this->getAttributeValue($condition, $entity);
 
-            // dd($attributeValue);
+            if ($rule->condition_type == 1) {
+                if (! $this->validateAttribute($condition['operator'], $attributeValue, $condition['value'])) {
+                    return false;
+                } else {
+                    $validConditionCount++;
+                }
+            } else {
+                if ($this->validateAttribute($condition['operator'], $attributeValue, $condition['value']))
+                    return true;
+            }
+        }
+
+        return $validConditionCount == $totalConditionCount ? true : false;
+    }
+
+    /**
+     * Validate cart rule for condition
+     *
+     * @param CartRule $rule
+     * @param Cart     $cart
+     * @return boolean
+     */
+    public function validateCart($rule, $cart)
+    {
+        if (! $rule->conditions)
+            return true;
+
+        $validConditionCount = $totalConditionCount = 0;
+
+        foreach ($rule->conditions as $condition) {
+            if (! $condition['attribute'] || ! $condition['value'] || strpos($condition['attribute'], 'cart|') === false)
+                continue;
+
+            $totalConditionCount++;
+
+            $attributeValue = $this->getAttributeValue($condition, $cart);
 
             if ($rule->condition_type == 1) {
                 if (! $this->validateAttribute($condition['operator'], $attributeValue, $condition['value'])) {
