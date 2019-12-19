cf837ba2906008bce7d9482c9c1acd2110ec7690
shubhammehrotra.symfony@webkul.com <shubhammehrotra.symfony@webkul.com>
2019-12-19 15:53:01 +0530

    throw exception when session user is not available on product review page

--- packages/Webkul/Shop/src/Http/Controllers/ReviewController.php
+++ packages/Webkul/Shop/src/Http/Controllers/ReviewController.php
@@ -44,8 +44,7 @@ class ReviewController extends Controller
     public function __construct(
         ProductRepository $productRepository,
         ProductReviewRepository $productReviewRepository
-    )
-    {
+    ) {
         $this->productRepository = $productRepository;
 
         $this->productReviewRepository = $productReviewRepository;
@@ -57,15 +56,19 @@ class ReviewController extends Controller
      * Show the form for creating a new resource.
      *
      * @param  string $slug
-     * @return \Illuminate\View\View 
+     * @return \Illuminate\View\View
      */
     public function create($slug)
     {
-        $product = $this->productRepository->findBySlugOrFail($slug);
+        if (auth()->guard('customer')->check()) {
+            $product = $this->productRepository->findBySlugOrFail($slug);
 
-        $guest_review = core()->getConfigData('catalog.products.review.guest_review');
+            $guest_review = core()->getConfigData('catalog.products.review.guest_review');
 
-        return view($this->_config['view'], compact('product', 'guest_review'));
+            return view($this->_config['view'], compact('product', 'guest_review'));
+        }
+
+        abort(404);
     }
 
     /**
@@ -103,7 +106,7 @@ class ReviewController extends Controller
      * Display reviews of particular product.
      *
      * @param  string $slug
-     * @return \Illuminate\View\View 
+     * @return \Illuminate\View\View
     */
     public function show($slug)
     {
