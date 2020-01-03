6bc3b2e5f380715493aa0a63da20bd8810a46385
shubhammehrotra.symfony@webkul.com <shubhammehrotra.symfony@webkul.com>
2020-01-03 13:48:58 +0530

    include with variable update

---
+++ packages/Webkul/Velocity/src/Resources/views/home/category.blade.php
@@ -0,0 +1,37 @@
+@inject ('productRepository', 'Webkul\Product\Repositories\ProductRepository')
+
+@php
+    $categoryDetails = app('Webkul\Category\Repositories\CategoryRepository')->findByPath($category);
+@endphp
+
+@if ($categoryDetails)
+    @php
+        $products = $productRepository->getAll($categoryDetails->id);
+    @endphp
+
+    @if ($products->count())
+        <div class="container-fluid accessories">
+            <card-list-header
+                view-all="{{ route('shop.productOrCategory.index', ['slug' => $categoryDetails->slug]) }}"
+                heading="{{ $categoryDetails->name }}">
+            </card-list-header>
+
+            <div class="row flex-nowrap">
+                <carousel-component
+                    :slides-count="{{ sizeof($products) }}"
+                    slides-per-page="6"
+                    id="accessories-carousel">
+
+                    @foreach ($products as $index => $product)
+
+                        <slide slot="slide-{{ $index }}">
+                            @include ('shop::products.list.card', ['product' => $product])
+                        </slide>
+
+                    @endforeach
+
+                </carousel-component>
+            </div>
+        </div>
+    @endif
+@endif
