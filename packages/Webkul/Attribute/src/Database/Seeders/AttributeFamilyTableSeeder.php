132dd6c0f0ef466f7e726350667e032df450d7ee
Annika Wolff <annika@galaxis-euphoria.de>
2020-01-17 14:42:47 +0100

    introduce guest checkout configuration

--- packages/Webkul/Attribute/src/Database/Seeders/AttributeFamilyTableSeeder.php
+++ packages/Webkul/Attribute/src/Database/Seeders/AttributeFamilyTableSeeder.php
@@ -3,16 +3,20 @@
 namespace Webkul\Attribute\Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use DB;
+use Illuminate\Support\Facades\DB;
 
 class AttributeFamilyTableSeeder extends Seeder
 {
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
         DB::table('attribute_families')->delete();
 
         DB::table('attribute_families')->insert([
             ['id' => '1','code' => 'default','name' => 'Default','status' => '0','is_user_defined' => '1']
         ]);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -23,4 +23,20 @@ class AcceptanceTester extends \Codeception\Actor
    /**
     * Define custom actions here
     */
+
+    /**
+     * Logging in as an Admin
+     */
+    public function loginAsAdmin()
+    {
+        $I = $this;
+        $I->amOnPage('/admin');
+        $I->see('Sign In');
+        $I->fillField('email', 'admin@example.com');
+        $I->fillField('password', 'admin123');
+        $I->dontSee('The "Email" field is required.');
+        $I->dontSee('The "Password" field is required.');
+        $I->click('Sign In');
+        $I->see('Dashboard', '//h1');
+    }
 }
