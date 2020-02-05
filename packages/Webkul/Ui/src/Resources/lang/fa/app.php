be71b01c17935a5d67cb44b4301188f6422dd66b
rahul shukla <rahulshukla.symfony517@webkul.com>
2020-02-05 11:06:06 +0530

    Issue #2268

--- packages/Webkul/Ui/src/Resources/lang/fa/app.php
+++ packages/Webkul/Ui/src/Resources/lang/fa/app.php
@@ -33,5 +33,7 @@ return [
         'between' => 'ما بین',
         'apply' => 'درخواست',
         'items-per-page' => 'Items Per Page',
+        'value-here' => 'Value here',
+        'numeric-value-here' => 'Numeric Value here'
     ]
 ];
