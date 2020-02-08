d2a2b2a8c6f0ee7981b303deeef5eb162364dd58
Vivek Sharma <viveksh047@webkul.com>
2020-02-08 12:59:08 +0530

    [Fixed: issue #2384 has been resolved.]

--- packages/Webkul/Shop/src/Resources/lang/en/app.php
+++ packages/Webkul/Shop/src/Resources/lang/en/app.php
@@ -227,6 +227,7 @@ return [
                     'first_name' => 'First name',
                     'last_name' => 'Last name',
                     'vat_id' => 'Vat id',
+                    'vat_help_note' => '[Note: Use Country Code with VAT Id. Eg. INV01234567891]',
                     'title' => 'Add Address',
                     'street-address' => 'Street Address',
                     'country' => 'Country',
