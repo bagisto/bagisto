51295074562d4b3ecf41e2643b92882f8f32efc8
Herbert Maschke <thyseus@pm.me>
2020-02-17 16:04:59 +0100

    add mail notification defaults (everything true) to ConfigTableSeeder.php

--- packages/Webkul/Core/src/Database/Seeders/ConfigTableSeeder.php
+++ packages/Webkul/Core/src/Database/Seeders/ConfigTableSeeder.php
@@ -15,13 +15,104 @@ class ConfigTableSeeder extends Seeder
         $now = Carbon::now();
 
         DB::table('core_config')->insert([
-            'id' => 1,
-            'code' => 'catalog.products.guest-checkout.allow-guest-checkout',
-            'value' => '1',
+            'id'           => 1,
+            'code'         => 'catalog.products.guest-checkout.allow-guest-checkout',
+            'value'        => '1',
             'channel_code' => null,
-            'locale_code' => null,
-            'created_at' => $now,
-            'updated_at' => $now
+            'locale_code'  => null,
+            'created_at'   => $now,
+            'updated_at'   => $now,
         ]);
+
+        DB::table('core_config')->insert([
+            'id'           => 2,
+            'code'         => 'emails.verification',
+            'value'        => '1',
+            'channel_code' => null,
+            'locale_code'  => null,
+            'created_at'   => $now,
+            'updated_at'   => $now,
+        ]);
+
+        DB::table('core_config')->insert([
+            'id'           => 3,
+            'code'         => 'emails.customer-notification',
+            'value'        => '1',
+            'channel_code' => null,
+            'locale_code'  => null,
+            'created_at'   => $now,
+            'updated_at'   => $now,
+        ]);
+
+        DB::table('core_config')->insert([
+            'id'           => 5,
+            'code'         => 'emails.new-order-notification',
+            'value'        => '1',
+            'channel_code' => null,
+            'locale_code'  => null,
+            'created_at'   => $now,
+            'updated_at'   => $now,
+        ]);
+
+        DB::table('core_config')->insert([
+            'id'           => 6,
+            'code'         => 'emails.new-admin-notification',
+            'value'        => '1',
+            'channel_code' => null,
+            'locale_code'  => null,
+            'created_at'   => $now,
+            'updated_at'   => $now,
+        ]);
+
+        DB::table('core_config')->insert([
+            'id'           => 7,
+            'code'         => 'emails.new-invoice-notification',
+            'value'        => '1',
+            'channel_code' => null,
+            'locale_code'  => null,
+            'created_at'   => $now,
+            'updated_at'   => $now,
+        ]);
+
+        DB::table('core_config')->insert([
+            'id'           => 8,
+            'code'         => 'emails.new-refund-notification',
+            'value'        => '1',
+            'channel_code' => null,
+            'locale_code'  => null,
+            'created_at'   => $now,
+            'updated_at'   => $now,
+        ]);
+
+        DB::table('core_config')->insert([
+            'id'           => 9,
+            'code'         => 'emails.new-shipment-notification',
+            'value'        => '1',
+            'channel_code' => null,
+            'locale_code'  => null,
+            'created_at'   => $now,
+            'updated_at'   => $now,
+        ]);
+
+        DB::table('core_config')->insert([
+            'id'           => 10,
+            'code'         => 'emails.new-inventory-source-notification',
+            'value'        => '1',
+            'channel_code' => null,
+            'locale_code'  => null,
+            'created_at'   => $now,
+            'updated_at'   => $now,
+        ]);
+
+        DB::table('core_config')->insert([
+            'id'           => 11,
+            'code'         => 'emails.cancel-order-notification',
+            'value'        => '1',
+            'channel_code' => null,
+            'locale_code'  => null,
+            'created_at'   => $now,
+            'updated_at'   => $now,
+        ]);
+
     }
 }
