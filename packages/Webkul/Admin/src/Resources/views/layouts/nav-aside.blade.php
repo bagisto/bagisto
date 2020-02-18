09fd7b418d73e5b6c8b738ad3f4cf1f42ba82b27
shivam kumar <shivam.kumar174@webkul.com>
2020-02-18 18:06:43 +0530

    #1248 enhanced

--- packages/Webkul/Admin/src/Resources/views/layouts/nav-aside.blade.php
+++ packages/Webkul/Admin/src/Resources/views/layouts/nav-aside.blade.php
@@ -1,4 +1,4 @@
-<div class="aside-nav">
+<div class="aside-nav" id="aside-nav">
     <ul>
         @if (request()->route()->getName() != 'admin.configuration.index')
             <?php $keys = explode('.', $menu->currentKey);  ?>
@@ -31,7 +31,32 @@
         @endif
     </ul>
 
-    <!-- <div class="close-nav-aside">
-        <i class="icon angle-left-icon close-icon"></i>
-    </div> -->
+    <div class="close-nav-aside">
+        <i class="icon angle-left-icon close-icon" id="sideBar" onClick="closeSideBar()"></i>
+    </div>
 </div>
+
+@push('scripts')
+    <script>
+        function closeSideBar()
+        {
+            var asideNav = document.getElementById("aside-nav").style.width;
+            var element = document.getElementById("sideBar");
+            var content = document.getElementsByClassName("content-wrapper")[0];
+
+            if (asideNav == '0px') {
+                content.style.marginLeft = "280px";
+                element.classList.add("angle-left-icon");
+                element.classList.remove("angle-right-icon");
+                document.getElementById("aside-nav").style.width = "280px";
+            } else {
+                content.style.marginLeft = "0px";
+                element.classList.remove("angle-left-icon");
+                element.classList.add("angle-right-icon");
+                document.getElementById("aside-nav").style.width = "0px";
+            }
+
+            document.getElementById("sideBar").style.float = "left";
+        }
+    </script>
+@endpush
