60a03e3b2861ba87af3f5def1bba5d81d7bd1c88
shubhammehrotra.symfony@webkul.com <shubhammehrotra.symfony@webkul.com>
2020-02-18 17:34:19 +0530

    misc. updates

--- packages/Webkul/Velocity/src/Resources/assets/js/UI/components/product-quick-view.vue
+++ packages/Webkul/Velocity/src/Resources/assets/js/UI/components/product-quick-view.vue
@@ -1,54 +1,83 @@
 <template>
     <div class="modal-parent scrollable">
         <div class="cd-quick-view">
-            <div class="col-lg-6 product-gallery">
-                <ul class="cd-slider" type="none">
-                    <carousel-component
-                        slides-per-page="1"
-                        navigation-enabled="hide"
-                        :slides-count="product.galleryImages.length">
+            <template v-if="showProductDetails || true">
+                <div class="col-lg-6 product-gallery">
+                    <ul class="cd-slider" type="none">
+                        <carousel-component
+                            slides-per-page="1"
+                            navigation-enabled="hide"
+                            :slides-count="product.galleryImages.length">
 
-                            <slide
-                                :key="index"
-                                :slot="`slide-${index}`"
-                                v-for="(image, index) in product.galleryImages">
+                                <slide
+                                    :key="index"
+                                    :slot="`slide-${index}`"
+                                    v-for="(image, index) in product.galleryImages">
 
-                                <li class="selected">
-                                    <img :src="image.medium_image_url" :alt="product.name" />
-                                </li>
-                            </slide>
-                    </carousel-component>
-                </ul>
-            </div>
+                                    <li class="selected" @click="showProductDetails = false">
+                                        <img :src="image.medium_image_url" :alt="product.name" />
+                                    </li>
+                                </slide>
+                        </carousel-component>
+                    </ul>
+                </div>
 
-            <div class="col-lg-6 fs16">
-                <h2 class="text-nowrap fw6 quick-view-name">{{ product.name }}</h2>
+                <div class="col-lg-6 fs16">
+                    <h2 class="fw6 quick-view-name">{{ product.name }}</h2>
 
-                <div class="product-price" v-html="product.priceHTML"></div>
+                    <div class="product-price" v-html="product.priceHTML"></div>
+
+                    <div
+                        class="product-rating"
+                        v-if="product.totalReviews && product.totalReviews > 0">
+
+                        <star-ratings :ratings="product.avgRating"></star-ratings>
+                        <a class="pl10 unset active-hover" :href="`${$root.baseUrl}/reviews/${product.slug}`">
+                            {{ __('products.reviews-count', {'totalReviews': product.totalReviews}) }}
+                        </a>
+                    </div>
+
+                    <div class="product-rating" v-else>
+                        <span class="fs14" v-text="product.firstReviewText"></span>
+                    </div>
+
+                    <p class="pt14 fs14 description-text" v-html="product.shortDescription"></p>
+
+                    <vnode-injector :nodes="getAddToCartHtml()"></vnode-injector>
+                </div>
 
                 <div
-                    class="product-rating"
-                    v-if="product.totalReviews && product.totalReviews > 0">
+                    @click="closeQuickView"
+                    class="close-btn rango-close fs18 cursor-pointer">
+                </div>
+            </template>
 
-                    <star-ratings :ratings="product.avgRating"></star-ratings>
-                    <span class="pl10">
-                        {{ __('products.reviews', {'totalReviews': product.totalReviews}) }}
-                    </span>
+            <template v-else>
+                <div class="product-gallery">
+                    <ul class="cd-slider" type="none">
+                        <carousel-component
+                            slides-per-page="1"
+                            navigation-enabled="hide"
+                            :slides-count="product.galleryImages.length">
+
+                                <slide
+                                    :key="index"
+                                    :slot="`slide-${index}`"
+                                    v-for="(image, index) in product.galleryImages">
+
+                                    <li class="selected">
+                                        <img :src="image.medium_image_url" :alt="product.name" />
+                                    </li>
+                                </slide>
+                        </carousel-component>
+                    </ul>
                 </div>
 
-                <div class="product-rating" v-else>
-                    <span class="fs14" v-text="product.firstReviewText"></span>
+                <div
+                    @click="showProductDetails = true"
+                    class="close-btn rango-close fs18 cursor-pointer">
                 </div>
-
-                <p class="pt14 fs14 description-text" v-html="product.description"></p>
-
-                <vnode-injector :nodes="getAddToCartHtml()"></vnode-injector>
-            </div>
-
-            <div
-                @click="closeQuickView"
-                class="close-btn rango-close fs18 cursor-pointer">
-            </div>
+            </template>
         </div>
     </div>
 </template>
@@ -58,6 +87,7 @@
         data: function () {
             return {
                 currentlyActiveImage: 0,
+                showProductDetails: true,
                 product: this.$root.productDetails,
             }
         },
