3365924529b1af56ba61f25257771356b6555e69
ghermans <glhermans@gmail.com>
2020-02-25 03:30:13 +0100

    #2525 updated config file

--- config/app.php
+++ config/app.php
@@ -116,8 +116,8 @@ return [
     | Here you may specify the base currency code for your application.
     |
     */
-
-    'currency' => env('APP_CURRENCY','USD'),
+    
+    'currency' => env('APP_CURRENCY', 'USD'),
 
     /*
     |--------------------------------------------------------------------------
