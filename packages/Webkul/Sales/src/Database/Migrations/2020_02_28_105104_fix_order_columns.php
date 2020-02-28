6c5e9a73f9298801964ac6877664a0c595f2cd06
Herbert Maschke <thyseus@pm.me>
2020-02-28 10:54:00 +0100

    remove unnecessary database columns in order table

---
+++ packages/Webkul/Sales/src/Database/Migrations/2020_02_28_105104_fix_order_columns.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FixOrderColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order', function (Blueprint $table) {
+            $table->removeColumn('customer_company_name');
+            $table->removeColumn('customer_vat_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
