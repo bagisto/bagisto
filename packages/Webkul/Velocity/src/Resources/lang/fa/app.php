54d8ff9487c564f1fe47c678d19559763cf3b6ce
mohd asif <mohdasif.woocommerce337@webkul.com>
2020-03-12 16:09:05 +0530

    added tranlation fa for velocity package

---
+++ packages/Webkul/Velocity/src/Resources/lang/fa/app.php
@@ -0,0 +1,290 @@
+<?php
+
+return [
+    'admin' => [
+        'system'   => [
+            'velocity' => [
+                'general'  => 'عمومی',
+                'category'  => 'دسته بندی',
+                'settings'  => 'تنظیمات',
+                'extension_name' => 'تم سرعت',
+                'error-module-inactive' => 'Warning: وضعیت موضوع سرعت غیرفعال است',
+            ],
+
+            'settings' => [
+                'channels'=> [
+                    'subscription_bar' => 'محتوای نوار اشتراک'
+                ],
+            ],
+
+            'general' => [
+                'status' => 'وضعیت',
+                'active' => 'فعال',
+                'inactive' => 'غیر فعال',
+            ],
+            'category' => [
+                'all' => 'همه',
+                'left' => 'ترک کرد',
+                'right' => 'درست',
+                'active' => 'فعال',
+                'custom' => 'سفارشی',
+                'inactive' => 'غیر فعال',
+                'image-alignment' => 'تراز تصویر',
+                'icon-status' => 'وضعیت نماد دسته',
+                'image-status' => 'وضعیت تصویر گروه',
+                'sub-category-show' => 'نمایش زیر گروه',
+                'image-height' => '[in Pixel]ارتفاع تصویر',
+                'image-width' => '[in Pixel]عرض تصویر',
+                'show-tooltip' => 'نمایش راهنمای ابزار',
+                'num-sub-category' => 'تعداد زیر گروه',
+            ]
+        ],
+        'layouts' => [
+            'velocity' => 'Velocity',
+            'cms-pages' => 'CMS صفحات',
+            'meta-data' => 'Meta Data',
+            'category-menu' => 'منوی دسته',
+            'header-content' => 'محتوای سرصفحه',
+        ],
+        'contents' => [
+            'self' => 'خود',
+            'active' => 'فعال',
+            'new-tab' => 'برگه جدید',
+            'inactive' => 'غیر فعال',
+            'title' => 'لیست مطالب',
+            'select' => '-- انتخاب کنید --',
+            'add-title' => 'اضافه کردن محتوا',
+            'btn-add-content' => 'اضافه کردن محتوا',
+            'save-btn-title' => 'ذخیره محتوا',
+            'autocomplete' => '[کامل]',
+            'no-result-found' => 'هیچ سابقه ای پیدا نشد.',
+            'search-hint' => '... جستجوی محصول در اینجا',
+            'mass-delete-success' => 'محتوای انتخاب شده با موفقیت حذف شد',
+            'tab' => [
+                'page' => 'تنظیم صفحه',
+                'content' => 'تنظیم محتوا',
+                'meta_content' => 'Meta Data',
+            ],
+            'page' => [
+                'title' => 'عنوان',
+                'status' => 'وضعیت',
+                'position' => 'موقعیت',
+            ],
+            'content' => [
+                'content-type' => 'نوع محتوا',
+                'custom-title' => 'عنوان سفارشی',
+                'category-slug' => 'دسته Slug',
+                'link-target' => 'هدف پیوند صفحه',
+                'custom-product' => 'محصولات فروشگاهی',
+                'custom-heading' => 'عنوان سفارشی',
+                'catalog-type' => 'نوع کاتالوگ محصول',
+                'static-description' => 'توضیحات محتوا',
+                'page-link' => '[e.g. http://example.com/../../] پیوند صفحه',
+            ],
+            'datagrid' => [
+                'id' => 'Id',
+                'title' => 'عنوان',
+                'status' => 'وضعیت',
+                'position' => 'موقعیت',
+                'content-type' => 'نوع محتوا',
+            ]
+        ],
+        'meta-data' => [
+            'footer' => 'Footer',
+            'title' => 'داده های متا سرعت',
+            'activate-slider' => 'لغزنده را فعال کنید',
+            'home-page-content' => 'محتوای صفحه اصلی',
+            'footer-left-content' => 'بالا و پایین صفحه',
+            'subscription-content' => 'نوار اشتراک محتوا',
+            'sidebar-categories' => 'دسته بندی های نوار کناری',
+            'footer-left-raw-content' => '<p>ما دوست داریم که نرم افزارهایی را تهیه کرده و مشکلات دنیای واقعی را با باینری حل کنیم. ما به اهداف خود بسیار متعهد هستیم. ما منابع خود را برای ایجاد کلاس های نرم افزاری و برنامه های کاربردی برای تجارت سازمانی با درجه برتر ، در لبه تخصص فناوری سرمایه گذاری می کنیم..</p>',
+            'slider-path' => 'مسیر کشویی',
+            'category-logo' => 'آرم دسته',
+            'product-policy' => 'خط مشی محصول',
+            'update-meta-data' => 'Meta Data به روز رسانی',
+            'product-view-image' => 'تصویر نمایش محصول',
+            'advertisement-two' => 'تبلیغات دو تصویر',
+            'advertisement-one' => 'تبلیغات یک تصویر',
+            'footer-middle-content' => 'پاورقی مطالب میانی',
+            'advertisement-four' => 'تبلیغات چهار تصویر',
+            'advertisement-three' => 'تبلیغات سه تصویر',
+            'images' => 'تصاویر',
+            'general' => 'عمومی',
+            'add-image-btn-title' => 'تصویر اضافه کن'
+        ],
+        'category' => [
+            'save-btn-title' => 'ذخیره منو',
+            'title' => 'لیست فهرست های دسته بندی',
+            'add-title' => 'محتوای منو را اضافه کنید',
+            'edit-title' => 'محتوای منو را ویرایش کنید',
+            'btn-add-category' => 'محتوای دسته بندی را اضافه کنید',
+            'datagrid' => [
+                'category-id' => 'Id دسته بندی',
+                'category-name' => 'نام دسته',
+                'category-icon' => 'نماد دسته',
+                'category-status' => 'وضعیت',
+            ],
+            'tab' => [
+                'general' => 'عمومی',
+            ],
+            'status' => 'وضعیت',
+            'active' => 'فعال',
+            'inactive' => 'غیر فعال',
+            'select' => '-- انتخاب کنید --',
+            'icon-class' => 'کلاس آیکون',
+            'select-category' => 'دسته را انتخاب کنید',
+            'tooltip-content' => 'محتوای راهنمای ابزار',
+            'mass-delete-success' => 'منوی دسته انتخاب شده با موفقیت حذف شد.',
+        ],
+        'general' => [
+            'locale_logo' => 'لوگوی محلی',
+        ],
+    ],
+
+    'home' => [
+        'view-all' => 'مشاهده همه',
+        'add-to-cart' => 'افزودن به سبد خرید',
+        'hot-categories' => 'دسته بندی های داغ',
+        'payment-methods' => 'روش های پرداخت',
+        'customer-reviews' => 'نظرات مشتری',
+        'shipping-methods' => 'روش های حمل و نقل',
+        'popular-categories' => 'دسته بندی های محبوب',
+    ],
+
+    'header' => [
+        'cart' => 'سبد خرید',
+        'cart' => 'سبد خرید',
+        'guest' => 'مهمان',
+        'logout' => 'خروج',
+        'title' => 'حساب',
+        'account' => 'حساب',
+        'profile' => 'مشخصات',
+        'wishlist' => 'لیست علاقه مندیها',
+        'all-categories' => 'همه دسته بندی ها',
+        'search-text' => 'محصولات را در اینجا جستجو کنید',
+        'welcome-message' => 'خوش آمدی, :customer_name',
+        'dropdown-text' => 'مدیریت سبد خرید, سفارشات و لیست دلخواه',
+    ],
+
+    'menu-navbar' => [
+        'text-more' => 'بیشتر',
+        'text-category' => 'خرید بر اساس طبقه بندی',
+    ],
+
+    'minicart' => [
+        'cart' => 'سبد خرید',
+        'view-cart' => 'مشاهده سبد خرید',
+    ],
+
+    'checkout' => [
+        'qty' => 'تعداد',
+        'checkout' => 'وارسی',
+        'cart' => [
+            'view-cart' => 'مشاهده سبد خرید',
+            'cart-summary' => 'خلاصه سبد خرید',
+        ],
+        'qty' => 'تعداد',
+        'items' => 'موارد',
+        'subtotal' => 'فرعی',
+        'sub-total' => 'زیر مجموع',
+        'proceed' => 'ادامه به پرداخت',
+    ],
+
+    'customer' => [
+        'compare' => [
+            'text' => 'مقایسه کنید',
+            'compare_similar_items' => 'موارد مشابه را مقایسه کنید',
+            'added' => 'مورد با موفقیت برای مقایسه لیست اضافه شد',
+            'already_added' => 'مورد در حال حاضر برای مقایسه لیست اضافه شده است',
+            'removed' => 'مورد با موفقیت از لیست مقایسه حذف شد',
+            'empty-text' => "شما هیچ موردی را در لیست مقایسه خود ندارید",
+        ],
+        'login-form' => [
+            'sign-up' => 'ثبت نام',
+            'new-customer' => 'مشتری جدید',
+            'customer-login' => 'ورود مشتری',
+            'registered-user' => 'کاربر ثبت نام شده',
+            'your-email-address' => 'آدرس ایمیل شما',
+            'form-login-text' => 'Iدر صورت داشتن یک حساب کاربری ، با آدرس ایمیل خود وارد سیستم شوید.',
+        ],
+        'signup-form' => [
+            'login' => 'وارد شدن',
+            'become-user' => 'کاربر شوید',
+            'user-registration' => 'ثبت نام کاربر',
+            'form-sginup-text' => 'اگر تازه وارد فروشگاه ما هستید ، ما دوست داریم شما را به عنوان عضویت در اختیار داشته باشیم.',
+        ],
+        'forget-password' => [
+            'login' => 'وارد شدن',
+            'forgot-password' => 'رمز عبور را فراموش کرده اید',
+            'recover-password' => 'بازیابی رمز عبور',
+            'recover-password-text' => 'اگر رمز عبور خود را فراموش کرده اید ، با وارد کردن آدرس ایمیل خود آن را بازیابی کنید.',
+        ]
+    ],
+
+    'error' => [
+        'go-to-home' => 'برو خانه',
+        'page-lost-short' => 'محتوای گم شده صفحه',
+        'something_went_wrong' => 'چیزی پیش آمد',
+        'page-lost-description' => "صفحه مورد نظر در دسترس نیست. سعی کنید دوباره جستجو کنید یا از دکمه Go Back در زیر استفاده کنید.",
+    ],
+
+    'products' => [
+        'text' => 'محصولات',
+        'details' => 'جزئیات',
+        'reviews-title' => 'بررسی ها',
+        'reviewed' => 'بررسی شده',
+        'review-by' => 'مرور توسط',
+        'quick-view' => 'مشاهده سریع',
+        'not-available' => 'در دسترس نیست',
+        'submit-review' => 'ارسال بررسی',
+        'ratings' => ':totalRatings رتبه بندی',
+        'reviews-count' => ':totalReviews بررسی ها',
+        'customer-rating' => 'رتبه بندی مشتری',
+        'more-infomation' => 'اطلاعات بیشتر',
+        'view-all-reviews' => 'مشاهده همه نظرات',
+        'write-your-review' => 'نظر خود را بنویسید',
+        'short-description' => 'توضیحات کوتاه',
+        'recently-viewed' => 'محصولات اخیرا مشاهده شده',
+        'be-first-review' => 'اولین نفری باشید که نظر می دهد',
+    ],
+
+    'shop' => [
+        'gender' => [
+            'male' => 'نر',
+            'other' => 'دیگر',
+            'female' => 'زن',
+        ],
+        'general' => [
+            'view' => 'چشم انداز',
+            'filter' => 'فیلتر',
+            'orders' => 'سفارشات',
+            'update' => 'به روز رسانی',
+            'reviews' => 'بررسی ها',
+            'addresses' => 'آدرس ها',
+            'top-brands' => 'برندهای برتر',
+            'new-password' => 'رمز عبور جدید',
+            'downloadables' => 'محصولات دانلودی',
+            'confirm-new-password' => 'رمزعبور جدید را تأیید کنید',
+            'enter-current-password' => 'رمز عبور فعلی خود را وارد کنید',
+
+            'alert' => [
+                'info' => 'اطلاعات',
+                'error' => 'خطا',
+                'success' => 'موفقیت',
+                'warning' => 'هشدار دهنده',
+            ],
+        ],
+        'wishlist' => [
+            'add-wishlist-text' => 'محصول را به لیست دلخواه اضافه کنید',
+            'remove-wishlist-text' => 'محصول را از لیست دلخواه حذف کنید'
+        ]
+    ],
+
+    'responsive' => [
+        'header' => [
+            'greeting' => ':customer ! خوش آمدی',
+        ]
+    ],
+]
+
+?>
