80508e7a06c6b0dcea5f5e70a5af552f28e41b8d
Pranshu Tomar <pranshutomar@Pranshus-MacBook-Air.local>
2020-03-24 18:09:02 +0530

    Upgraded velocity theme npm dependencies

--- packages/Webkul/Velocity/webpack.mix.js
+++ packages/Webkul/Velocity/webpack.mix.js
@@ -1,4 +1,9 @@
-const { mix } = require("laravel-mix");
+const mix = require("laravel-mix");
+
+if (mix == 'undefined') {
+    const { mix } = require("laravel-mix");
+}
+
 require("laravel-mix-merge-manifest");
 
 var publicPath = "../../../public/themes/velocity/assets";
@@ -12,8 +17,7 @@ mix.disableNotifications();
 
 mix
     .js(
-        __dirname + "/src/Resources/assets/js/app.js",
-        "js/velocity.js"
+        __dirname + "/src/Resources/assets/js/app.js", "js/velocity.js"
     )
 
     .sass(
@@ -22,9 +26,7 @@ mix
     )
     .sass(
         __dirname + '/src/Resources/assets/sass/app.scss',
-        __dirname + '/' + publicPath + '/css/velocity.css', {
-            includePaths: ['node_modules/bootstrap-sass/assets/stylesheets/'],
-        }
+        __dirname + '/' + publicPath + '/css/velocity.css'
     )
 
     .options({
