a8f8ecc23f70bc77e3d75c6860435d7e379e0c0e
Jan Bickel <jan.bickel@kmso.de>
2020-04-28 15:49:58 +0200

    added: files for translation

---
+++ packages/Webkul/Velocity/src/Resources/lang/de/app.php
@@ -0,0 +1,293 @@
+<?php
+
+return [
+    'admin'         => [
+        'system'    => [
+            'velocity' => [
+                'general'  => 'General',
+                'category'  => 'Category',
+                'settings'  => 'Settings',
+                'extension_name' => 'Velocity Theme',
+                'error-module-inactive' => 'Warning: Velocity theme status is inactive',
+            ],
+
+            'settings' => [
+                'channels'=> [
+                    'subscription_bar' => 'Subscription bar content'
+                ],
+            ],
+
+            'general' => [
+                'status' => 'Status',
+                'active' => 'Active',
+                'inactive' => 'Inactive',
+            ],
+            'category' => [
+                'all' => 'All',
+                'left' => 'Left',
+                'right' => 'Right',
+                'active' => 'Active',
+                'custom' => 'Custom',
+                'inactive' => 'Inactive',
+                'image-alignment' => 'Image Alignment',
+                'icon-status' => 'Category Icon Status',
+                'image-status' => 'Category Image Status',
+                'sub-category-show' => 'Show Sub Category',
+                'image-height' => 'Image\'s Height [in Pixel]',
+                'image-width' => 'Image\'s Width [in Pixel]',
+                'show-tooltip' => 'Show Category\'s Tooltip',
+                'num-sub-category' => 'Number Of Sub Category',
+            ]
+        ],
+        'layouts'   => [
+            'velocity' => 'Velocity',
+            'cms-pages' => 'CMS Pages',
+            'meta-data' => 'Meta Data',
+            'category-menu' => 'Category Menu',
+            'header-content' => 'Header Content',
+        ],
+        'contents'  => [
+            'self' => 'Self',
+            'active' => 'Active',
+            'new-tab' => 'New Tab',
+            'inactive' => 'Inactive',
+            'title' => 'Content List',
+            'select' => '-- Select --',
+            'add-title' => 'Add Content',
+            'btn-add-content' => 'Add Content',
+            'save-btn-title' => 'Save Content',
+            'autocomplete' => '[Autocomplete]',
+            'no-result-found' => 'No record found.',
+            'search-hint' => 'Search product here...',
+            'mass-delete-success' => 'Selected content deleted successfully.',
+            'tab' => [
+                'page' => 'Page Setting',
+                'content' => 'Content Setting',
+                'meta_content' => 'Meta Data',
+            ],
+            'page' => [
+                'title' => 'Title',
+                'status' => 'Status',
+                'position' => 'Position',
+            ],
+            'content' => [
+                'content-type' => 'Content Type',
+                'custom-title' => 'Custom Title',
+                'category-slug' => 'Category Slug',
+                'link-target' => 'Page Link Target',
+                'custom-product' => 'Store Products',
+                'custom-heading' => 'Custom Heading',
+                'catalog-type' => 'Product Catalog Type',
+                'static-description' => 'Content Description',
+                'page-link' => 'Page Link [e.g. http://example.com/../../]',
+            ],
+            'datagrid' => [
+                'id' => 'Id',
+                'title' => 'Title',
+                'status' => 'Status',
+                'position' => 'Position',
+                'content-type' => 'Content Type',
+            ]
+        ],
+        'meta-data' => [
+            'footer' => 'Footer',
+            'title' => 'Velocity meta data',
+            'activate-slider' => 'Activate Slider',
+            'home-page-content' => 'Home Page Content',
+            'footer-left-content' => 'Footer Left Content',
+            'subscription-content' => 'Subscription bar Content',
+            'sidebar-categories' => 'Sidebar Categories',
+            'footer-left-raw-content' => '<p>We love to craft softwares and solve the real world problems with the binaries. We are highly committed to our goals. We invest our resources to create world class easy to use softwares and applications for the enterprise business with the top notch, on the edge technology expertise.</p>',
+            'slider-path' => 'Slider Path',
+            'category-logo' => 'Category logo',
+            'product-policy' => 'Product Policy',
+            'update-meta-data' => 'Update Meta Data',
+            'product-view-image' => 'Product View Image',
+            'advertisement-two' => 'Advertisement Two Images',
+            'advertisement-one' => 'Advertisement One Images',
+            'footer-middle-content' => 'Footer Middle Content',
+            'advertisement-four' => 'Advertisement Four Images',
+            'advertisement-three' => 'Advertisement Three Images',
+            'images' => 'Images',
+            'general' => 'General',
+            'add-image-btn-title' => 'Add Image'
+        ],
+        'category'  => [
+            'save-btn-title' => 'Save Menu',
+            'title' => 'Category Menu List',
+            'add-title' => 'Add Menu Content',
+            'edit-title' => 'Edit Menu Content',
+            'btn-add-category' => 'Add Category Content',
+            'datagrid' => [
+                'category-id' => 'Category Id',
+                'category-name' => 'Category Name',
+                'category-icon' => 'Category Icon',
+                'category-status' => 'Status',
+            ],
+            'tab' => [
+                'general' => 'General',
+            ],
+            'status' => 'Status',
+            'active' => 'Active',
+            'inactive' => 'Inactive',
+            'select' => '-- Select --',
+            'icon-class' => 'Icon Class',
+            'select-category' => 'Choose Category',
+            'tooltip-content' => 'Tooltip Content',
+            'mass-delete-success' => 'Selected categories menu deleted successfully.',
+        ],
+        'general'   => [
+            'locale_logo' => 'Locale Logo',
+        ],
+    ],
+
+    'home'          => [
+        'view-all'           => 'View All',
+        'add-to-cart'        => 'Add To Cart',
+        'hot-categories'     => 'Hot Categories',
+        'payment-methods'    => 'Payment Methods',
+        'customer-reviews'   => 'Customer Reviews',
+        'shipping-methods'   => 'Shipping Methods',
+        'popular-categories' => 'Popular Categories',
+    ],
+
+    'header'        => [
+        'cart'              => 'Cart',
+        'cart'              => 'Cart',
+        'guest'             => 'Guest',
+        'logout'            => 'Logout',
+        'title'             => 'Account',
+        'account'           => 'Account',
+        'profile'           => 'Profile',
+        'wishlist'          => 'Wishlist',
+        'all-categories'    => 'All Categories',
+        'search-text'       => 'Search products here',
+        'welcome-message'   => 'Welcome, :customer_name',
+        'dropdown-text'     => 'Manage Cart, Orders & Wishlist',
+    ],
+
+    'menu-navbar'   => [
+        'text-more'     => 'More',
+        'text-category' => 'Shop by Category',
+    ],
+
+    'minicart'      => [
+        'cart'      => 'Cart',
+        'view-cart' => 'View Cart',
+    ],
+
+    'checkout'      => [
+        'qty'       => 'Qty',
+        'checkout'  => 'Checkout',
+        'cart'      => [
+            'view-cart'     => 'View Cart',
+            'cart-summary'  => 'Cart Summary',
+        ],
+        'qty'       => 'Qty',
+        'items'     => 'Items',
+        'subtotal'  => 'Subtotal',
+        'sub-total' => 'Sub Total',
+        'proceed'   => 'Proceed to checkout',
+    ],
+
+    'customer'      => [
+        'compare'           => [
+            'text'                  => 'Compare',
+            'compare_similar_items' => 'Compare Similar Items',
+            'added'                 => 'Item successfully added to compare list',
+            'already_added'         => 'Item already added to compare list',
+            'removed'               => 'Item successfully removed from compare list',
+            'empty-text'            => "You don't have any items in your compare list",
+        ],
+        'login-form'        => [
+            'sign-up'               => 'Sign up',
+            'new-customer'          => 'New Customer',
+            'customer-login'        => 'Customer Login',
+            'registered-user'       => 'Registered User',
+            'your-email-address'    => 'Your email address',
+            'form-login-text'       => 'If you have an account, sign in with your email address.',
+        ],
+        'signup-form'       => [
+            'login'             => 'Login',
+            'become-user'       => 'Become User',
+            'user-registration' => 'User Registration',
+            'form-sginup-text'  => 'If you are new to our store, we glad to have you as member.',
+        ],
+        'forget-password'   => [
+            'login'                 => 'Login',
+            'forgot-password'       => 'Forgot Password',
+            'recover-password'      => 'Recover Password',
+            'recover-password-text' => 'If you forgot your password, recover it by entering your email address.',
+        ]
+    ],
+
+    'error'         => [
+        'go-to-home'            => 'Go to home',
+        'page-lost-short'       => 'Page lost content',
+        'something_went_wrong'  => 'something went wrong',
+        'page-lost-description' => "The page you're looking for isn't available. Try to search again or use the Go Back button below.",
+    ],
+
+    'products'      => [
+        'text'              => 'Products',
+        'details'           => 'Details',
+        'reviews-title'     => 'Reviews',
+        'reviewed'          => 'Reviewed',
+        'review-by'         => 'Review by',
+        'quick-view'        => 'Quick View',
+        'not-available'     => 'Not Available',
+        'submit-review'     => 'Submit Review',
+        'ratings'           => ':totalRatings Ratings',
+        'reviews-count'     => ':totalReviews Reviews',
+        'customer-rating'   => 'Customer Rating',
+        'more-infomation'   => 'More Information',
+        'view-all-reviews'  => 'View All Reviews',
+        'write-your-review' => 'Write Your Review',
+        'short-description' => 'Short Descriptions',
+        'recently-viewed'   => 'Recently Viewed Products',
+        'be-first-review'   => 'Be the first to write a review',
+    ],
+
+    'shop'          => [
+        'gender'    => [
+            'male'   => 'Male',
+            'other'  => 'Other',
+            'female' => 'Female',
+        ],
+        'general'   => [
+            'no'                     => 'No',
+            'yes'                    => 'Yes',
+            'view'                   => 'View',
+            'filter'                 => 'Filter',
+            'orders'                 => 'Orders',
+            'update'                 => 'Update',
+            'reviews'                => 'Reviews',
+            'addresses'              => 'Addresses',
+            'top-brands'             => 'Top Brands',
+            'new-password'           => 'New password',
+            'downloadables'          => 'Downloadable Products',
+            'confirm-new-password'   => 'Confirm new password',
+            'enter-current-password' => 'Enter your current password',
+
+            'alert' => [
+                'info'      => 'Info',
+                'error'     => 'Error',
+                'success'   => 'Success',
+                'warning'   => 'Warning',
+            ],
+        ],
+        'wishlist'  => [
+            'add-wishlist-text'     => 'Add product to wishlist',
+            'remove-wishlist-text'  => 'Remove product from wishlist'
+        ]
+    ],
+
+    'responsive'    => [
+        'header' => [
+            'languages' => 'Languages',
+            'greeting'  => 'Welcome, :customer !',
+        ]
+    ],
+]
+
+?>
