cbd873b1067f005ff1ee7896e6f616e1f0d49c0b
Steffen Mahler <steffen.mahler@haendlerbund.de>
2020-04-29 09:27:19 +0200

    fix calulation helper in test

--- tests/unit/CartRule/CartRuleCest.php
+++ tests/unit/CartRule/CartRuleCest.php
@@ -138,11 +138,11 @@ class expectedCart
     {
         $this->sub_total = round($this->sub_total, self::CART_TOTAL_PRECISION);
         $this->tax_total = round($this->tax_total, self::CART_TOTAL_PRECISION);
-        $this->grand_total = $this->sub_total + $this->tax_total - $this->discount_amount;
+        $this->grand_total = round($this->sub_total + $this->tax_total - $this->discount_amount, self::CART_TOTAL_PRECISION);
 
         $this->base_sub_total = round($this->base_sub_total, self::CART_TOTAL_PRECISION);
         $this->base_tax_total = round($this->base_tax_total, self::CART_TOTAL_PRECISION);
-        $this->base_grand_total = $this->base_sub_total + $this->base_tax_total - $this->base_discount_amount;
+        $this->base_grand_total = round($this->base_sub_total + $this->base_tax_total - $this->base_discount_amount, self::CART_TOTAL_PRECISION);
     }
 
     public function toArray(): array
