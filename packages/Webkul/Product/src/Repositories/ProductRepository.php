fee6645138ddac4414719aebcaead72401f0523a
rahulshukla-home <rahulcs0082@gmail.com>
2020-04-29 14:35:49 +0530

    Issue #2942

--- packages/Webkul/Product/src/Repositories/ProductRepository.php
+++ packages/Webkul/Product/src/Repositories/ProductRepository.php
@@ -289,7 +289,7 @@ class ProductRepository extends Repository
                             ->where('product_flat.featured', 1)
                             ->where('product_flat.channel', $channel)
                             ->where('product_flat.locale', $locale)
-                            ->orderBy('product_id', 'desc');
+                            ->inRandomOrder();
         })->paginate(4);
 
         return $results;
