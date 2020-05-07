34d780707aef1b763c109919b2f615403a0e81bd
prateek srivastava <prateek.srivastava781@webkul.com>
2020-05-07 18:35:30 +0530

    file name changes

--- packages/Webkul/Bulkupload/src/Database/Migrations/2019_04_23_115227_create_bulkupload_data_flow_profiles_table.php
+++ packages/Webkul/Bulkupload/src/Database/Migrations/2019_04_23_115227_create_bulkupload_data_flow_profiles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBulkuploadDataflowprofileTable extends Migration
+class CreateBulkuploadDataflowprofilesTable extends Migration
 {
     /**
      * Run the migrations.
--- packages/Webkul/Bulkupload/src/Database/Migrations/2019_04_29_105529_create_import_new_products_table.php
+++ packages/Webkul/Bulkupload/src/Database/Migrations/2019_04_29_105529_create_import_new_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateImportNewProductsByAdminTable extends Migration
+class CreateImportNewProductsTable extends Migration
 {
     /**
      * Run the migrations.
