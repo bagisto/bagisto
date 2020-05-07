54b535f8f41e467d9a496246a03b8f1378cf972a
Jan Bickel <jan.bickel@kmso.de>
2020-05-07 14:48:41 +0200

    customer german translation
    # Conflicts:
    #	packages/Webkul/Customer/src/Resources/lang/de/app.php

--- packages/Webkul/Customer/src/Resources/lang/de/app.php
+++ packages/Webkul/Customer/src/Resources/lang/de/app.php
@@ -2,19 +2,19 @@
 
 return [
     'wishlist' => [
-        'success' => 'Produkt erfolgreich zur Wunschliste hinzugefügt',
-        'failure' => 'Produkt kann nicht zur Wunschliste hinzugefügt werden',
-        'already' => 'Produkt befindet sich bereits in der Wunschliste',
-        'removed' => 'Produkt erfolgreich von der Wunschliste entfernt',
-        'remove-fail' => 'Produkt kann nicht von der Wunschliste entfernt werden',
-        'empty' => 'Sie haben noch keine Produkte in Ihrer Wunschliste',
-        'select-options' => 'Es müssen Optionen ausgewählt werden, bevor das Produkt zur Wunschliste hinzugefügt werden kann',
-        'remove-all-success' => 'Alle Produkte wurden von der Wunschliste entfernt',
+        'success' => 'Artikel erfolgreich zur Wunschliste hinzugefügt',
+        'failure' => 'Artikel kann nicht zur Wunschliste hinzugefügt werden',
+        'already' => 'Artikel bereits in Ihrer Wunschliste vorhanden',
+        'removed' => 'Artikel erfolgreich von der Wunschliste entfernt',
+        'remove-fail' => 'Artikel kann nicht von der Wunschliste entfernt werden',
+        'empty' => 'Sie haben keine Artikel auf Ihrer Wunschliste',
+        'select-options' => 'Vor dem Hinzufügen zur Wunschliste müssen Optionen ausgewählt werden',
+        'remove-all-success' => 'Alle Elemente von Ihrer Wunschliste wurden entfernt',
     ],
     'reviews' => [
         'empty' => 'Sie haben noch kein Produkt bewertet'
     ],
     'forget_password' => [
-        'reset_link_sent' => 'Wir haben ihnen den Link zum Zurücksetzen Ihres Passworts per E-Mail zugeschickt.'
+        'reset_link_sent' => 'Wir haben Ihren Link zum Zurücksetzen des Passworts per E-Mail gesendet.'
     ]
 ];
