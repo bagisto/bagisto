9229e361752ad11831de40a474f6aa5736e9fc20
ottopodo <ottopodo@gmail.com>
2020-05-11 19:19:28 +0200

    Italian Translations

---
+++ packages/Webkul/Core/src/Resources/lang/it/validation.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'slug' => ':attribute deve essere uno slug valido.',
+    'code' => ':attribute deve essere valido.',
+    'decimal' => ':attribute deve essere valido.'
+];
---
+++ packages/Webkul/Customer/src/Resources/lang/it/app.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'wishlist' => [
+        'success' => 'Prodotto aggiunto ai Preferiti',
+        'failure' => 'Il Prodotto non può essere aggiunto ai Preferiti',
+        'already' => 'Prodotto già presente nei tuoi Preferiti',
+        'removed' => 'Prodotto rimosso con successo dai Preferiti',
+        'remove-fail' => 'Non è stato possibile rimuovere il prodotto dai Preferiti',
+        'empty' => 'Non hai prodotti nei tuoi Preferiti',
+        'select-options' => 'Seleziona una opzione del prodotto prima di aggiungerlo ai Preferiti',
+        'remove-all-success' => 'Tutti i Prodotti presenti nei preferiti sono stati rimossi',
+    ],
+    'reviews' => [
+        'empty' => 'Non hai ancora recensito i nostri prodotti'
+    ],
+	'forget_password' => [
+        'reset_link_sent' => 'Ti abbiamo inviato una email per generare una nuova password.'
+    ]
+
+];
