18e4af0356bfc8e0aa5c3e98e4504c68901bc76c
rahulshukla-home <rahulcs0082@gmail.com>
2020-05-12 14:23:41 +0530

    Issue #3047

--- packages/Webkul/Product/src/Type/Configurable.php
+++ packages/Webkul/Product/src/Type/Configurable.php
@@ -573,6 +573,8 @@ class Configurable extends AbstractType
     public function haveSufficientQuantity($qty)
     {
         $backorders = core()->getConfigData('catalog.inventory.stock_options.backorders');
+
+        $backorders = ! is_null ($backorders) ? $backorders : false;
      
         foreach ($this->product->variants as $variant) {
             if ($variant->haveSufficientQuantity($qty)) {
@@ -597,5 +599,36 @@ class Configurable extends AbstractType
         }
             
         return false;
+    }
+
+    /**
+     * @return int
+     */
+    public function totalQuantity()
+    {
+        $total = 0;
+
+        $channelInventorySourceIds = core()->getCurrentChannel()
+                                           ->inventory_sources()
+                                           ->where('status', 1)
+                                           ->pluck('id');
+
+        foreach ($this->product->variants as $variant) {
+            foreach ($variant->inventories as $inventory) {
+                if (is_numeric($index = $channelInventorySourceIds->search($inventory->inventory_source_id))) {
+                    $total += $inventory->qty;
+                }
+            }
+
+            $orderedInventory = $variant->ordered_inventories()
+                                          ->where('channel_id', core()->getCurrentChannel()->id)
+                                          ->first();
+
+            if ($orderedInventory) {
+                $total -= $orderedInventory->qty;
+            }
+        }
+
+        return $total;
     }
 }
