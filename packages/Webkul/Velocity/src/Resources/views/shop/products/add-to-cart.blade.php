502a0b1ac8f82d0691bb832a91c094e0712b9832
shivam kumar <shivam.kumar174@webkul.com>
2020-05-14 13:42:20 +0530

    added #2729 for velocity

--- packages/Webkul/Velocity/src/Resources/views/shop/products/add-to-cart.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/products/add-to-cart.blade.php
@@ -69,7 +69,7 @@
                     add-class-to-btn="{{ $addToCartBtnClass ?? '' }}"
                     is-enable={{ ! $product->isSaleable() ? 'false' : 'true' }}
                     show-cart-icon={{ !(isset($showCartIcon) && !$showCartIcon) }}
-                    btn-text="{{ $btnText ?? __('shop::app.products.add-to-cart') }}">
+                    btn-text="{{ ($product->type == 'booking') ? $btnText ?? __('shop::app.products.book-now') : $btnText ?? __('shop::app.products.add-to-cart') }}">
                 </add-to-cart>
             @endif
         </div>
