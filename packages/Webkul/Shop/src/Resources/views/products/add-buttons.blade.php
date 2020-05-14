cb1fdadd63e3bf5198cf89ac00df2bb518fe5b3b
shivam kumar <shivam.kumar174@webkul.com>
2020-05-14 11:34:01 +0530

    #2729 resolved

--- packages/Webkul/Shop/src/Resources/views/products/add-buttons.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/add-buttons.blade.php
@@ -3,7 +3,13 @@
         @csrf
         <input type="hidden" name="product_id" value="{{ $product->product_id }}">
         <input type="hidden" name="quantity" value="1">
-        <button class="btn btn-lg btn-primary addtocart" {{ $product->isSaleable() ? '' : 'disabled' }}>{{ __('shop::app.products.add-to-cart') }}</button>
+        <button class="btn btn-lg btn-primary addtocart" {{ $product->isSaleable() ? '' : 'disabled' }}>
+            @if($product->type == 'booking')
+                {{ __('shop::app.products.book-now') }}
+            @else
+                {{ __('shop::app.products.add-to-cart') }}
+            @endif
+        </button>
     </form>
 
     @include('shop::products.wishlist')
