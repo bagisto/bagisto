e189a4fa80182c437ab6814dde2534631e2fd44c
Matt April <matthew.japril@gmail.com>
2020-05-29 12:26:13 -0400

    Added haveSufficientQuantity() method for Bundle - checks that all required options have at least one product in stock.

--- packages/Webkul/Product/src/Type/Bundle.php
+++ packages/Webkul/Product/src/Type/Bundle.php
@@ -652,4 +652,27 @@ class Bundle extends AbstractType
 
         return $options;
     }
+
+    /**
+     * @param  int  $qty
+     * @return bool
+     */
+    public function haveSufficientQuantity($qty)
+    {
+        # to consider a bundle in stock we need to check that at least one product from each required group is available for the given quantity
+        foreach ($this->product->bundle_options as $option) {
+            if($option->is_required) {
+                foreach ($option->bundle_option_products as $bundleOptionProduct) {
+                    # as long as at least one product in the required group is available we can continue checking other groups
+                    if($bundleOptionProduct->product->haveSufficientQuantity($bundleOptionProduct->qty * $qty)) {
+                        continue 2;
+                    }
+                }
+                # if any required option does not have any in-stock product option we will get here.
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
