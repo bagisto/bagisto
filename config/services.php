66a11e3a4d635246f962c633f97d2605fb1749c6
Prashant Singh <prashantsingh2408@gmail.com>
2020-05-30 19:09:56 +0530

    adding credentials
    For implementing social login feature.

--- config/services.php
+++ config/services.php
@@ -13,7 +13,13 @@ return [
     | to have a conventional place to find your various credentials.
     |
     */
-
+     
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT')
+    ],
+    
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
