68e4dc80b358716f4d0efb74aa2f5e05f4c06f6a
Prashant Singh <prashantsingh2408@gmail.com>
2020-06-02 21:23:02 +0530

    Update app.php

--- config/app.php
+++ config/app.php
@@ -212,7 +212,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Laravel\Socialite\SocialiteServiceProvider::class,
 
         /*
          * Package Service Providers...
