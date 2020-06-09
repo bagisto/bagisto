5b398574aec26d596aa5f5a1d70908ddfaaea70e
Pranshu Tomar <pranshutomar@Pranshus-MacBook-Air.local>
2020-06-09 12:54:46 +0530

    Migrate to laravel 7

--- config/session.php
+++ config/session.php
@@ -164,7 +164,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', false),
+    'secure' => env('SESSION_SECURE_COOKIE', null),
 
     /*
     |--------------------------------------------------------------------------
