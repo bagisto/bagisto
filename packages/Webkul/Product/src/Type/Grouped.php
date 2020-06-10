4049c2c5507f696a154c20e0bee40848ef4b637b
Shubham Mehrotra <shubh0896m@gmail.com>
2020-06-10 13:01:04 +0530

    #3202

--- packages/Webkul/Product/src/Type/Grouped.php
+++ packages/Webkul/Product/src/Type/Grouped.php
@@ -124,7 +124,7 @@ class Grouped extends AbstractType
      *
      * @return float
      */
-    public function getMinimalPrice()
+    public function getMinimalPrice($qty = null)
     {
         $minPrices = [];
 
