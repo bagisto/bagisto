191f3c0d8f0766ce28b11f5bcf276304a253ed52
Shubham Mehrotra <shubh0896m@gmail.com>
2020-06-11 15:02:39 +0530

    #2971

--- packages/Webkul/Velocity/src/Helpers/Helper.php
+++ packages/Webkul/Velocity/src/Helpers/Helper.php
@@ -214,14 +214,16 @@ class Helper extends Review
      *
      * @return array
      */
-    public function getVelocityMetaData()
+    public function getVelocityMetaData($locale = null)
     {
-        try {
-            $metaData = $this->velocityMetadataRepository->get();
+        if (! $locale) {
+            $locale = request()->get('locale') ?: app()->getLocale();
+        }
 
-            if (! ($metaData && isset($metaData[0]) && $metaData = $metaData[0])) {
-                $metaData = null;
-            }
+        try {
+            $metaData = $this->velocityMetadataRepository->findOneWhere([
+                'locale' => $locale
+            ]);
 
             return $metaData;
         } catch (\Exception $exception) {
@@ -287,6 +289,7 @@ class Helper extends Review
      * @param  \Webkul\Product\Contracts\Product  $product
      * @param  bool                               $list
      * @param  array                              $metaInformation
+     * 
      * @return array
      */
     public function formatProduct($product, $list = false, $metaInformation = [])
