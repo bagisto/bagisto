3ba83ace265d796b2f8d6200748a165e11fc04e7
Annika Wolff <annika@galaxis-euphoria.de>
2020-06-11 14:23:34 +0200

    prevent flickering, use blisss theme

--- packages/Webkul/BookingProduct/src/Database/Factories/BookingProductEventTicketFactory.php
+++ packages/Webkul/BookingProduct/src/Database/Factories/BookingProductEventTicketFactory.php
@@ -2,16 +2,14 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use Carbon\Carbon;
 use Faker\Generator as Faker;
 use Webkul\BookingProduct\Models\BookingProduct;
 use Webkul\BookingProduct\Models\BookingProductEventTicket;
-use Webkul\Product\Models\Product;
 
-$factory->define(BookingProductEventTicket::class, function (Faker $faker, array $attributes) {
+$factory->define(BookingProductEventTicket::class, static function (Faker $faker, array $attributes) {
     return [
         'price'              => $faker->randomFloat(4, 3, 900),
-        'qty'                => $faker->randomNumber(2),
+        'qty'                => $faker->numberBetween(100, 1000),
         'booking_product_id' => static function () {
             return factory(BookingProduct::class)->create(['type' => 'event'])->id;
         }
