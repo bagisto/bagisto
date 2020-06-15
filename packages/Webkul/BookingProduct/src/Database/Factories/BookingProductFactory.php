a388d0eed9d499f6f2c987c0038453ea88786334
Herbert Maschke <thyseus@pm.me>
2020-06-15 11:56:38 +0200

    format factories

--- packages/Webkul/BookingProduct/src/Database/Factories/BookingProductFactory.php
+++ packages/Webkul/BookingProduct/src/Database/Factories/BookingProductFactory.php
@@ -9,12 +9,12 @@ use Webkul\Product\Models\Product;
 
 $factory->define(BookingProduct::class, function (Faker $faker, array $attributes) {
     return [
-        'type' => array_rand(['event']),
-        'qty' => $faker->randomNumber(2),
+        'type'           => array_rand(['event']),
+        'qty'            => $faker->randomNumber(2),
         'available_from' => Carbon::yesterday(),
-        'available_to' => Carbon::tomorrow(),
-        'product_id' => function () {
+        'available_to'   => Carbon::tomorrow(),
+        'product_id'     => function () {
             return factory(Product::class)->create(['type' => 'booking'])->id;
-        }
+        },
     ];
 });
