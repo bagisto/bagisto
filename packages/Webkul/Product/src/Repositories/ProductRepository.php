9c1d9087670d24eaa0e6ca4de47d585974b4df8f
Devansh <devansh.bawari419@webkul.com>
2020-07-22 11:44:02 +0530

    per page zero issue fixed

--- packages/Webkul/Product/src/Repositories/ProductRepository.php
+++ packages/Webkul/Product/src/Repositories/ProductRepository.php
@@ -111,7 +111,7 @@ class ProductRepository extends Repository
         if (core()->getConfigData('catalog.products.storefront.products_per_page')) {
             $pages = explode(',', core()->getConfigData('catalog.products.storefront.products_per_page'));
 
-            $perPage = isset($params['limit']) ? $params['limit'] : current($pages);
+            $perPage = isset($params['limit']) ? (!empty($params['limit']) ? $params['limit'] : 9) : current($pages);
         } else {
             $perPage = isset($params['limit']) && !empty($params['limit']) ? $params['limit'] : 9;
         }
