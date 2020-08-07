96e6c56375b44e841b4d41f2d88221b40b4e4cbd
Herbert Maschke <thyseus@pm.me>
2020-08-07 21:48:13 +0200

    doc improvements

--- config/products.php
+++ config/products.php
@@ -2,10 +2,10 @@
 
 return [
     // use the 'code' of the 'attributes' table here to be able to control which attributes
-    // should be skipped when doing a copy (admin->catalog->products->copy product)
-    // defaults to none (which means everything is copied)
+    // should be skipped when doing a copy (admin->catalog->products->copy product).
+    // you can also add every relation that should not be copied here to skip them.
+    // defaults to none (which means everything is copied).
     'skipAttributesOnCopy' => [
 
     ],
-
 ];
