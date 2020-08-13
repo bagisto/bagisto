5e7474d307b4fdd80d25c8485f1f3d15ebf45ed0
Devansh <devansh.bawari419@webkul.com>
2020-08-13 15:59:32 +0530

    Migration Issue Fixed

--- packages/Webkul/Core/src/Database/Migrations/2020_08_12_114128_removing_foriegn_key.php
+++ packages/Webkul/Core/src/Database/Migrations/2020_08_12_114128_removing_foriegn_key.php
@@ -18,26 +18,10 @@ class RemovingForiegnKey extends Migration
     {
         Schema::table('invoices', static function (Blueprint $table) {
             $table->dropForeign(['order_address_id']);
-
-            OrderAddress::query()
-                ->orderBy('id', 'asc') // for some reason each() needs an orderBy in before
-                ->each(static function ($row) {
-                    Invoice::query()
-                        ->where('order_address_id', $row->additional['old_order_address_id'])
-                        ->update(['order_address_id' => $row->id]);
-                });
         });
 
         Schema::table('shipments', static function (Blueprint $table) {
             $table->dropForeign(['order_address_id']);
-
-            OrderAddress::query()
-                ->orderBy('id', 'asc') // for some reason each() needs an orderBy in before
-                ->each(static function ($row) {
-                    Shipment::query()
-                        ->where('order_address_id', $row->additional['old_order_address_id'])
-                        ->update(['order_address_id' => $row->id]);
-                });
         });
     }
 
