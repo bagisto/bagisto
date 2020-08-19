ce1b92370ee210896c401039fd70610f9c79efc6
Akhtar Khan <akhtar.khan671@webkul.com>
2020-08-19 16:45:11 +0530

    fixed

--- packages/Webkul/Velocity/src/Repositories/ContentRepository.php
+++ packages/Webkul/Velocity/src/Repositories/ContentRepository.php
@@ -130,7 +130,7 @@ class ContentRepository extends Repository
         $query = $this->model::orderBy('position', 'ASC');
 
         $velocityMetaData = app('Webkul\Velocity\Helpers\Helper')->getVelocityMetaData();
-        $headerContentCount = $velocityMetaData['header_content_count'];
+        $headerContentCount = $velocityMetaData->header_content_count;
 
         $headerContentCount = $headerContentCount != '' ? $headerContentCount : 5;
 
