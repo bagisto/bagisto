47b0dc30c9b97f8cca7bb1381a1ae8393ae56459
gusaindeepak54 <gusaindeepak54@gmail.com>
2020-08-20 12:05:30 +0530

    Update Fields of Meta data Info

--- packages/Webkul/Velocity/src/Resources/views/admin/meta-info/meta-data.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/admin/meta-info/meta-data.blade.php
@@ -85,7 +85,8 @@
                         <label>{{ __('velocity::app.admin.meta-data.sidebar-categories') }}</label>
 
                         <input
-                            type="text"
+                            type="number"
+                            min="0"
                             class="control"
                             id="sidebar_category_count"
                             name="sidebar_category_count"
@@ -108,7 +109,8 @@
                         <label>{{ __('shop::app.home.featured-products') }}</label>
 
                         <input
-                            type="text"
+                            type="number"
+                            min="0"
                             class="control"
                             id="featured_product_count"
                             name="featured_product_count"
@@ -119,7 +121,8 @@
                         <label>{{ __('shop::app.home.new-products') }}</label>
 
                         <input
-                            type="text"
+                            type="number"
+                            min="0"
                             class="control"
                             id="new_products_count"
                             name="new_products_count"
