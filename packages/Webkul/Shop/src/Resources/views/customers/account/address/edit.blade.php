6694acd41202e4d05e18d81e83988cf358200c27
Devansh <devansh.bawari419@webkul.com>
2020-09-03 14:39:21 +0530

    Added Previous URL Route To File

--- packages/Webkul/Shop/src/Resources/views/customers/account/address/edit.blade.php
+++ packages/Webkul/Shop/src/Resources/views/customers/account/address/edit.blade.php
@@ -12,7 +12,7 @@
         <div class="account-layout">
 
             <div class="account-head mb-15">
-                <span class="back-icon"><a href="{{ route('customer.account.index') }}"><i class="icon icon-menu-back"></i></a></span>
+                <span class="back-icon"><a href="{{ url()->previous() }}"><i class="icon icon-menu-back"></i></a></span>
                 <span class="account-heading">{{ __('shop::app.customer.account.address.edit.title') }}</span>
                 <span></span>
             </div>
--- packages/Webkul/Shop/src/Resources/views/customers/account/downloadable_products/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/customers/account/downloadable_products/index.blade.php
@@ -12,7 +12,7 @@
         <div class="account-layout">
 
             <div class="account-head mb-10">
-                <span class="back-icon"><a href="{{ route('customer.account.index') }}"><i class="icon icon-menu-back"></i></a></span>
+                <span class="back-icon"><a href="{{ url()->previous() }}"><i class="icon icon-menu-back"></i></a></span>
                 <span class="account-heading">
                     {{ __('shop::app.customer.account.downloadable_products.title') }}
                 </span>
@@ -26,7 +26,7 @@
                 <div class="account-table-content">
 
                     {!! app('Webkul\Shop\DataGrids\DownloadableProductDataGrid')->render() !!}
-                    
+
                 </div>
             </div>
 
