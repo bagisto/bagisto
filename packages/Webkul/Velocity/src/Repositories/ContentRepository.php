bba79fc0c127ad1222c61ea487240d94d72ab46b
gusaindeepak54 <gusaindeepak54@gmail.com>
2020-09-10 15:54:10 +0530

    resolved conflicts

--- packages/Webkul/Velocity/src/Repositories/ContentRepository.php
+++ packages/Webkul/Velocity/src/Repositories/ContentRepository.php
@@ -130,9 +130,8 @@ class ContentRepository extends Repository
         $query = $this->model::orderBy('position', 'ASC');
 
         $velocityMetaData = app('Webkul\Velocity\Helpers\Helper')->getVelocityMetaData();
-        
         $headerContentCount = $velocityMetaData->header_content_count;
-       
+
         $headerContentCount = $headerContentCount != '' ? $headerContentCount : 5;
 
         $contentCollection = $query
