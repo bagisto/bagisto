286404d2c3ca0df6d2a94d8bbf1b82aed1a4c202
gusaindeepak54 <gusaindeepak54@gmail.com>
2020-09-15 17:26:49 +0530

    main-category working

--- packages/Webkul/Velocity/src/Resources/views/shop/UI/header.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/UI/header.blade.php
@@ -369,7 +369,7 @@
         </div>
 
         <div
-            v-else
+            id="main-category"
             @mouseout="toggleSidebar('0', $event, 'mouseout')"
             @mouseover="toggleSidebar('0', $event, 'mouseover')"
             :class="`main-category fs16 unselectable fw6 ${($root.sharedRootCategories.length > 0) ? 'cursor-pointer' : 'cursor-not-allowed'} left`">
