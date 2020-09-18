7eff946013d8343a440de51970db18906e563b81
gusaindeepak54 <gusaindeepak54@gmail.com>
2020-09-18 16:25:17 +0530

    Update Shop Theme

--- packages/Webkul/Shop/src/Resources/views/home/new-products.blade.php
+++ packages/Webkul/Shop/src/Resources/views/home/new-products.blade.php
@@ -1,4 +1,4 @@
-@if (app('Webkul\Product\Repositories\ProductRepository')->getNewProducts()->count())
+@if (app('Webkul\Product\Repositories\ProductRepository')->getNewProducts())
     <section class="featured-products">
 
         <div class="featured-heading">
