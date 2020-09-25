39537c3cd3b4ddb7f00ae333bbc1c7c060701c1b
HUNG HUYNH BA MANH <hunghbm@greenglobal.vn>
2020-09-25 16:20:55 +0700

    Feat (Back Button): Update back button in Admin interface BUGS: - Fix flash notification reappears when clicking back button see #3968

--- packages/Webkul/Admin/src/Resources/views/catalog/products/edit.blade.php
+++ packages/Webkul/Admin/src/Resources/views/catalog/products/edit.blade.php
@@ -18,7 +18,7 @@
                 <div class="page-title">
                     <h1>
                         <i class="icon angle-left-icon back-link"
-                           onclick="history.length > 1 ? history.go(-1) : window.location = '{{ route('admin.dashboard.index') }}';"></i>
+                           onclick="window.location = history.length > 1 ? document.referrer : '{{ route('admin.dashboard.index') }}'"></i>
 
                         {{ __('admin::app.catalog.products.edit-title') }}
                     </h1>
