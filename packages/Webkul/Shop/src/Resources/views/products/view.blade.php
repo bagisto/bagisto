08b968ec26b2024803b470b7fb751ed5d923d032
JunaidJaved8461 <72268940+JunaidJaved8461@users.noreply.github.com>
2020-10-06 16:35:15 +0500

    Issue Fixed #3945
    Showing some html code while sharing product on facebook #3945 is fixed now

--- packages/Webkul/Shop/src/Resources/views/products/view.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/view.blade.php
@@ -1,13 +1,13 @@
 @extends('shop::layouts.master')
 
 @section('page_title')
-    {{ trim($product->meta_title) != "" ? $product->meta_title : $product->name }}
+    {!! trim($product->meta_title) != "" ? $product->meta_title : $product->name !!}
 @stop
 
 @section('seo')
     <meta name="description" content="{{ trim($product->meta_description) != "" ? $product->meta_description : \Illuminate\Support\Str::limit(strip_tags($product->description), 120, '') }}"/>
 
-    <meta name="keywords" content="{{ $product->meta_keywords }}"/>
+    <meta name="keywords" content="{!! $product->meta_keywords !!}"/>
 
     @if (core()->getConfigData('catalog.rich_snippets.products.enable'))
         <script type="application/ld+json">
