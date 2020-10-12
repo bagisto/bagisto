65d215ee168d578625f0fcc548e47cd867efd781
gusaindeepak54 <gusaindeepak54@gmail.com>
2020-10-12 16:50:55 +0530

    add image_search varibale

--- packages/Webkul/Shop/src/Resources/views/layouts/header/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/layouts/header/index.blade.php
@@ -1,5 +1,6 @@
 <?php
     $term = request()->input('term');
+    $image_search = request()->input('image-search');
 
     if (! is_null($term)) {
         $serachQuery = 'term='.request()->input('term');
