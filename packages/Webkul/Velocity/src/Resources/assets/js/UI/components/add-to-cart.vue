089e3d1f5b78a359145d7af4d7edaedd40678a8b
devansh bawari <devansh.bawari419@webkul.com>
2020-10-27 14:10:46 +0530

    Reduced font size and added pre wrap

--- packages/Webkul/Velocity/src/Resources/assets/js/UI/components/add-to-cart.vue
+++ packages/Webkul/Velocity/src/Resources/assets/js/UI/components/add-to-cart.vue
@@ -1,6 +1,6 @@
 <template>
     <form method="POST" @submit.prevent="addToCart">
-        
+
         <!-- for move to cart from wishlist -->
         <a
             :href="wishlistMoveRoute"
@@ -11,7 +11,7 @@
 
             <i class="material-icons text-down-3" v-if="showCartIcon">shopping_cart</i>
 
-            <span class="fs14 fw6 text-uppercase text-up-4" v-text="btnText"></span>
+            <span class="fs10 fw6 text-uppercase text-up-4" v-text="btnText"></span>
         </a>
 
         <!-- for add to cart -->
@@ -24,7 +24,7 @@
 
             <i class="material-icons text-down-3" v-if="showCartIcon">shopping_cart</i>
 
-            <span class="fs14 fw6 text-uppercase text-up-4" v-text="btnText"></span>
+            <span class="fs10 fw6 text-uppercase text-up-4" v-text="btnText"></span>
         </button>
 
     </form>
