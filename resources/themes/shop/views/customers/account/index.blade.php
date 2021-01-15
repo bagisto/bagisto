98d87387444caf7364d673e2c69b58e6b5c457c3
rahul shukla <rahulshukla.symfony517@webkul.com>
2021-01-15 19:03:39 +0530

    enhancement #4468 added for velocity theme

--- resources/themes/shop/views/customers/account/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('shop::layouts.master')
-
-@section('content-wrapper')
-    <div class="account-content">
-        @include('shop::customers.account.partials.sidemenu')
-        <h1>Account Index Page</h1>
-    </div>
-    
-@endsection
--- resources/themes/shop/views/home/slider.blade.php
+++
@@ -1,5 +0,0 @@
-@if (count($sliderData))
-    <section class="slider-block" style="height: 500px">
-        <image-slider :slides='@json($sliderData)' public_path="{{ url()->to('/') }}"></image-slider>
-    </section>
-@endif
