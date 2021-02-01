c9d09731e1446629b0ffba75b484b3f67d982295
rahul shukla <rahulshukla.symfony517@webkul.com>
2021-02-01 16:40:13 +0530

    chnage according to comment

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,8 +25,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('booking:cron')->dailyAt('3:00');
-
-        $schedule->command('campaign:process')->daily();
     }
 
     /**
@@ -38,7 +36,6 @@ class Kernel extends ConsoleKernel
     {
         $this->load(__DIR__.'/Commands');
         $this->load(__DIR__.'/../../packages/Webkul/Core/src/Console/Commands');
-        $this->load(__DIR__.'/../../packages/Webkul/Marketing/src/Console/Commands');
 
         require base_path('routes/console.php');
     }
