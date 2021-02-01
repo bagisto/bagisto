cd26f9915353a4b27f37a4136cc1c4bc07187884
devansh bawari <devansh.bawari419@webkul.com>
2021-02-01 11:35:56 +0530

    Extra Info Added

--- packages/Webkul/API/Http/Resources/Catalog/Product.php
+++ packages/Webkul/API/Http/Resources/Catalog/Product.php
@@ -50,9 +50,22 @@ class Product extends JsonResource
             'description'            => $product->description,
             'images'                 => ProductImage::collection($product->images),
             'base_image'             => $this->productImageHelper->getProductBaseImage($product),
-            'variants'               => Self::collection($product->variants),
             'created_at'             => $product->created_at,
             'updated_at'             => $product->updated_at,
+
+            /* child informations */
+            'grouped_products'       => $this->when(
+                $productTypeInstance instanceof \Webkul\Product\Type\Grouped,
+                $product->grouped_products
+            ),
+            'bundle_options'        => $this->when(
+                $productTypeInstance instanceof \Webkul\Product\Type\Bundle,
+                $product->bundle_options
+            ),
+            'variants'               => $this->when(
+                $productTypeInstance instanceof \Webkul\Product\Type\Configurable,
+                $product->variants
+            ),
 
             /* super attributes */
             $this->mergeWhen($productTypeInstance->isComposite(), [
