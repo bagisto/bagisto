51cb46a13bcaa036705b3dc21a56e52c92920294
rahul shukla <rahulshukla.symfony517@webkul.com>
2021-03-10 15:16:50 +0530

    Issue #4702 fixed

--- packages/Webkul/Admin/src/Resources/views/export/temp.blade.php
+++ packages/Webkul/Admin/src/Resources/views/export/temp.blade.php
@@ -13,6 +13,10 @@
         @foreach ($records as $record)
             <tr>
                 @foreach($record as $column => $value)
+                    @php
+                        $value = preg_replace('/[^A-Za-z0-9@#$%^&*()_+!\-]/', '', $value);
+                    @endphp
+
                     <td>{{ $value }} </td>
                 @endforeach
             </tr>
