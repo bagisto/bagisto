46108735b88fefe48df2947ea00f1ab603beac8b
devansh bawari <devansh.bawari419@webkul.com>
2021-03-26 18:27:06 +0530

    Redis Cache And Config Fixed

--- config/database.php
+++ config/database.php
@@ -115,6 +115,13 @@ return [
             'database' => 0,
         ],
 
+        'session' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 1,
+        ],
+
     ],
 
 ];
