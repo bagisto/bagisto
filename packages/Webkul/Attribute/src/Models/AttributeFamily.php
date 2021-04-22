cd87356355ee66edc038ea222187b977ae044ec8
rahul shukla <rahulshukla.symfony517@webkul.com>
2021-04-22 16:52:19 +0530

    Issue #4813 fixed

--- packages/Webkul/Attribute/src/Models/AttributeFamily.php
+++ packages/Webkul/Attribute/src/Models/AttributeFamily.php
@@ -31,7 +31,7 @@ class AttributeFamily extends Model implements AttributeFamilyContract
     public function getComparableAttributesBelongsToFamily()
     {
         return (AttributeProxy::modelClass())::join('attribute_group_mappings', 'attribute_group_mappings.attribute_id', '=', 'attributes.id')
-            ->select('attributes.*')->where('attributes.is_comparable', 1)->get();
+            ->select('attributes.*')->where('attributes.is_comparable', 1)->distinct()->get();
     }
 
     /**
