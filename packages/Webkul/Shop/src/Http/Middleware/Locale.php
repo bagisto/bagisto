d0adc7ab014f34056c928424eddefb9b6c2f3382
devansh bawari <devansh.bawari419@webkul.com>
2021-06-14 17:10:06 +0530

    Middleware Fallback Removed

--- packages/Webkul/Shop/src/Http/Middleware/Locale.php
+++ packages/Webkul/Shop/src/Http/Middleware/Locale.php
@@ -29,7 +29,7 @@ class Locale
     */
     public function handle($request, Closure $next)
     {
-        $locale = core()->getRequestedLocaleCode();
+        $locale = core()->getRequestedLocaleCode('locale', false);
 
         if ($locale) {
             if ($this->locale->findOneByField('code', $locale)) {
