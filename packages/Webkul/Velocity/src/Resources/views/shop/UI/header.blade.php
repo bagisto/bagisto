b0bd3a30a901409666f29bf623a22d83b7bef1dd
devansh bawari <devansh.bawari419@webkul.com>
2021-07-29 15:10:46 +0530

    Search Bar Component Breaked And Minified

--- packages/Webkul/Velocity/src/Resources/views/shop/UI/header.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/UI/header.blade.php
@@ -389,7 +389,39 @@
                 </a>
             </div>
 
-            <searchbar-component v-if="isSearchbar"></searchbar-component>
+            <div class="right searchbar" v-if="isSearchbar">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="input-group">
+                            <form
+                                method="GET"
+                                role="search"
+                                id="search-form"
+                                action="{{ route('velocity.search.index') }}">
+
+                                <div
+                                    class="btn-toolbar full-width search-form"
+                                    role="toolbar">
+
+                                    <searchbar-component>
+                                        <template v-slot:image-search>
+                                            <image-search-component
+                                                status="{{core()->getConfigData('general.content.shop.image_search') == '1' ? 'true' : 'false'}}"
+                                                upload-src="{{ route('shop.image.search.upload') }}"
+                                                view-src="{{ route('shop.search.index') }}"
+                                                common-error="{{ __('shop::app.common.error') }}"
+                                                size-limit-error="{{ __('shop::app.common.image-upload-limit') }}">
+                                            </image-search-component>
+                                        </template>
+                                    </searchbar-component>
+
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </script>
 
--- packages/Webkul/Velocity/src/Resources/views/shop/layouts/header/index.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/layouts/header/index.blade.php
@@ -4,7 +4,84 @@
             <img class="logo" src="{{ core()->getCurrentChannel()->logo_url ?? asset('themes/velocity/assets/images/logo-text.png') }}" alt="" />
         </a>
 
-        <searchbar-component></searchbar-component>
+        <div class="right searchbar">
+            <div class="row">
+                <div class="col-lg-5 col-md-12">
+                    <div class="input-group">
+                        <form
+                            method="GET"
+                            role="search"
+                            id="search-form"
+                            action="{{ route('velocity.search.index') }}">
+
+                            <div
+                                class="btn-toolbar full-width"
+                                role="toolbar">
+
+                                <searchbar-component>
+                                    <template v-slot:image-search>
+                                        <image-search-component
+                                            status="{{core()->getConfigData('general.content.shop.image_search') == '1' ? 'true' : 'false'}}"
+                                            upload-src="{{ route('shop.image.search.upload') }}"
+                                            view-src="{{ route('shop.search.index') }}"
+                                            common-error="{{ __('shop::app.common.error') }}"
+                                            size-limit-error="{{ __('shop::app.common.image-upload-limit') }}">
+                                        </image-search-component>
+                                    </template>
+                                </searchbar-component>
+
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+
+                <div class="col-lg-7 col-md-12 vc-full-screen">
+                    <div class="left-wrapper">
+
+                        {!! view_render_event('bagisto.shop.layout.header.wishlist.before') !!}
+
+                            @php
+                                $showWishlist = core()->getConfigData('general.content.shop.wishlist_option') == "1" ? true : false;
+                            @endphp
+
+                            @if($showWishlist)
+
+                                <wishlist-component-with-badge
+                                    is-customer="{{ auth()->guard('customer')->check() ? 'true' : 'false' }}"
+                                    src="{{ route('customer.wishlist.index') }}">
+                                </wishlist-component-with-badge>
+
+                            @endif
+
+                        {!! view_render_event('bagisto.shop.layout.header.wishlist.after') !!}
+
+                        {!! view_render_event('bagisto.shop.layout.header.compare.before') !!}
+
+                            @php
+                                $showCompare = core()->getConfigData('general.content.shop.compare_option') == "1" ? true : false;
+                            @endphp
+
+                            @if ($showCompare)
+
+                                <compare-component-with-badge
+                                    is-customer="{{ auth()->guard('customer')->check() ? 'true' : 'false' }}"
+                                    src="{{ auth()->guard('customer')->check() ? route('velocity.customer.product.compare') : route('velocity.product.compare') }}">
+                                </compare-component-with-badge>
+
+                            @endif
+
+                        {!! view_render_event('bagisto.shop.layout.header.compare.after') !!}
+
+                        {!! view_render_event('bagisto.shop.layout.header.cart-item.before') !!}
+
+                            @include('shop::checkout.cart.mini-cart')
+
+                        {!! view_render_event('bagisto.shop.layout.header.cart-item.after') !!}
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </header>
 
@@ -14,7 +91,7 @@
             document.addEventListener('scroll', e => {
                 scrollPosition = Math.round(window.scrollY);
 
-                if (scrollPosition > 50){
+                if (scrollPosition > 50) {
                     document.querySelector('header').classList.add('header-shadow');
                 } else {
                     document.querySelector('header').classList.remove('header-shadow');
