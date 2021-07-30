0f0e49e765dfb658306580bd042d68a42e09d090
devansh bawari <devansh.bawari419@webkul.com>
2021-07-30 19:46:47 +0530

    Default Slider First Image Added

--- packages/Webkul/Velocity/src/Resources/views/shop/home/slider.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/home/slider.blade.php
@@ -6,7 +6,11 @@
             :banners="{{ json_encode($sliderData) }}">
 
             {{-- this is default content if js is not loaded --}}
-            <img class="col-12 no-padding banner-icon" src="{{ asset('/themes/velocity/assets/images/banner.webp') }}" alt=""/>
+            @if(! empty($sliderData))
+                <img class="col-12 no-padding banner-icon" src="{{ asset('/storage/' . $sliderData[0]['path']) }}" alt=""/>
+            @else
+                <img class="col-12 no-padding banner-icon" src="{{ asset('/themes/velocity/assets/images/banner.webp') }}" alt=""/>
+            @endif
 
         </slider-component>
     </div>
