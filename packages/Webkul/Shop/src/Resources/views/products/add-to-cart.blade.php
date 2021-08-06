83e8e4c66f1106f2b55d624a633068b5df79da07
devansh bawari <devansh.bawari419@webkul.com>
2021-08-06 09:52:45 +0530

    Add To Cart Button Issue Fixed On Compare Page

--- packages/Webkul/Shop/src/Resources/views/products/add-to-cart.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/add-to-cart.blade.php
@@ -4,9 +4,17 @@
     $width = (core()->getConfigData('catalog.products.storefront.buy_now_button_display') == 1) ? '49' : '95';
 @endphp
 
-<button type="submit" class="btn btn-lg btn-primary addtocart" {{ ! $product->isSaleable() ? 'disabled' : '' }}
-style="width: <?php echo $width.'%';?>;">
-    {{ ($product->type == 'booking') ?  __('shop::app.products.book-now') :  __('shop::app.products.add-to-cart') }}
-</button>
+<form action="{{ route('cart.add', $product->product_id) }}" method="POST">
+    @csrf
+
+    <input type="hidden" name="product_id" value="{{ $product->product_id }}">
+
+    <input type="hidden" name="quantity" value="1">
+
+    <button type="submit" class="btn btn-lg btn-primary addtocart" {{ ! $product->isSaleable() ? 'disabled' : '' }}
+    style="width: {{ $width . '%' }};">
+        {{ ($product->type == 'booking') ?  __('shop::app.products.book-now') :  __('shop::app.products.add-to-cart') }}
+    </button>
+</form>
 
 {!! view_render_event('bagisto.shop.products.add_to_cart.after', ['product' => $product]) !!}
