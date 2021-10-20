414bd392bc3422c76db8b75b3cfd08135d90150e
Devansh <devansh.bawari419@webkul.com>
2021-10-20 12:49:38 +0530

    Completed Styling And Shop End Breadcrumb

--- packages/Webkul/Shop/src/Resources/views/customers/account/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/customers/account/index.blade.php
@@ -1,6 +1,10 @@
 @extends('shop::layouts.master')
 
 @section('content-wrapper')
+    <div>
+        {{ Breadcrumbs::render() }}
+    </div>
+
     <div class="account-content">
         @include('shop::customers.account.partials.sidemenu')
 
