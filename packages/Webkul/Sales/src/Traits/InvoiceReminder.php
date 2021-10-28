deb51733e794e569377939b66f8171fe9758b3e4
David Callizaya <davidcallizaya@gmail.com>
2021-10-28 17:21:34 -0400

    Add sendInvoiceReminder

---
+++ packages/Webkul/Sales/src/Traits/InvoiceReminder.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Webkul\Sales\Traits;
+
+use Illuminate\Support\Facades\Mail;
+use Webkul\Admin\Mail\InvoiceOverdueReminder;
+
+trait InvoiceReminder
+{
+    /**
+     * Wether the core config have maximum limit of reminders.
+     *
+     * @return bool
+     */
+    private function hasOverdueRemindersLimit()
+    {
+        return $this->getOverdueRemindersLimit()
+            ? true : false;
+    }
+
+    /**
+     * Get maximum limit of reminders from the core config.
+     *
+     * @return int
+     */
+    private function getOverdueRemindersLimit()
+    {
+        static $remindersLimit = 0;
+
+        if ($remindersLimit) {
+            return $remindersLimit;
+        }
+
+        return $remindersLimit = (int) core()->getConfigData('sales.invoice_setttings.invoice_reminders.reminders_limit');
+    }
+
+    /**
+     * Send an Invoice reminder
+     *
+     * @return void
+     */
+    public function sendInvoiceReminder()
+    {
+        if ($this->hasOverdueRemindersLimit()) {
+            $limit = $this->getOverdueRemindersLimit();
+            if ($this->reminders >= $limit) {
+                return;
+            }
+        }
+        /** @var Webkul\Customer\Models\Customer $customer */
+        $customer = $this->customer;
+        Mail::queue(new InvoiceOverdueReminder($customer, $this));
+        $this->reminders++;
+        $this->save();
+    }
+}
