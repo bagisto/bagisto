a97c9d4cef99acfb1009d40af088329b69c9b7c6
Devansh <devansh.bawari419@webkul.com>
2021-11-03 18:07:54 +0530

    Reverted One File

--- config/filesystems.php
+++ config/filesystems.php
@@ -67,7 +67,6 @@ return [
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
-            'visibility' => 'public',
         ],
 
     ],
