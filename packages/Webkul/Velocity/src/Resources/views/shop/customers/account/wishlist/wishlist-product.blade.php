e8c7b8f32abf94e553d8d357db5af3e90f5e0cb6
Devansh <devansh.bawari419@webkul.com>
2021-11-15 18:07:24 +0530

    Chaining Added

--- packages/Webkul/Velocity/src/Resources/views/shop/customers/account/wishlist/wishlist-product.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/customers/account/wishlist/wishlist-product.blade.php
@@ -1,20 +1,11 @@
-@php
-    /**
-     * Product model.
-     *
-     * @var \Webkul\Product\Models\Product
-     */
-    $product = $item->product;
-@endphp
-
 <div class="col-12 lg-card-container list-card product-card row">
     <div class="product-image">
         <a
-            title="{{ $product->name }}"
-            href="{{ route('shop.productOrCategory.index', $product->url_key) }}">
+            title="{{ $item->product->name }}"
+            href="{{ route('shop.productOrCategory.index', $item->product->url_key) }}">
 
             <img
-                src="{{ productimage()->getProductBaseImage($product)['medium_image_url'] }}"
+                src="{{ productimage()->getProductBaseImage($item->product)['medium_image_url'] }}"
                 :onerror="`this.src='${this.$root.baseUrl}/vendor/webkul/ui/assets/images/product/large-product-placeholder.png'`" alt="" />
 
                 <div class="quick-view-in-list">
@@ -26,15 +17,15 @@
         <div class="p-2">
             <div class="product-name">
                 <a
-                    href="{{ route('shop.productOrCategory.index', $product->url_key) }}"
-                    title="{{ $product->name }}" class="unset">
+                    href="{{ route('shop.productOrCategory.index', $item->product->url_key) }}"
+                    title="{{ $item->product->name }}" class="unset">
 
-                    <span class="fs16">{{ $product->name }}</span>
+                    <span class="fs16">{{ $item->product->name }}</span>
                 </a>
             </div>
 
             <div class="product-price">
-                @include ('shop::products.price', ['product' => $product])
+                @include ('shop::products.price', ['product' => $item->product])
             </div>
 
             <div class="cart-wish-wrap mt5">
@@ -51,7 +42,7 @@
                 <div>
                     @include('shop::products.add-to-cart', [
                         'addWishlistClass'  => 'pl10',
-                        'product'           => $product,
+                        'product'           => $item->product,
                         'addToCartBtnClass' => 'medium-padding',
                         'showCompare'       => core()->getConfigData('general.content.shop.compare_option') == "1" ? true : false,
                     ])
