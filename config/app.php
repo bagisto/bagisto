f36275bf9fdfa6304677b2ba1caf597148703d45
Devansh <devansh.bawari419@webkul.com>
2021-11-18 18:41:13 +0530

    Packages Updated

--- config/app.php
+++ config/app.php
@@ -251,7 +251,7 @@ return [
         Konekt\Concord\ConcordServiceProvider::class,
         Flynsarmy\DbBladeCompiler\DbBladeCompilerServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
-        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        PHPOpenSourceSaver\JWTAuth\Providers\LaravelServiceProvider::class,
 
         //Webkul packages
         Webkul\Theme\Providers\ThemeServiceProvider::class,
