7c953c8aa319f810b496610028a83e5cf5ccb115
Devansh <devansh.bawari419@webkul.com>
2021-11-19 16:18:59 +0530

    Package Moved

--- config/app.php
+++ config/app.php
@@ -251,7 +251,6 @@ return [
         Konekt\Concord\ConcordServiceProvider::class,
         Flynsarmy\DbBladeCompiler\DbBladeCompilerServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
-        PHPOpenSourceSaver\JWTAuth\Providers\LaravelServiceProvider::class,
 
         //Webkul packages
         Webkul\Theme\Providers\ThemeServiceProvider::class,
