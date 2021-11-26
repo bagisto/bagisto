420cb926689d24cdaa18f475e28913babc400170
Federico Ricchiuto <fed.ricchiuto@gmail.com>
2021-11-26 23:10:55 +0100

    update: Webkul\Velocity laravel-mix @ v6.0
    - support for latest node v16 (LTS)
    - webpack.mix.js updated to comply with new syntax
      - `.vue()`
      - `sassOptions`
    - can be used as blueprint to upgrade other packages
    
    BREAKING CHANGE needs proper testing

--- packages/Webkul/Velocity/webpack.mix.js
+++ packages/Webkul/Velocity/webpack.mix.js
@@ -1,39 +1,36 @@
-const { mix } = require('laravel-mix');
+let mix = require('laravel-mix');
 
 require('laravel-mix-merge-manifest');
 
-let publicPath = '../../../public/themes/velocity/assets';
-
-if (mix.inProduction()) {
-    publicPath = 'publishable/assets';
-}
+const publicPath = mix.inProduction()
+                   ? `publishable/assets`
+                   : `../../../public/themes/velocity/assets`;
 
 mix.setPublicPath(publicPath).mergeManifest();
 mix.disableNotifications();
 
 mix
-    .js(__dirname + '/src/Resources/assets/js/app-core.js', 'js/velocity-core.js')
-    .js(
-        __dirname + '/src/Resources/assets/js/app.js',
-        'js/velocity.js'
-    )
+    .js(`${__dirname}/src/Resources/assets/js/app-core.js`, 'js/velocity-core.js')
+    .js(`${__dirname}/src/Resources/assets/js/app.js`,      'js/velocity.js')
+    .vue()
 
-    .copy(__dirname + '/src/Resources/assets/images', publicPath + '/images')
+    .copy(`${__dirname}/src/Resources/assets/images`, `${publicPath}/images`)
 
     .sass(
-        __dirname + '/src/Resources/assets/sass/admin.scss',
-        __dirname + '/' + publicPath + '/css/velocity-admin.css'
+        `${__dirname}/src/Resources/assets/sass/admin.scss`,
+        `${__dirname}/${publicPath}/css/velocity-admin.css`
     )
     .sass(
-        __dirname + '/src/Resources/assets/sass/app.scss',
-        __dirname + '/' + publicPath + '/css/velocity.css',
+        `${__dirname}/src/Resources/assets/sass/app.scss`,
+        `${__dirname}/${publicPath}/css/velocity.css`,
         {
-            includePaths: ['node_modules/bootstrap-sass/assets/stylesheets/']
+            sassOptions: {
+                includePaths: [ 'node_modules/bootstrap-sass/assets/stylesheets/' ],
+            },
         }
     )
-
     .options({
-        processCssUrls: false
+                 processCssUrls: false,
     });
 
 if (mix.inProduction()) {
