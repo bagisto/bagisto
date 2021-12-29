df2d2fea106ee7f7218faf8adf913fb1f2025b9d
Devansh <devansh.bawari419@webkul.com>
2021-12-29 19:29:19 +0530

    Locale Image Issue Fixed

--- packages/Webkul/Core/src/Models/Locale.php
+++ packages/Webkul/Core/src/Models/Locale.php
@@ -3,10 +3,11 @@
 namespace Webkul\Core\Models;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Database\Eloquent\Model;
-use Webkul\Core\Database\Factories\LocaleFactory;
-use Webkul\Core\Contracts\Locale as LocaleContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Webkul\Core\Contracts\Locale as LocaleContract;
+use Webkul\Core\Database\Factories\LocaleFactory;
 
 class Locale extends Model implements LocaleContract
 {
@@ -24,12 +25,43 @@ class Locale extends Model implements LocaleContract
     ];
 
     /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['image_url'];
+
+    /**
      * Create a new factory instance for the model.
      *
      * @return Factory
      */
     protected static function newFactory(): Factory
     {
-        return LocaleFactory::new();
+        return LocaleFactory::new ();
+    }
+
+    /**
+     * Get image url for the logo image.
+     *
+     * @return string
+     */
+    public function image_url()
+    {
+        if (! $this->locale_image) {
+            return '';
+        }
+
+        return Storage::url($this->locale_image);
+    }
+
+    /**
+     * Get image url for the logo image.
+     *
+     * @return string
+     */
+    public function getImageUrlAttribute()
+    {
+        return $this->image_url();
     }
 }
