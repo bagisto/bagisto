46d750391770f22b8d5d56b8b3e6a14cd64d41b3
Devansh <devansh.bawari419@webkul.com>
2022-01-18 14:35:19 +0530

    Added Sidebar

--- packages/Webkul/Velocity/src/Resources/views/shop/cms/page.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/cms/page.blade.php
@@ -19,7 +19,7 @@
 @endsection
 
 @section('content-wrapper')
-    <div class="cms-page-container cart-details row">
+    <div class="cms-page-container p-2">
         {!! DbView::make($page)->field('html_content')->render() !!}
     </div>
 @endsection
