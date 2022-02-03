757dc9a8ed0e541edd4394e0ac6f78891ee115ee
Devansh <devansh.bawari419@webkul.com>
2022-02-03 16:42:37 +0530

    Refactored Image And Video Repository

--- packages/Webkul/Product/src/Repositories/ProductImageRepository.php
+++ packages/Webkul/Product/src/Repositories/ProductImageRepository.php
@@ -3,12 +3,9 @@
 namespace Webkul\Product\Repositories;
 
 use Illuminate\Container\Container as App;
-use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\Storage;
-use Webkul\Core\Eloquent\Repository;
 use Webkul\Product\Repositories\ProductRepository;
 
-class ProductImageRepository extends Repository
+class ProductImageRepository extends ProductMediaRepository
 {
     /**
      * Product repository object.
@@ -44,17 +41,6 @@ class ProductImageRepository extends Repository
     }
 
     /**
-     * Get product directory.
-     *
-     * @param  \Webkul\Product\Contracts\Product $product
-     * @return string
-     */
-    public function getProductDirectory($product): string
-    {
-        return 'product/' . $product->id;
-    }
-
-    /**
      * Upload images.
      *
      * @param  array  $data
@@ -63,63 +49,10 @@ class ProductImageRepository extends Repository
      */
     public function uploadImages($data, $product): void
     {
-        $this->upload($product, $data['images'] ?? null);
+        $this->upload($data, $product, 'images');
 
         if (isset($data['variants'])) {
             $this->uploadVariantImages($data['variants']);
-        }
-    }
-
-    /**
-     * Upload.
-     *
-     * @param  \Webkul\Product\Contracts\Product $product
-     * @param  array
-     * @return void
-     */
-    public function upload($product, $images): void
-    {
-        /**
-         * Previous images ids for filtering.
-         */
-        $previousImageIds = $product->images()->pluck('id');
-
-        if (
-            isset($images['files']) && $images['files']
-            && isset($images['positions']) && $images['positions']
-        ) {
-            /**
-             * Filter out existing images because new image positions are already setuped by index.
-             */
-            $imagePositions = collect($images['positions'])->keys()->filter(function ($imagePosition) {
-                return is_numeric($imagePosition);
-            });
-
-            foreach ($images['files'] as $indexOrImageId => $image) {
-                if ($image instanceof UploadedFile) {
-                    $this->create([
-                        'path'       => $image->store($this->getProductDirectory($product)),
-                        'product_id' => $product->id,
-                        'position'   => $indexOrImageId,
-                    ]);
-                } else {
-                    $this->update([
-                        'position' => $imagePositions->search($indexOrImageId),
-                    ], $indexOrImageId);
-
-                    if (is_numeric($index = $previousImageIds->search($indexOrImageId))) {
-                        $previousImageIds->forget($index);
-                    }
-                }
-            }
-        }
-
-        foreach ($previousImageIds as $indexOrImageId) {
-            if ($image = $this->find($indexOrImageId)) {
-                Storage::delete($image->path);
-
-                $this->delete($indexOrImageId);
-            }
         }
     }
 
@@ -131,14 +64,14 @@ class ProductImageRepository extends Repository
      */
     public function uploadVariantImages($variants): void
     {
-        foreach ($variants as $variantsId => $variant) {
+        foreach ($variants as $variantsId => $variantData) {
             $product = $this->productRepository->find($variantsId);
 
             if (! $product) {
                 break;
             }
 
-            $this->upload($product, $variant['images'] ?? null);
+            $this->upload($variantData, $product, 'images');
         }
     }
 }
--- packages/Webkul/Product/src/Repositories/ProductVideoRepository.php
+++ packages/Webkul/Product/src/Repositories/ProductVideoRepository.php
@@ -2,11 +2,7 @@
 
 namespace Webkul\Product\Repositories;
 
-use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\Storage;
-use Webkul\Core\Eloquent\Repository;
-
-class ProductVideoRepository extends Repository
+class ProductVideoRepository extends ProductMediaRepository
 {
     /**
      * Specify model class name.
@@ -19,17 +15,6 @@ class ProductVideoRepository extends Repository
     }
 
     /**
-     * Get product directory.
-     *
-     * @param  \Webkul\Product\Contracts\Product $product
-     * @return string
-     */
-    public function getProductDirectory($product): string
-    {
-        return 'product/' . $product->id;
-    }
-
-    /**
      * Upload videos.
      *
      * @param  array  $data
@@ -38,48 +23,6 @@ class ProductVideoRepository extends Repository
      */
     public function uploadVideos($data, $product)
     {
-        /**
-         * Previous images ids for filtering.
-         */
-        $previousVideoIds = $product->videos()->pluck('id');
-
-        if (
-            isset($data['videos']['files']) && $data['videos']['files']
-            && isset($data['videos']['positions']) && $data['videos']['positions']
-        ) {
-            /**
-             * Filter out existing videos because new video positions are already setuped by index.
-             */
-            $videoPositions = collect($data['videos']['positions'])->keys()->filter(function ($videoPosition) {
-                return is_numeric($videoPosition);
-            });
-
-            foreach ($data['videos']['files'] as $indexOrVideoId => $video) {
-                if ($video instanceof UploadedFile) {
-                    $this->create([
-                        'path'       => $video->store($this->getProductDirectory($product)),
-                        'product_id' => $product->id,
-                        'position'   => $indexOrVideoId,
-                        'type'       => 'video',
-                    ]);
-                } else {
-                    $this->update([
-                        'position' => $videoPositions->search($indexOrVideoId),
-                    ], $indexOrVideoId);
-
-                    if (is_numeric($index = $previousVideoIds->search($indexOrVideoId))) {
-                        $previousVideoIds->forget($index);
-                    }
-                }
-            }
-        }
-
-        foreach ($previousVideoIds as $indexOrVideoId) {
-            if ($video = $this->find($indexOrVideoId)) {
-                Storage::delete($video->path);
-
-                $this->delete($indexOrVideoId);
-            }
-        }
+        $this->upload($data, $product, 'videos');
     }
 }
