42937bbac18a42937f429d143d48d32be9c7d6ad
jitendra <jitendra@webkul.com>
2022-03-10 12:06:38 +0530

    Fixed

--- packages/Webkul/Velocity/src/Resources/views/shop/customers/account/profile/index.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/customers/account/profile/index.blade.php
@@ -97,7 +97,7 @@
                         <div class="control-group" :class="[errors.has('password') ? 'has-error' : '']">
                             <label for="password" class="required">{{ __('admin::app.users.users.password') }}</label>
 
-                            <input type="password" v-validate="'required|min:6|max:18'" class="control" id="password" name="password" data-vv-as="&quot;{{ __('admin::app.users.users.password') }}&quot;"/>
+                            <input type="password" v-validate="'required|min:6'" class="control" id="password" name="password" data-vv-as="&quot;{{ __('admin::app.users.users.password') }}&quot;"/>
 
                             <span class="control-error" v-if="errors.has('password')" v-text="errors.first('password')"></span>
                         </div>
