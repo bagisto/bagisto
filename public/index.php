837a95ccd7b5ed56c636611d947c076a0ed5186d
jitendra <jitendra@webkul.com>
2022-03-24 09:52:06 +0530

    Removed unnecessary code from index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
-error_reporting(-1);
-
 /*
 |--------------------------------------------------------------------------
 | Checking For Bagisto's Installation
