d948532666afa053efa4e2f6189d570c98229dc6
Sanjay <sanjay.bhatt371@webkul.com>
2022-04-26 13:37:23 +0530

    Changes-done

--- packages/Webkul/Shop/src/Resources/views/products/add-to-cart.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/add-to-cart.blade.php
@@ -4,7 +4,7 @@
     $width = (core()->getConfigData('catalog.products.storefront.buy_now_button_display') == 1) ? '49' : '100';
 @endphp
 
-<button type="submit" class="btn btn-lg btn-primary addtocart" {{ ! $product->isSaleable() ? 'disabled' : '' }}>
+<button type="submit" class="btn btn-lg btn-primary add-to-cart" {{ ! $product->isSaleable() ? 'disabled' : '' }}>
     {{ ($product->type == 'booking') ?  __('shop::app.products.book-now') :  __('shop::app.products.add-to-cart') }}
 </button>
 
