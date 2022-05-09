db401ba626e954a060f5eb0101fe74e93877b5c0
Devansh <devansh.bawari419@webkul.com>
2022-05-09 19:05:21 +0530

    Location Icon Fixed

--- packages/Webkul/BookingProduct/src/Resources/views/shop/default/products/view/booking.blade.php
+++ packages/Webkul/BookingProduct/src/Resources/views/shop/default/products/view/booking.blade.php
@@ -1,29 +1,33 @@
 @if ($product->type == 'booking')
-
     @if ($bookingProduct = app('\Webkul\BookingProduct\Repositories\BookingProductRepository')->findOneByField('product_id', $product->product_id))
-
         @push('css')
             <link rel="stylesheet" href="{{ bagisto_asset('css/default-booking.css') }}">
         @endpush
 
-        <booking-information></booking-information>        
+        <booking-information></booking-information>
 
         @push('scripts')
-
             <script type="text/x-template" id="booking-information-template">
                 <div class="booking-information">
-
                     @if ($bookingProduct->location != '')
                         <div class="booking-info-row">
-                            <span class="icon bp-location-icon"></span>
-                            <span class="title">{{ __('bookingproduct::app.shop.products.location') }}</span>
-                            <span class="value">{{ $bookingProduct->location }}</span>
-                            <a href="https://maps.google.com/maps?q={{ $bookingProduct->location }}" target="_blank">View on Map</a>
+                            <span class="title">
+                                <span class="icon bp-location-icon"></span>
+
+                                <span class="location-text">{{ __('bookingproduct::app.shop.products.location') }}</span>
+                            </span>
+
+                            <span class="value">
+                                {{ $bookingProduct->location }}
+                            </span>
+
+                            <span class="value">
+                                <a href="https://maps.google.com/maps?q={{ $bookingProduct->location }}" target="_blank">View on Map</a>
+                            </span>
                         </div>
                     @endif
 
                     @include ('bookingproduct::shop.products.view.booking.' . $bookingProduct->type, ['bookingProduct' => $bookingProduct])
-
                 </div>
             </script>
 
@@ -40,9 +44,6 @@
                     }
                 });
             </script>
-        
         @endpush
-
     @endif
-
 @endif
