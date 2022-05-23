3a23603659f7662e6c53972d5dd517e9c87902b3
Sanjay <sanjay.bhatt371@webkul.com>
2022-05-23 12:48:20 +0530

    Enhancement #6367 done

--- packages/Webkul/Velocity/src/Resources/views/shop/products/price.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/products/price.blade.php
@@ -1,7 +1,18 @@
 {!! view_render_event('bagisto.shop.products.price.before', ['product' => $product]) !!}
 
+@php 
+    if($product->getTypeInstance()->haveSpecialPrice()){
+        $specialPrice = $product->getTypeInstance()->getSpecialPrice();
+        $proce = $product->price;
+        $Discount_Precent = "(".round(($specialPrice*100)/$proce)."% off)";  
+
+    }else{
+       $Discount_Precent ='';
+    } 
+@endphp 
+
 <div class="product-price">
-    {!! $product->getTypeInstance()->getPriceHtml() !!}
+    {!! $product->getTypeInstance()->getPriceHtml() !!} {{ $Discount_Precent }}
 </div>
 
 {!! view_render_event('bagisto.shop.products.price.after', ['product' => $product]) !!}
