62bddf177a2ee11cadcb6f093c51cf5e5beb1f4f
sanjay bhatt <sanjay.bhatt371@webkul.com>
2022-07-11 17:57:36 +0530

    Created-return-typr--for-blankDownloadLinks

--- packages/Webkul/Product/src/Helpers/Downloadable.php
+++ packages/Webkul/Product/src/Helpers/Downloadable.php
@@ -5,6 +5,31 @@ namespace Webkul\Product\Helpers;
 class Downloadable extends AbstractProduct
 {
     /**
+     * return blank downloadable product links.
+     *
+     * @param  \Webkul\Product\Models\Product  $product
+     * @return \Illuminate\Support\Collection
+     */
+
+     public function blankDownloadableLinks()
+     {
+        return [
+            'title'       => '',
+            'type'        => 'file',
+            'file'        => '',
+            'file_name'   => '',
+            'url'         => '',
+            'sample_type' => 'file',
+            'sample_file' => '',
+            'sample_file_name' => '',
+            'sample_url'       => '',
+            'downloads'        => 0,
+            'sort_order'       => 0
+        ];
+     }
+   
+   
+    /**
      * Get downloadable product links.
      *
      * @param  \Webkul\Product\Models\Product  $product
@@ -14,19 +39,7 @@ class Downloadable extends AbstractProduct
     {
         if ($product->downloadable_links->isEmpty()) {
             return collect([
-                [
-                    'title'       => '',
-                    'type'        => 'file',
-                    'file'        => '',
-                    'file_name'   => '',
-                    'url'         => '',
-                    'sample_type' => 'file',
-                    'sample_file' => '',
-                    'sample_file_name' => '',
-                    'sample_url'       => '',
-                    'downloads'        => 0,
-                    'sort_order'       => 0
-                ]
+               $this->blankDownloadableLinks()
             ]);
         }
 
