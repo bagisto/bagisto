9662f095de6af360442a022dc0ea44d8f8d2b221
Devansh <68321766+devansh-webkul@users.noreply.github.com>
2022-07-20 19:11:19 +0530

    Update text.blade.php

--- packages/Webkul/Admin/src/Resources/views/catalog/products/field-types/text.blade.php
+++ packages/Webkul/Admin/src/Resources/views/catalog/products/field-types/text.blade.php
@@ -5,15 +5,14 @@
     class="control"
     id="{{ $attribute->code }}"
 
-    {{-- To Do (@devansh-webkul): Need to handle from the backend. --}}
     @if ($attribute->code === 'sku')
         v-validate="{ required: true, regex: /^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$/ }"
     @else
         v-validate="'{{ $validations }}'"
     @endif
+    
     data-vv-as="&quot;{{ $attribute->admin_name }}&quot;"
 
-    {{-- To Do (@devansh-webkul): Need to handle from the backend. --}}
     {{ in_array($attribute->code, ['url_key']) ? 'v-slugify' : '' }}
     {{ $attribute->code == 'name' && ! $product->url_key  ? 'v-slugify-target=\'url_key\'' : ''  }}
 >
