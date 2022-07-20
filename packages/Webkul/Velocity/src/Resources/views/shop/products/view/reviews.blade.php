a753bd5430008c7133a1f047d1a2c15459145729
Abhishek Kathuria <abhishekkathuria.oc522@webkul.com>
2022-07-20 19:33:40 +0530

    added condition on review button

--- packages/Webkul/Velocity/src/Resources/views/shop/products/view/reviews.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/products/view/reviews.blade.php
@@ -219,8 +219,8 @@
 
     @else
         @if (
-            core()->getConfigData('catalog.products.review.guest_review')
-            || auth()->guard('customer')->check()
+            (core()->getConfigData('catalog.products.review.guest_review')
+            || auth()->guard('customer')->check()) && $orderCount != 0
         )
             <div class="customer-rating" style="border: none">
                 <a href="{{ route('shop.reviews.create', ['slug' => $product->url_key ]) }}">
