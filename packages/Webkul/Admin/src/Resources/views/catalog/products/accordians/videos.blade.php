749e29ac7fd0d18cac16acdba14aac33d7f23122
Abhishek Kathuria <abhishekkathuria.oc522@webkul.com>
2022-07-30 16:45:27 +0530

    Fixed issue #5801

--- packages/Webkul/Admin/src/Resources/views/catalog/products/accordians/videos.blade.php
+++ packages/Webkul/Admin/src/Resources/views/catalog/products/accordians/videos.blade.php
@@ -1,5 +1,9 @@
 {!! view_render_event('bagisto.admin.catalog.product.edit_form_accordian.videos.before', ['product' => $product]) !!}
 
+@php
+    $max_upload = ini_get("upload_max_filesize");
+@endphp
+
 <accordian title="{{ __('admin::app.catalog.products.videos') }}" :active="false">
     <div slot="body">
         {!! view_render_event('bagisto.admin.catalog.product.edit_form_accordian.videos.controls.before', ['product' => $product]) !!}
@@ -13,6 +17,7 @@
                 class="control-error"
                 v-text="'{{ $errors->first('videos.files.*') }}'">
             </span>
+            <span class="control-info mt-10">{{ __('admin::app.catalog.products.video-size') ." ". $max_upload }}</span>
         </div>
 
         {!! view_render_event('bagisto.admin.catalog.product.edit_form_accordian.videos.controls.after', ['product' => $product]) !!}
