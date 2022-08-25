c5c1991e833b73d708c208eb85ecec4688042eab
sanjeev <sanjeev.papnoi829@webkul.in>
2022-08-25 13:18:39 +0530

    Restructure code

--- packages/Webkul/BookingProduct/src/Type/Booking.php
+++ packages/Webkul/BookingProduct/src/Type/Booking.php
@@ -173,16 +173,19 @@ class Booking extends Virtual
         $bookingProduct = $this->getBookingProduct($data['product_id']);
 
 
-        if ($bookingProduct->type == 'rental' && isset($data['booking']['slot']['from'])) {
-            $time = $data['booking']['slot']['to'] - $data['booking']['slot']['from'];
-            $hours = floor($time / 60)/60;
+        if ($bookingProduct->type == 'rental') {
+            if (isset($data['booking']['slot']['from'])) {
+                $time = $data['booking']['slot']['to'] - $data['booking']['slot']['from'];
+                $hours = floor($time / 60) / 60;
 
-            if ($hours > 1) {
-                return trans('shop::app.checkout.cart.integrity.select_hourly_duration');
+                if ($hours > 1) {
+                    return trans('shop::app.checkout.cart.integrity.select_hourly_duration');
+                }
             }
-        }
 
-        if ($bookingProduct->type == 'event') {
+            $products = parent::prepareForCart($data);
+            
+        } elseif ($bookingProduct->type == 'event') {
             if (
                 Carbon::now() > $bookingProduct->available_from
                 && Carbon::now() > $bookingProduct->available_to
