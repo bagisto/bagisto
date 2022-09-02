ad127a9b2d447b85e52211135f01b7d7fb790f59
Abhishek Kathuria <abhishekkathuria.oc522@webkul.com>
2022-09-02 11:36:57 +0530

    Fixed issue #6377

--- packages/Webkul/Velocity/src/Resources/views/shop/products/view/configurable-options.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/products/view/configurable-options.blade.php
@@ -314,7 +314,7 @@
                             });
 
                             let priceLabelElement = document.querySelector('.price-label');
-                            let priceElement = document.querySelector('.final-price');
+                            let priceElement = document.querySelector('.special-price');
                             let regularPriceElement = document.querySelector('.regular-price');
 
                             if (this.childAttributes.length == selectedOptionCount) {
@@ -322,7 +322,18 @@
 
                                 priceElement.innerHTML = this.config.variant_prices[this.simpleProduct].final_price.formated_price;
 
-                                if (regularPriceElement) {
+                                if (
+                                    this.config.variant_prices[this.simpleProduct].regular_price.formated_price == "$0.00" 
+                                    || this.config.variant_prices[this.simpleProduct].regular_price.formated_price == this.config.variant_prices[this.simpleProduct].final_price.formated_price
+                                ) {
+                                    regularPriceElement.innerHTML = "";
+                                }
+                                
+                                if (
+                                    regularPriceElement 
+                                    && this.config.variant_prices[this.simpleProduct].regular_price.formated_price != "$0.00" 
+                                    && this.config.variant_prices[this.simpleProduct].regular_price.formated_price != this.config.variant_prices[this.simpleProduct].final_price.formated_price 
+                                ) {
                                     regularPriceElement.innerHTML = this.config.variant_prices[this.simpleProduct].regular_price.formated_price;
                                 }
 
