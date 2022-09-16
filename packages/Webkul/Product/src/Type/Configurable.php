834791adeba8e8d06951cd59991abe186966ee93
sanjeev <sanjeev.papnoi829@webkul.in>
2022-09-16 12:47:30 +0530

    updates for regular price

--- packages/Webkul/Product/src/Type/Configurable.php
+++ packages/Webkul/Product/src/Type/Configurable.php
@@ -636,10 +636,20 @@ class Configurable extends AbstractType
     public function getPriceHtml()
     {
         if ($this->haveSpecialPrice()) {
-            return '<div class="sticker sale">' . trans('shop::app.products.sale') . '</div>'
+            $html = '';
+
+            $html .=  '<div class="sticker sale">' . trans('shop::app.products.sale') . '</div>'
             . '<span class="price-label">' . trans('shop::app.products.price-label') . '</span>'
-            . '<span class="special-price">' . core()->currency($this->evaluatePrice($this->getMinimalPrice())) . '</span>'
-            . '<span class="regular-price">' . core()->currency($this->evaluatePrice($this->getOfferPrice())) . '</span>';
+            . '<span class="special-price">' . core()->currency($this->evaluatePrice($this->getMinimalPrice())) . '</span>';
+
+            if (core()->currency($this->evaluatePrice($this->getMinimalPrice()) ) !=  core()->currency($this->evaluatePrice($this->getOfferPrice()))) {
+                $html .= '<span class="regular-price">' . core()->currency($this->evaluatePrice($this->getOfferPrice())) . '</span>';
+            } else {
+                $html .= '<span class="regular-price"></span>';
+            }
+            
+            return $html;
+
         } elseif ($this->haveOffer()) {
             return '<div class="sticker sale">' . trans('shop::app.products.sale') . '</div>'
             . '<span class="price-label">' . trans('shop::app.products.price-label') . '</span>'
