75e2fd2e38fc202d6c13aa87eb6ea42a2d42cd26
sanjeev <sanjeev.papnoi829@webkul.in>
2022-09-23 16:45:33 +0530

    updates

--- packages/Webkul/Product/src/Http/Controllers/ProductController.php
+++ packages/Webkul/Product/src/Http/Controllers/ProductController.php
@@ -156,14 +156,19 @@ class ProductController extends Controller
     public function update(ProductForm $request, $id)
     {
         $product = $this->productRepository->find($id);
-
-        if (
-            (($product->type == 'bundle') 
-            && (($request->get('bundle_options') == null)))
-        ) {
+        
+        if ($product->type == 'bundle' && $request->get('bundle_options') == null) {
             session()->flash('warning', trans('admin::app.catalog.products.atleast-one-bundle-item'));
 
             return back();
+        } elseif ($product->type == 'bundle' && $request->get('bundle_options') != null) {
+            foreach ($request->get('bundle_options') as $productOption) {
+                if (! isset($productOption['products'])) {
+                    session()->flash('warning', trans('admin::app.catalog.products.atleast-one-bundle-item'));
+
+                    return back();
+                }
+            }
         }
 
         Event::dispatch('catalog.product.update.before', $id);
