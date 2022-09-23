e8e041dfb7c1e1c8e8dae4b49b4b0c6ab4cf17ba
jitendra <jitendra@webkul.com>
2022-09-23 15:39:03 +0530

    Refactorred User package

--- packages/Webkul/User/src/Database/Factories/AdminFactory.php
+++ packages/Webkul/User/src/Database/Factories/AdminFactory.php
@@ -3,6 +3,7 @@
 namespace Webkul\User\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Webkul\User\Models\Admin;
 
 class AdminFactory extends Factory
 {
@@ -11,7 +12,7 @@ class AdminFactory extends Factory
      *
      * @var string
      */
-    protected $model = \Webkul\User\Models\Admin::class;
+    protected $model = Admin::class;
 
     /**
      * Define the model's default state.
@@ -21,11 +22,11 @@ class AdminFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->email,
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'role_id' => 1,
-            'status' => 1
+            'name'     => $this->faker->name(),
+            'email'    => $this->faker->unique()->email,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'role_id'  => 1,
+            'status'   => 1
         ];
     }
 }
