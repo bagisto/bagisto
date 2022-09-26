0e0b691d1203a54e7e45b9b7ee6445fc1ebb0523
jitendra <jitendra@webkul.com>
2022-09-26 16:16:51 +0530

    Fixed wishlish icon issue

--- packages/Webkul/Velocity/src/Resources/views/shop/products/add-to-cart.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/products/add-to-cart.blade.php
@@ -24,19 +24,62 @@
         @endif
 
         <div class="add-to-cart-btn pl0">
-            <add-to-cart
-                form="true"
-                csrf-token='{{ csrf_token() }}'
-                product-flat-id="{{ $product->id }}"
-                product-id="{{ $product->product_id }}"
-                reload-page="{{ $reloadPage ?? false }}"
-                move-to-cart="{{ $moveToCart ?? false }}"
-                wishlist-move-route="{{ $wishlistMoveRoute ?? false }}"
-                add-class-to-btn="{{ $addToCartBtnClass ?? '' }}"
-                is-enable={{ ! $product->isSaleable() ? 'false' : 'true' }}
-                show-cart-icon={{ empty($showCartIcon) }}
-                btn-text="{{ (! isset($moveToCart) && $product->type == 'booking') ?  __('shop::app.products.book-now') : $btnText ?? __('shop::app.products.add-to-cart') }}">
-            </add-to-cart>
+            @if (
+                isset($form)
+                && ! $form
+            )
+                <button
+                    type="submit"
+                    {{ ! $product->isSaleable() ? 'disabled' : '' }}
+                    class="theme-btn {{ $addToCartBtnClass ?? '' }}">
+
+                    @if (
+                        ! (isset($showCartIcon)
+                        && ! $showCartIcon)
+                    )
+                        <i class="material-icons text-down-3">shopping_cart</i>
+                    @endif
+
+                    {{ ($product->type == 'booking') ?  __('shop::app.products.book-now') :  __('shop::app.products.add-to-cart') }}
+                </button>
+            @elseif(isset($addToCartForm) && ! $addToCartForm)
+                <form
+                    method="POST"
+                    action="{{ route('shop.cart.add', $product->product_id) }}">
+
+                    @csrf
+
+                    <input type="hidden" name="product_id" value="{{ $product->product_id }}">
+                    <input type="hidden" name="quantity" value="1">
+                    <button
+                        type="submit"
+                        {{ ! $product->isSaleable() ? 'disabled' : '' }}
+                        class="btn btn-add-to-cart {{ $addToCartBtnClass ?? '' }}">
+
+                        @if (empty($showCartIcon))
+                            <i class="material-icons text-down-3">shopping_cart</i>
+                        @endif
+
+                        <span class="fs14 fw6 text-uppercase text-up-4">
+                            {{ ($product->type == 'booking') ?  __('shop::app.products.book-now') : $btnText ?? __('shop::app.products.add-to-cart') }}
+                        </span>
+                    </button>
+                </form>
+            @else
+                <add-to-cart
+                    form="true"
+                    csrf-token='{{ csrf_token() }}'
+                    product-flat-id="{{ $product->id }}"
+                    product-id="{{ $product->product_id }}"
+                    reload-page="{{ $reloadPage ?? false }}"
+                    move-to-cart="{{ $moveToCart ?? false }}"
+                    wishlist-move-route="{{ $wishlistMoveRoute ?? false }}"
+                    add-class-to-btn="{{ $addToCartBtnClass ?? '' }}"
+                    is-enable={{ ! $product->isSaleable() ? 'false' : 'true' }}
+                    show-cart-icon={{ empty($showCartIcon) }}
+                    btn-text="{{ (! isset($moveToCart) && $product->type == 'booking') ?  __('shop::app.products.book-now') : $btnText ?? __('shop::app.products.add-to-cart') }}">
+                </add-to-cart>
+            @endif
         </div>
     </div>
 
