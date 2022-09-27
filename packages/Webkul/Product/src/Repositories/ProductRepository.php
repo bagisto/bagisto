5b065b4438f7ca4938f59cc0c9238b99ffce454a
jitendra <jitendra@webkul.com>
2022-09-27 13:40:30 +0530

    Removed dd function

--- packages/Webkul/Product/src/Repositories/ProductRepository.php
+++ packages/Webkul/Product/src/Repositories/ProductRepository.php
@@ -294,7 +294,6 @@ class ProductRepository extends Repository
             });
 
             $items = $query->get();
-            dd($items->toArray());
         }
 
         $results = new LengthAwarePaginator($items, $count, $perPage, $page, [
