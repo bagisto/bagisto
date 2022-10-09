e9837c5002899478d8680a978a28e8eb9f1103e0
Alexey Pyltsyn <lex61rus@gmail.com>
2022-10-09 11:40:05 +0300

    Add various improvements for mobile nav

--- packages/Webkul/Admin/src/Resources/views/layouts/mobile-nav.blade.php
+++ packages/Webkul/Admin/src/Resources/views/layouts/mobile-nav.blade.php
@@ -19,97 +19,101 @@
 
             <div class="nav-toggle"></div>
 
-            <div class="overlay"></div>
+            <div class="overlay" @click="closeNavBar"></div>
 
-            <div class="nav-top">
-                <div class="pro-info">
-                    <div class="profile-info-icon">
-                        <span style="">{{ substr(auth()->guard('admin')->user()->name, 0, 1) }}</span>
-                    </div>
+            <div class="nav-content">
 
-                    <div class="profile-info-desc">
-                        <div class="name">
-                            {{ auth()->guard('admin')->user()->name }}
+                <div class="nav-top">
+                    <div class="pro-info">
+                        <div class="profile-info-icon">
+                            <span style="">{{ substr(auth()->guard('admin')->user()->name, 0, 1) }}</span>
                         </div>
 
-                        <div class="role">
-                            {{ auth()->guard('admin')->user()->role['name'] }}
+                        <div class="profile-info-desc">
+                            <div class="name">
+                                {{ auth()->guard('admin')->user()->name }}
+                            </div>
+
+                            <div class="role">
+                                {{ auth()->guard('admin')->user()->role['name'] }}
+                            </div>
                         </div>
-                    </div>
-                    <div style="display:inline-block" @click="closeNavBar">
-                        <span class="close"></span>
+                        <div style="display:inline-block" @click="closeNavBar">
+                            <span class="close"></span>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="nav-items">
-                @foreach ($menu->items as $menuItem)
-                    <div class="nav-item {{ $menu->getActive($menuItem) }}">
-                        <a class="nav-tab-name" href="{{ $menuItem['key'] == 'dashboard' ? $menuItem['url'] : '#' }}">
-                            <span class="icon-menu icon {{ $menuItem['icon-class'] }}"
-                            style="margin-right:10px; display: inline-block;vertical-align: middle;transform: scale(0.8);"></span>
+                <div class="nav-items">
+                    @foreach ($menu->items as $menuItem)
+                        <div class="nav-item {{ $menu->getActive($menuItem) }}">
+                            <a class="nav-tab-name" href="{{ $menuItem['key'] == 'dashboard' ? $menuItem['url'] : '#' }}">
+                                <span class="icon-menu icon {{ $menuItem['icon-class'] }}"></span>
 
-                            <span class="menu-label">{{ trans($menuItem['name']) }}</span>
-                            @if(count($menuItem['children']) || $menuItem['key'] == 'configuration' )
-                            <span class="icon arrow-icon"></span>
-                            @endif
-                        </a>
-                        @if ($menuItem['key'] != 'configuration')
-                            @if (count($menuItem['children']))
-                            <ul>
-                                @foreach ($menuItem['children'] as $subMenuItem)
-                                    <li class="navbar-child {{ $menu->getActive($subMenuItem) }}">
-                                    <a href="{{ count($subMenuItem['children']) ? current($subMenuItem['children'])['url'] : $subMenuItem['url'] }}">
-                                            <span style="margin-left:47px">{{ trans($subMenuItem['name']) }}</span>
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                            @endif
-                        @else
-                            <ul>
-                                @foreach ($config->items as $key => $item)
-                                    <li class="navbar-child {{ $item['key'] == request()->route('slug') ? 'active' : '' }}">
-                                        <a href="{{ route('admin.configuration.index', $item['key']) }}">
-                                            <span style="margin-left:47px">{{ trans($item['name'] ?? '') }}</span>
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        @endif
-                    </div>
-                @endforeach
-                <div class="nav-item">
-                    <a class="nav-tab-name">
-                        <span class="icon-menu icon accounts-icon"
-                            style="margin-right:10px; display: inline-block;vertical-align: middle;transform: scale(0.8);"></span>
-                        <span class="menu-label">{{ __('admin::app.layouts.account-title') }}</span>
-                        <span class="icon arrow-icon"></span>
-                    </a>
-                    <ul>
-                        <li class="navbar-child">
-                            <a>
-                                <span style="display:flex;justify-content:space-between;height:20px">
-                                    <div style="margin-top:12px;margin-left:47px">
-                                        <span>{{ __('admin::app.layouts.mode') }}</span>
-                                    </div>
-                                    <dark style="margin-left:13%"></dark>
+                                <span class="menu-label">{{ trans($menuItem['name']) }}</span>
+                                @if(count($menuItem['children']) || $menuItem['key'] == 'configuration' )
+                                <span class="arrow-icon-wrapper">
+                                    <span class="icon arrow-icon"></span>
                                 </span>
+                                @endif
                             </a>
+                            @if ($menuItem['key'] != 'configuration')
+                                @if (count($menuItem['children']))
+                                <ul>
+                                    @foreach ($menuItem['children'] as $subMenuItem)
+                                        <li class="navbar-child {{ $menu->getActive($subMenuItem) }}">
+                                        <a href="{{ count($subMenuItem['children']) ? current($subMenuItem['children'])['url'] : $subMenuItem['url'] }}">
+                                                <span>{{ trans($subMenuItem['name']) }}</span>
+                                            </a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                                @endif
+                            @else
+                                <ul>
+                                    @foreach ($config->items as $key => $item)
+                                        <li class="navbar-child {{ $item['key'] == request()->route('slug') ? 'active' : '' }}">
+                                            <a href="{{ route('admin.configuration.index', $item['key']) }}">
+                                                <span>{{ trans($item['name'] ?? '') }}</span>
+                                            </a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            @endif
+                        </div>
+                    @endforeach
+                    <div class="nav-item">
+                        <a class="nav-tab-name">
+                            <span class="icon-menu icon accounts-icon"></span>
+                            <span class="menu-label">{{ __('admin::app.layouts.account-title') }}</span>
+                            <span class="arrow-icon-wrapper">
+                                <span class="icon arrow-icon"></span>
+                            </span>
+                        </a>
+                        <ul>
+                            <li class="navbar-child">
+                                <a>
+                                    <span class="dark-mode-switcher">
+                                            {{ __('admin::app.layouts.mode') }}
+                                        <dark style="margin-left:13%"></dark>
+                                    </span>
+                                </a>
 
-                        </li>
-                        <li  class="navbar-child">
-                            <a href="{{ route('admin.account.edit') }}">
-                                <span style="margin-left:47px">{{ __('admin::app.layouts.my-account') }}</span>
-                            </a>
-                        </li>
-                        <li  class="navbar-child">
-                            <a href="{{ route('admin.session.destroy') }}">
-                                <span style="margin-left:47px">{{ __('admin::app.layouts.logout') }}</span>
-                            </a>
-                        </li>
-                    </ul>
+                            </li>
+                            <li  class="navbar-child">
+                                <a href="{{ route('admin.account.edit') }}">
+                                    <span>{{ __('admin::app.layouts.my-account') }}</span>
+                                </a>
+                            </li>
+                            <li  class="navbar-child">
+                                <a href="{{ route('admin.session.destroy') }}">
+                                    <span>{{ __('admin::app.layouts.logout') }}</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
+
             </div>
         </div>
     </script>
@@ -136,8 +140,10 @@
                     toggle.addEventListener("click", () => {
                         if (nav.classList.contains("is-active")) {
                             nav.classList.remove("is-active");
+                            document.body.style.overflowY = null;
                         } else {
                             nav.classList.add("is-active");
+                            document.body.style.overflowY = "hidden";
                         }
                     });
 
