57e015c7dd3b5f1f947106575d24f517d065db69
Devansh <devansh.bawari419@webkul.in>
2022-10-21 10:13:00 +0530

    Issue #6987 Fixed

--- packages/Webkul/BookingProduct/src/Resources/views/admin/sales/bookings/index/view-swither.blade.php
+++ packages/Webkul/BookingProduct/src/Resources/views/admin/sales/bookings/index/view-swither.blade.php
@@ -1,18 +1,18 @@
 <div class="switch-view-container">
     @if (request('view_type'))
-        <a href="{{ route('admin.sales.bookings.index') }}" class="icon-container">
+        <a href="{{ route('admin.sales.bookings.index') }}" class="icon-container"  title="table View">
             <i class="icon table-icon"></i>
         </a>
 
-        <a  class="icon-container active">
+        <a  class="icon-container active"  title="calendar View">
             <i class="icon calendar-white-icon"></i>
         </a>
     @else
-        <a class="icon-container active">
+        <a class="icon-container active"  title="table View"> 
             <i class="icon table-white-icon"></i>
         </a>
 
-        <a href="{{ route('admin.sales.bookings.index', ['view_type' => 'calendar']) }}" class="icon-container">
+        <a href="{{ route('admin.sales.bookings.index', ['view_type' => 'calendar']) }}" class="icon-container"  title="calendar View">
             <i class="icon calendar-icon"></i>
         </a>
     @endif
