803b6fdcfcffeedafaed8afe3d0e58733ae17682
Alan Paul <57307037+Alanzz@users.noreply.github.com>
2022-10-22 21:21:13 +0530

    Malayalam language support for authentication.

---
+++ lang/mal/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'ഈ യോഗ്യതാപത്രങ്ങൾ ഞങ്ങളുടെ രേഖകളുമായി പൊരുത്തപ്പെടുന്നില്ല.',
+    'throttle' => 'വളരെയധികം ലോഗിൻ ശ്രമങ്ങൾ. ദയവായി സെക്കൻഡുകൾക്കുള്ളിൽ വീണ്ടും ശ്രമിക്കുക.',
+
+];
