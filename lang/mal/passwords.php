2b407ed0fee94c5a13a6bd5d87500af128e558fe
Alan Paul <57307037+Alanzz@users.noreply.github.com>
2022-10-22 21:35:54 +0530

    Malayalam language support for passwords

---
+++ lang/mal/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password'  => 'പാസ്‌വേഡ് കുറഞ്ഞത് ആറ് പ്രതീകങ്ങളെങ്കിലും ദൈർഘ്യമുള്ളതും സ്ഥിരീകരണവുമായി പൊരുത്തപ്പെടുന്നതുമായിരിക്കണം.',
+    'reset'     => 'നിങ്ങളുടെ പാസ്വേഡ് മാറ്റി!',
+    'sent'      => 'നിങ്ങളുടെ പാസ്‌വേഡ് പുനഃസജ്ജീകരണ ലിങ്ക് ഞങ്ങൾ ഇമെയിൽ ചെയ്തു!',
+    'token'     => 'ഈ പാസ്‌വേഡ് റീസെറ്റ് ടോക്കൺ അസാധുവാണ്.',
+    'user'      => 'ആ ഇ-മെയിൽ വിലാസമുള്ള ഒരു ഉപയോക്താവിനെ കണ്ടെത്താൻ ഞങ്ങൾക്ക് കഴിയുന്നില്ല.',
+
+];
