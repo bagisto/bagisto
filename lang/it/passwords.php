23b3ab16511ca30dcb9fc026870c0c14d0cfb5a9
Alberto Pasqualetto <39854348+albertopasqualetto@users.noreply.github.com>
2022-10-28 13:33:19 +0000

    Italian translation

---
+++ lang/it/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Le password devono avere almeno sei caratteri e corrispondere con la conferma.',
+    'reset' => 'La tua password è stata resettata!',
+    'sent' => 'Ti abbiamo inviato un link per il reset della tua password via mail',
+    'token' => 'Questo token per il reset della password non è valido',
+    'user' => "Non riusciamo a trovare un utente con quell' indirizzo email.",
+
+];
