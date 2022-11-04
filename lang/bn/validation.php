a055699f7c325745f2ca78b9027da9c54ce1e5c2
Sagnik Sahoo <sahoosagnik1@gmail.com>
2022-11-04 17:18:23 +0530

    Create validation.php

---
+++ lang/bn/validation.php
@@ -0,0 +1,146 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'accepted'             => ':attribute অবশ্যই গ্রহণ করতে হবে.',
+    'active_url'           => ':attribute একটি বৈধ URL নয়।',
+    'after'                => ':attribute অবশ্যই :date এর পরে একটি তারিখ হতে হবে৷',
+    'after_or_equal'       => ':attribute অবশ্যই :date এর পরে বা তার সমান হতে হবে৷',
+    'alpha'                => ':attribute শুধুমাত্র অক্ষর থাকতে পারে।',
+    'alpha_dash'           => ':attribute শুধুমাত্র অক্ষর, সংখ্যা, ড্যাশ এবং আন্ডারস্কোর থাকতে পারে।',
+    'alpha_num'            => ':attribute শুধুমাত্র অক্ষর এবং সংখ্যা থাকতে পারে।',
+    'array'                => ':attribute একটি অ্যারে হতে হবে।',
+    'before'               => ':attribute অবশ্যই :date এর আগে একটি তারিখ হতে হবে।',
+    'before_or_equal'      => ':attribute টি অবশ্যই :date এর আগে বা সমান একটি তারিখ হতে হবে।',
+    'between'              => [
+        'numeric' => ':attribute অবশ্যই :min এবং :max এর মধ্যে হতে হবে।',
+        'file'    => ':attribute অবশ্যই :min এবং :max কিলোবাইটের মধ্যে হতে হবে৷',
+        'string'  => ':attribute অবশ্যই :min এবং :max অক্ষরের মধ্যে হতে হবে।',
+        'array'   => ':attribute এর মধ্যে থাকতে হবে :min এবং :max আইটেম।',
+    ],
+    'boolean'              => ':attribute ক্ষেত্রটি সত্য বা মিথ্যা হতে হবে।',
+    'confirmed'            => ':attribute নিশ্চিতকরণ মেলে না।',
+    'date'                 => ':attribute একটি বৈধ তারিখ নয়।',
+    'date_format'          => ':attribute টি :format ফর্ম্যাটের সাথে মেলে না।',
+    'different'            => ':attribute এবং :other আলাদা হতে হবে।',
+    'digits'               => ':attribute অবশ্যই :digits সংখ্যা হতে হবে।.',
+    'digits_between'       => ':attribute অবশ্যই :min এবং :max সংখ্যার মধ্যে হতে হবে।',
+    'dimensions'           => ':attribute অবৈধ চিত্রের মাত্রা রয়েছে।',
+    'distinct'             => ':attribute ক্ষেত্রের একটি ডুপ্লিকেট মান আছে।',
+    'email'                => ':attribute একটি বৈধ ইমেল ঠিকানা হতে হবে।',
+    'exists'               => 'নির্বাচিত :attribute অবৈধ।',
+    'file'                 => ':attribute একটি ফাইল হতে হবে।',
+    'filled'               => ':attribute ক্ষেত্রের একটি মান থাকতে হবে।',
+    'gt'                   => [
+        'numeric' => ':attribute অবশ্যই :value এর থেকে বড় হতে হবে।',
+        'file'    => ':attribute অবশ্যই :value কিলোবাইটের চেয়ে বেশি হতে হবে।',
+        'string'  => ':attribute অবশ্যই :value অক্ষরের চেয়ে বড় হতে হবে।',
+        'array'   => ':attribute-এ অবশ্যই :value আইটেম এর থেকে বেশি থাকতে হবে।',
+    ],
+    'gte'                  => [
+        'numeric' => ':attribute অবশ্যই :value এর চেয়ে বড় বা সমান হতে হবে।',
+        'file'    => ':attribute অবশ্যই :value কিলোবাইটের চেয়ে বড় বা সমান হতে হবে৷',
+        'string'  => ':attribute অবশ্যই :value অক্ষরের চেয়ে বড় বা সমান হতে হবে।',
+        'array'   => ':attribute এর অবশ্যই :value আইটেম বা তার বেশি থাকতে হবে।',
+    ],
+    'image'                => ':attribute একটি ছবি হতে হবে।',
+    'in'                   => 'নির্বাচিত :attribute অবৈধ।',
+    'in_array'             => ':attribute ক্ষেত্রটি :other মধ্যে বিদ্যমান নেই।',
+    'integer'              => 'The :attribute must be an integer.',
+    'ip'                   => 'The :attribute must be a valid IP address.',
+    'ipv4'                 => 'The :attribute must be a valid IPv4 address.',
+    'ipv6'                 => 'The :attribute must be a valid IPv6 address.',
+    'json'                 => 'The :attribute must be a valid JSON string.',
+    'lt'                   => [
+        'numeric' => 'The :attribute must be less than :value.',
+        'file'    => 'The :attribute must be less than :value kilobytes.',
+        'string'  => 'The :attribute must be less than :value characters.',
+        'array'   => 'The :attribute must have less than :value items.',
+    ],
+    'lte'                  => [
+        'numeric' => 'The :attribute must be less than or equal :value.',
+        'file'    => 'The :attribute must be less than or equal :value kilobytes.',
+        'string'  => 'The :attribute must be less than or equal :value characters.',
+        'array'   => 'The :attribute must not have more than :value items.',
+    ],
+    'max'                  => [
+        'numeric' => 'The :attribute may not be greater than :max.',
+        'file'    => 'The :attribute may not be greater than :max kilobytes.',
+        'string'  => 'The :attribute may not be greater than :max characters.',
+        'array'   => 'The :attribute may not have more than :max items.',
+    ],
+    'mimes'                => 'The :attribute must be a file of type: :values.',
+    'mimetypes'            => 'The :attribute must be a file of type: :values.',
+    'min'                  => [
+        'numeric' => 'The :attribute must be at least :min.',
+        'file'    => 'The :attribute must be at least :min kilobytes.',
+        'string'  => 'The :attribute must be at least :min characters.',
+        'array'   => 'The :attribute must have at least :min items.',
+    ],
+    'not_in'               => 'The selected :attribute is invalid.',
+    'not_regex'            => 'The :attribute format is invalid.',
+    'numeric'              => 'The :attribute must be a number.',
+    'present'              => 'The :attribute field must be present.',
+    'regex'                => 'The :attribute format is invalid.',
+    'required'             => 'The :attribute field is required.',
+    'required_if'          => 'The :attribute field is required when :other is :value.',
+    'required_unless'      => 'The :attribute field is required unless :other is in :values.',
+    'required_with'        => 'The :attribute field is required when :values is present.',
+    'required_with_all'    => 'The :attribute field is required when :values is present.',
+    'required_without'     => 'The :attribute field is required when :values is not present.',
+    'required_without_all' => 'The :attribute field is required when none of :values are present.',
+    'same'                 => 'The :attribute and :other must match.',
+    'size'                 => [
+        'numeric' => 'The :attribute must be :size.',
+        'file'    => 'The :attribute must be :size kilobytes.',
+        'string'  => 'The :attribute must be :size characters.',
+        'array'   => ':attribute এ অবশ্যই :size আইটেম থাকতে হবে।.',
+    ],
+    'string'               => ':attribute একটি স্ট্রিং হতে হবে।',
+    'timezone'             => ':attribute অবশ্যই একটি বৈধ অঞ্চল হতে হবে।',
+    'unique'               => ':attribute ইতিমধ্যে নেওয়া হয়েছে।',
+    'uploaded'             => ':attribute আপলোড করতে ব্যর্থ হয়েছে।',
+    'url'                  => ':attribute বিন্যাসটি অবৈধ।',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
+
+    'custom' => [
+        'attribute-name' => [
+            'rule-name' => 'custom-message',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap attribute place-holders
+    | with something more reader friendly such as E-Mail Address instead
+    | of "email". This simply helps us make messages a little cleaner.
+    |
+    */
+
+    'attributes' => [],
+
+];
