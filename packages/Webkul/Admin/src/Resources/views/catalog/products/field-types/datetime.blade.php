b2834e22b8567b2359103c865a4cd7c07154c9ac
Devansh <68321766+devansh-webkul@users.noreply.github.com>
2023-02-13 13:17:38 +0530

    Update datetime.blade.php

--- packages/Webkul/Admin/src/Resources/views/catalog/products/field-types/datetime.blade.php
+++ packages/Webkul/Admin/src/Resources/views/catalog/products/field-types/datetime.blade.php
@@ -4,7 +4,7 @@
         name="{{ $attribute->code }}"
         value="{{  old($attribute->code) ?: $product[$attribute->code]}}"
         class="control"
-        v-validate="{{ $attribute->is_required ? 'required' : '' }}"
+        v-validate="'{{ $attribute->is_required ? 'required' : '' }}'"
         data-vv-as="&quot;{{ $attribute->admin_name }}&quot;"
     >
 </datetime>
