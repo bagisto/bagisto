673749332a0243326b1a69c2175c97f681139e87
suraj kashyap <suraj.kashyap370@webkul.in>
2023-02-16 17:01:25 +0530

    Issue #7295 Fixed.

--- packages/Webkul/Admin/src/Resources/views/layouts/anonymous-master.blade.php
+++ packages/Webkul/Admin/src/Resources/views/layouts/anonymous-master.blade.php
@@ -6,6 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="base-url" content="{{ url()->to('/') }}">
 
         @if ($favicon = core()->getConfigData('general.design.admin_logo.favicon'))
             <link rel="icon" sizes="16x16" href="{{ \Illuminate\Support\Facades\Storage::url($favicon) }}" />
