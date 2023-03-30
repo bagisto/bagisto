61c987313c55c8a5adc28ae0bbad976c13d9cb5d
Shivam Tiwari <shivam.tiwari243@webkul.in>
2023-03-30 18:27:57 +0530

    #7332 issue fixed

--- packages/Webkul/Velocity/src/Resources/views/shop/layouts/particals/header-compts.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/layouts/particals/header-compts.blade.php
@@ -3,6 +3,11 @@
     items-count-src="{{ route('velocity.product.item-count') }}"
 ></header-component>
 
+@php
+$showCompare = (bool) core()->getConfigData('general.content.shop.compare_option');
+$showWishlist = (bool) core()->getConfigData('general.content.shop.wishlist_option');
+@endphp
+
 @push('scripts')
     <script type="text/x-template" id='header-component-template'>
         <div>
@@ -10,6 +15,7 @@
                 is-customer="{{ auth()->guard('customer')->check() ? 'true' : 'false' }}"
                 is-text="{{ isset($isText) && $isText ? 'true' : 'false' }}"
                 src="{{ route('shop.customer.wishlist.index') }}"
+                v-if="{{$showWishlist ? 'true' : 'false' }}"
                 :wishlist-item-count='wishlistCount'>
             </wishlist-component-with-badge>
 
@@ -17,6 +23,7 @@
                 is-customer="{{ auth()->guard('customer')->check() ? 'true' : 'false' }}"
                 is-text="{{ isset($isText) && $isText ? 'true' : 'false' }}"
                 src="{{ auth()->guard('customer')->check() ? route('velocity.customer.product.compare') : route('velocity.product.compare') }}"
+                v-if="{{$showCompare ? 'true' : 'false' }}"
                 :compare-item-count='compareCount'>
             </compare-component-with-badge>
             
