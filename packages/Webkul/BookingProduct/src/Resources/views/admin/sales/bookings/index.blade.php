f14ac81631bb97442970cc2b7cbf3e1ec4032577
Devansh <68321766+devansh-webkul@users.noreply.github.com>
2023-03-31 16:34:40 +0530

    Update index.blade.php

--- packages/Webkul/BookingProduct/src/Resources/views/admin/sales/bookings/index.blade.php
+++ packages/Webkul/BookingProduct/src/Resources/views/admin/sales/bookings/index.blade.php
@@ -11,10 +11,10 @@
         .grid-container .datagrid-filters .filter-right {
             grid-template-columns: auto auto auto;
         }
+        
         @media only screen and (max-width: 768px) {
-        .vuecal__no-event {
-            padding-top: 0rem!important;
-
+            .vuecal__no-event {
+                padding-top: 0rem!important;
             }
         }
     </style>
