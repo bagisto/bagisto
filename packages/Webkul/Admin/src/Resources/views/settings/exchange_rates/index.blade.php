5cd0b70b9f456255e37b33224202f40f25273175
shivendra gupta <you@example.com>
2023-04-03 16:54:55 +0530

    fixed #7431

--- packages/Webkul/Admin/src/Resources/views/settings/exchange_rates/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/settings/exchange_rates/index.blade.php
@@ -12,9 +12,12 @@
             </div>
 
             <div class="page-action">
-                <a href="{{ route('admin.exchange_rates.update_rates') }}" class="btn btn-lg btn-primary">
-                    {{ __('admin::app.settings.exchange_rates.update-rates') }}
-                </a>
+                @if(core()->getAllCurrencies()->count() > 1) 
+                    <a href="{{ route('admin.exchange_rates.update_rates') }}" class="btn btn-lg btn-primary">
+                        {{ __('admin::app.settings.exchange_rates.update-rates') }}
+                    </a>
+                @endif
+                    
                 @if (bouncer()->hasPermission('settings.exchange_rates.create'))
                     <a href="{{ route('admin.exchange_rates.create') }}" class="btn btn-lg btn-primary">
                         {{ __('admin::app.settings.exchange_rates.add-title') }}
