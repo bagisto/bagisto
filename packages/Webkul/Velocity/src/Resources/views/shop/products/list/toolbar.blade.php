327ebd22e5aa1b64ee5957f8e040f1f4ec94ef38
peeyush-webkul <peeyushsengar.laravel212@webkul.in>
2023-04-14 16:04:58 +0530

    filter width fixed

--- packages/Webkul/Velocity/src/Resources/views/shop/products/list/toolbar.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/products/list/toolbar.blade.php
@@ -88,14 +88,14 @@
                 @endif
             </div>
 
-            <div class="col-2" @click="toggleLayeredNavigation({event: $event, actionType: 'open'})">
+            <div class="col-2 col-filter" @click="toggleLayeredNavigation({event: $event, actionType: 'open'})">
                 <a class="unset">
                     <i class="material-icons">filter_list</i>
                     <span>{{ __('velocity::app.shop.general.filter') }}</span>
                 </a>
             </div>
 
-            <div class="col-5">
+            <div class="col-5 col-order">
                 <div class="sorter" id="sort-by">
 
                     <select class="selective-div border-normal styled-select" onchange="window.location.href = this.value">
@@ -112,7 +112,7 @@
                 </div>
             </div>
 
-            <div class="col-3">
+            <div class="col-3 col-count">
                 <div class="limiter" id="limit-by">
 
                     <select class="selective-div border-normal styled-select" onchange="window.location.href = this.value" style="width: 57px;" aria-label="Show">
@@ -133,7 +133,7 @@
                 </div>
             </div>
 
-            <div class="col-2">
+            <div class="col-2 col-view">
                 @php
                     $isList = $toolbarHelper->isModeActive('list');
                 @endphp
