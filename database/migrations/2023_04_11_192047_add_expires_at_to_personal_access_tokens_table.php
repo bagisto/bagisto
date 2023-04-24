9e81702bd68697959a82c0a9fc6798ef963cec41
adhamiamirhossein <adhamiamirhossein@gmail.com>
2023-04-24 13:13:52 +0330

    fixed - personal access token migration and removed old assets from resources

--- database/migrations/2023_04_11_192047_add_expires_at_to_personal_access_tokens_table.php
+++ database/migrations/2023_04_11_192047_add_expires_at_to_personal_access_tokens_table.php
@@ -13,7 +13,9 @@ return new class extends Migration {
     public function up()
     {
         Schema::table('personal_access_tokens', function (Blueprint $table) {
-            $table->timestamp('expires_at')->nullable()->after('last_used_at');
+            if (! Schema::hasColumn('personal_access_tokens', 'expires_at')) {
+                $table->timestamp('expires_at')->nullable()->after('last_used_at');
+            }
         });
     }
 
@@ -25,7 +27,9 @@ return new class extends Migration {
     public function down()
     {
         Schema::table('personal_access_tokens', function (Blueprint $table) {
-            $table->dropColumn('expires_at');
+            if (Schema::hasColumn('personal_access_tokens', 'expires_at')) {
+                $table->dropColumn('expires_at');
+            }
         });
     }
 };
--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,43 +1,13 @@
-import _ from 'lodash'
-window._ = _;
-import Popper from "popper.js";
-window.Popper = Popper.default;
-import $ from 'jquery'
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-try {
-    window.$ = window.jQuery = $;
-
-} catch (e) {}
-
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from "axios";
+import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Next we will register the CSRF Token as a common header with Axios so that
- * all outgoing HTTP requests automatically have it attached. This is just
- * a simple convenience so we don't have to attach every token manually.
- */
-
-let token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
