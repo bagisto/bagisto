95d8ac8a191a567ba3fa084c947227fb90becf18
jitendra <jitendra@webkul.com>
2023-04-28 12:10:38 +0530

    Product blade component added

---
+++ packages/Webkul/Shop/src/Resources/views/components/products/card.blade.php
@@ -0,0 +1,29 @@
+<div class="bs-single-card relative min-w-[291px]">
+    <div class="">
+        <img class="" src="{{ bagisto_asset('/images/mens-collections.jpg') }}">
+    </div>
+    <div class="action-items">
+        <p
+            class="rounded-[44px] text-[#fff] text-[14px] px-[10px] bg-navyBlue inline-block absolute top-[20px] left-[20px]">
+            New</p>
+        <span
+            class=" flex justify-center items-center w-[30px] h-[30px] bg-white rounded-md cursor-pointer absolute top-[20px] right-[20px] before:content-[' '] before:bg-[position:-170px_-65px] before:bs-main-sprite before:w-[21px] before:h-[20px] before:block"></span>
+        <span
+            class=" flex justify-center items-center w-[30px] h-[30px] bg-white rounded-md cursor-pointer absolute top-[60px] right-[20px] before:content-[' '] before:bg-[position:-98px_-90px] before:bs-main-sprite before:w-[21px] before:h-[20px] before:block"></span>
+        <div
+            class="rounded-xl bg-white text-navyBlue text-xs w-max font-medium py-[11px] px-[43px]  absolute top-[244px] left-[50%] -translate-x-[50%]">
+            Add to cart</div>
+    </div>
+
+    <p class="text-base">Slim High Ankle Jeans</p>
+    
+    <div class="price-block">
+        <p class="offer-price">$20.00</p>
+        <p class="original-price">$30.00</p>
+    </div>
+
+    <div class="change-card-color">
+        <span class="bg-[#B5DCB4] active"></span>
+        <span class="bg-[#5C5C5C]"></span>
+    </div>
+</div>
