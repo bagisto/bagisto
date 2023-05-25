0ad687905688533fa4bfb884595f38b004cfc2b9
Devansh <68321766+devansh-webkul@users.noreply.github.com>
2023-05-25 15:48:44 +0530

    Update app.php

--- packages/Webkul/Shop/src/Resources/lang/en/app.php
+++ packages/Webkul/Shop/src/Resources/lang/en/app.php
@@ -47,7 +47,6 @@ return [
 
     ],
 
-
     'invalid_vat_format' => 'The given vat id has a wrong format',
     'security-warning'   => 'Suspicious activity found!!!',
     'nothing-to-delete'  => 'Nothing to delete',
