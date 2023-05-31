d164811a6b4c6bdcc44b3f0d70f89dd291004655
Amit kumar <121479029+amit-webkul@users.noreply.github.com>
2023-05-31 13:59:07 +0530

    Update system.php

--- packages/Webkul/SocialShare/src/Config/system.php
+++ packages/Webkul/SocialShare/src/Config/system.php
@@ -8,36 +8,36 @@ return [
         'fields' => [
             [
                 'name'  => 'enabled',
-                'title' => trans('SocialShare::app.config.system.enable-social-share'),
+                'title' => 'SocialShare::app.config.system.enable-social-share',
                 'type'  => 'boolean',
             ], [
                 'name'  => 'facebook',
-                'title' => trans('SocialShare::app.config.system.enable-share-facebook'),
+                'title' => 'SocialShare::app.config.system.enable-share-facebook',
                 'type'  => 'boolean',
             ], [
                 'name'  => 'twitter',
-                'title' => trans('SocialShare::app.config.system.enable-share-twitter'),
+                'title' => 'SocialShare::app.config.system.enable-share-twitter',
                 'type'  => 'boolean',
             ], [
                 'name'  => 'pinterest',
-                'title' => trans('SocialShare::app.config.system.enable-share-pintrest'),
+                'title' => 'SocialShare::app.config.system.enable-share-pintrest',
                 'type'  => 'boolean',
             ], [
                 'name'  => 'whatsapp',
-                'title' => trans('SocialShare::app.config.system.enable-share-whatsapp'),
+                'title' => 'SocialShare::app.config.system.enable-share-whatsapp',
                 'type'  => 'boolean',
                 'info'  => 'What\'s App share link just will appear to mobile devices.'
             ], [
                 'name'  => 'linkedin',
-                'title' => trans('SocialShare::app.config.system.enable-share-linkedin'),
+                'title' => 'SocialShare::app.config.system.enable-share-linkedin',
                 'type'  => 'boolean',
             ], [
                 'name'  => 'email',
-                'title' => trans('SocialShare::app.config.system.enable-share-email'),
+                'title' => 'SocialShare::app.config.system.enable-share-email',
                 'type'  => 'boolean',
             ], [
                 'name'  => 'share_message',
-                'title' => trans('SocialShare::app.config.system.share-message'),
+                'title' => 'SocialShare::app.config.system.share-message',
                 'type'  => 'text',
             ],
         ],
