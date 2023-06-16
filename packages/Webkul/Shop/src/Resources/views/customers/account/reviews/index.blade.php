07c19c5ccf19a1ea653f5aa1046b5758ba5aa520
Devansh Bawari <devansh.bawari419@webkul.in>
2023-06-16 17:19:04 +0530

    some changes done

--- packages/Webkul/Shop/src/Resources/views/customers/account/reviews/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/customers/account/reviews/index.blade.php
@@ -5,12 +5,12 @@
                 <p class="flex items-center gap-x-[4px] text-[#7D7D7D] text-[16px] after:content-['/']">
                     @lang('shop::app.customers.account.title')
                 </p>
-                
+
                 <p class="flex items-center gap-x-[4px] text-[#7D7D7D] text-[16px] after:content-['/'] after:last:hidden">
                     @lang('shop::app.customers.account.reviews.title')
                 </p>
             </div>
-            
+
             <h2 class="text-[26px] font-medium">
                 @lang('shop::app.customers.account.reviews.title')
             </h2>
@@ -18,8 +18,8 @@
             @if (! $reviews->isEmpty())
                 <div class="grid mt-[60px] gap-[20px] max-1060:grid-cols-[1fr]">
                     @foreach($reviews as $review)
-                        <a 
-                            href="{{route('shop.productOrCategory.index', $review->product->url_key) }}" 
+                        <a
+                            href="{{ route('shop.productOrCategory.index', $review->product->url_key) }}"
                             id="{{ $review->product_id }}"
                         >
                             <div class="flex gap-[20px] border border-[#e5e5e5] rounded-[12px] p-[25px] max-sm:flex-wrap">
@@ -42,9 +42,9 @@
                                                     <span class="bg-[position:-151px_-229px] bs-main-sprite w-[14px] h-[14px]"></span>
                                                 @endfor
                                             @endif
-                                        
+
                                             @php $remaining_stars = 5 - $review->rating; @endphp
-                                        
+
                                             {{-- For Inactive stars --}}
                                             @if ($remaining_stars)
                                                 @for($i = 1; $i <= $remaining_stars; $i++)
@@ -57,7 +57,7 @@
                                     <p class="text-[14px] font-medium mt-[10px] max-sm:text-[12px]">
                                         {{ $review->created_at }}
                                     </p>
-                                    
+
                                     <p class="text-[16px] text-[#7D7D7D] mt-[20px] max-sm:text-[12px]">
                                         {{ $review->comment }}
                                     </p>
@@ -69,7 +69,7 @@
             @else
                 <div class="grid items-center justify-items-center w-max m-auto h-[476px] place-content-center">
                     <img class="" src="{{ bagisto_asset('images/review.png') }}" alt="" title="">
-                    
+
                     <p class="text-[20px]">
                         @lang('shop::app.customers.account.reviews.empty-review')
                     </p>
