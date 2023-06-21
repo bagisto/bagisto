5834f15d1a0c23691b64c94179c5860fe935999d
Shivendra <shivendra.gupta423@webkul.in>
2023-06-21 19:56:20 +0530

    simmer issue fixed

--- packages/Webkul/Shop/src/Resources/views/components/shimmer/categories/view.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/shimmer/categories/view.blade.php
@@ -7,11 +7,14 @@
         <div class="flex-1">
             {{-- Toolbar Shimmer Effect --}}
             <x-shop::shimmer.categories.toolbar></x-shop::shimmer.categories.toolbar>
-
-            <div class="grid grid-cols-3 gap-8 mt-[30px] max-sm:mt-[20px] max-1060:grid-cols-2 max-868:grid-cols-1 max-sm:justify-items-center">
-                {{-- Product Card Shimmer Effect --}}
-                <x-shop::shimmer.products.cards.grid count="12"></x-shop::shimmer.products.cards.grid>
-            </div>
+            @if(request()->query('mode') =='grid')
+                <div class="grid grid-cols-3 gap-8 mt-[30px] max-sm:mt-[20px] max-1060:grid-cols-2 max-868:grid-cols-1 max-sm:justify-items-center">
+                    {{-- Product Card Shimmer Effect --}}
+                        <x-shop::shimmer.products.cards.grid count="12"></x-shop::shimmer.products.cards.grid> 
+                </div> 
+            @else
+                <x-shop::shimmer.products.cards.list count="12"></x-shop::shimmer.products.cards.list>
+            @endif
 
             <button class="w-[171.516px] h-[48px] block mx-auto py-[11px] rounded-[18px] mt-[60px] shimmer"></button>
         </div>
