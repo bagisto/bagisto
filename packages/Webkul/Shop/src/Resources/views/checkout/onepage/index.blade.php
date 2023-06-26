2ba39b7f1155fcabe980b0d1b6e36258ebd051d2
suraj kashyap <suraj.kashyap370@webkul.in>
2023-06-26 20:38:41 +0530

    add top nav.

--- packages/Webkul/Shop/src/Resources/views/checkout/onepage/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/checkout/onepage/index.blade.php
@@ -3,6 +3,32 @@
     :has-feature="false"
     :has-footer="false"
 >
+    <div class="bs-dekstop-menu flex flex-wrap max-lg:hidden">
+        <div
+            class="w-full flex justify-between px-[60px] border border-t-0 border-b-[1px] border-l-0 border-r-0 pb-[5px] pt-[17px]"
+        >
+            <div class="flex items-center gap-x-[54px] max-[1180px]:gap-x-[35px]">
+                <a
+                    href="{{ route('shop.home.index') }}" 
+                    class="bs-logo bg-[position:-5px_-3px] bs-main-sprite w-[131px] h-[29px] inline-block mb-[16px]"
+                >
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="bs-mobile-menu flex-wrap hidden max-lg:flex px-[15px]gap-[15px] max-lg:mb-[15px]">
+        <div class="bs-mobile-menu flex-wrap hidden max-lg:flex px-[15px] pt-[25px] gap-[15px] max-lg:mb-[15px]">
+            <div class="w-full flex justify-between items-center px-[6px]">
+                <div class="flex  items-center gap-x-[5px]">
+                    <span class="icon-hamburger text-[24px]"></span>
+        
+                    <a herf="" class="bs-logo bg-[position:-5px_-3px] bs-main-sprite w-[131px] h-[29px] inline-block"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
     {{-- Checkout component --}}
     {{-- Todo (@suraj-webkul): need change translation of this page.  --}}
     {{-- @translations --}}
