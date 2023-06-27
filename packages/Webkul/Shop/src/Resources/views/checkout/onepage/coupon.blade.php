351a8acf197084e82f30f8bbedd8c1c62fa3c4bc
amit kumar laravel <amitkumar.laravel358@webkul.in>
2023-06-27 21:01:58 +0530

    Add check on onpage coupon

--- packages/Webkul/Shop/src/Resources/views/checkout/onepage/coupon.blade.php
+++ packages/Webkul/Shop/src/Resources/views/checkout/onepage/coupon.blade.php
@@ -10,73 +10,72 @@
             <p class="text-[16px] max-sm:text-[14px] max-sm:font-normal">@lang('shop::app.checkout.cart.coupon.discount')</p>
             
             <p class="text-[16px] max-sm:text-[14px] max-sm:font-medium font-medium cursor-pointer">
-                <x-shop::modal>
-                    <x-slot:toggle>
-                        <span 
-                            class="text-[#0A49A7]" 
-                            v-if="! isCouponApplied"
-                        >
-                            @lang('shop::app.checkout.cart.coupon.apply')
-                        </span>
-                    </x-slot:toggle>
-
-                    <x-slot:header>
-                        <h2 class="text-[25px] font-medium max-sm:text-[22px]">@lang('Apply Coupon')</h2>
-                    </x-slot:header>
-
-                    <x-slot:content>
-                       
-                        <x-shop::form
-                            v-slot="{ meta, errors, handleSubmit }"
-                            as="div"
-                        >
-                            <form @submit="handleSubmit($event, applyCoupon)">
-                                <x-shop::form.control-group>
-                                    <div class="p-[30px] bg-white">
-                                        <x-shop::form.control-group.control
-                                            type="text"
-                                            name="code"
-                                            class="text-[14px] appearance-none border rounded-[12px] w-full py-[20px] px-[25px] focus:outline-none focus:shadow-outline"
-                                            placeholder="Enter your code"
-                                            rules="required"
-                                            v-model="code"
-                                        >
-                                        </x-shop::form.control-group.control>
-
-                                        <x-shop::form.control-group.error
-                                            control-name="code"
-                                        >
-                                        </x-shop::form.control-group.error>
-                                    </div>
-                                </x-shop::form.control-group>
-
-                                <div class="p-[30px] bg-white mt-[20px]">
-                                    <div class="flex justify-between items-center gap-[15px] flex-wrap">
-                                        <p class="text-[14px] font-medium text-[#7D7D7D]">@lang('Subtotal')</p>
-                                        <div class="flex gap-[30px] items-center flex-auto flex-wrap">
-                                            <p 
-                                                class="text-[30px] font-semibold max-sm:text-[22px]"
-                                                v-text="subTotal"
+                <span v-if="! isCouponApplied">
+                    <x-shop::modal>
+                        <x-slot:toggle>
+                            <span class="text-[#0A49A7]">
+                                @lang('shop::app.checkout.cart.coupon.apply')
+                            </span>
+                        </x-slot:toggle>
+    
+                        <x-slot:header>
+                            <h2 class="text-[25px] font-medium max-sm:text-[22px]">@lang('Apply Coupon')</h2>
+                        </x-slot:header>
+    
+                        <x-slot:content>
+                           
+                            <x-shop::form
+                                v-slot="{ meta, errors, handleSubmit }"
+                                as="div"
+                            >
+                                <form @submit="handleSubmit($event, applyCoupon)">
+                                    <x-shop::form.control-group>
+                                        <div class="p-[30px] bg-white">
+                                            <x-shop::form.control-group.control
+                                                type="text"
+                                                name="code"
+                                                class="text-[14px] appearance-none border rounded-[12px] w-full py-[20px] px-[25px] focus:outline-none focus:shadow-outline"
+                                                placeholder="Enter your code"
+                                                rules="required"
+                                                v-model="code"
                                             >
-                                            </p>
-
-                                            <button
-                                                class="block flex-auto bg-navyBlue text-white text-base w-max font-medium py-[11px] px-[43px] rounded-[18px] text-center cursor-pointer max-sm:text-[14px] max-sm:px-[25px]"
-                                                type="submit"
+                                            </x-shop::form.control-group.control>
+    
+                                            <x-shop::form.control-group.error
+                                                control-name="code"
                                             >
-                                               @lang('shop::app.customers.account.save')
-                                            </button>
+                                            </x-shop::form.control-group.error>
+                                        </div>
+                                    </x-shop::form.control-group>
+    
+                                    <div class="p-[30px] bg-white mt-[20px]">
+                                        <div class="flex justify-between items-center gap-[15px] flex-wrap">
+                                            <p class="text-[14px] font-medium text-[#7D7D7D]">@lang('Subtotal')</p>
+                                            <div class="flex gap-[30px] items-center flex-auto flex-wrap">
+                                                <p 
+                                                    class="text-[30px] font-semibold max-sm:text-[22px]"
+                                                    v-text="subTotal"
+                                                >
+                                                </p>
+    
+                                                <button
+                                                    class="block flex-auto bg-navyBlue text-white text-base w-max font-medium py-[11px] px-[43px] rounded-[18px] text-center cursor-pointer max-sm:text-[14px] max-sm:px-[25px]"
+                                                    type="submit"
+                                                >
+                                                   @lang('shop::app.customers.account.save')
+                                                </button>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </form>
-                        </x-shop::form>
-                    </x-slot:content>
-                </x-shop::modal>
+                                </form>
+                            </x-shop::form>
+                        </x-slot:content>
+                    </x-shop::modal>
+                </span>
 
                 <div 
                     class="text-[12px] font-small flex justify-between items-center"
-                    v-if="isCouponApplied"
+                    v-else
                 >
                     <p class="text-[12px] mr-2">@lang('Coupon applied')</p>
                     
