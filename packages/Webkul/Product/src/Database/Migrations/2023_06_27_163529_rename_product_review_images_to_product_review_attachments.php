06ee00d5017a5a86f4c55876ec31187bea791e76
Shivendra <shivendra.gupta423@webkul.in>
2023-06-27 19:32:07 +0530

    fixed delete issue

--- packages/Webkul/Product/src/Database/Migrations/2023_06_27_163529_rename_product_review_images_to_product_review_attachments.php
+++ packages/Webkul/Product/src/Database/Migrations/2023_06_27_163529_rename_product_review_images_to_product_review_attachments.php
@@ -15,14 +15,9 @@ return new class extends Migration
     {
         Schema::table('product_review_images', function (Blueprint $table) {
             $table->string('type')->default('image')->change();
-            $table->dropForeign(['review_id']);
         });
 
         Schema::rename('product_review_images', 'product_review_attachments');
-
-        Schema::table('product_review_attachments', function (Blueprint $table) {
-            $table->foreign('review_id')->references('id')->on('product_reviews');
-        });
     }
 
     /**
@@ -30,16 +25,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('product_review_attachments', function (Blueprint $table) {
-            $table->dropForeign(['review_id']);
+        Schema::table('product_review_images', function (Blueprint $table) {
+            $table->text('type')->nullable()->change();
         });
 
         Schema::rename('product_review_attachments', 'product_review_images');
-
-        Schema::table('product_review_images', function (Blueprint $table) {
-            $table->text('type')->nullable()->change();
-
-            $table->foreign('review_id')->references('id')->on('product_reviews');
-        });
     }
 };
