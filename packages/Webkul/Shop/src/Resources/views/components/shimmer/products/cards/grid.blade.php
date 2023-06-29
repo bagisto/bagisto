f366e1de16bda1ddf60f5e22f898880b4e0b69fd
Jitendra <jitendra@webkul.in>
2023-06-29 11:13:13 +0530

    Fixed product listing shimmer ui issue

--- packages/Webkul/Shop/src/Resources/views/components/shimmer/products/cards/grid.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/shimmer/products/cards/grid.blade.php
@@ -1,17 +1,13 @@
 @props(['count' => 0])
 
 @for ($i = 0;  $i < $count; $i++)
-    <div class="grid gap-2.5 content-start relative max-sm:grid-cols-1 max-w-[291px] {{ $attributes["class"] }}">
-        <div class="relative overflow-hidden  rounded-sm min-h-[300px] bg-[#E9E9E9] shimmer">
-            <img
-                src=""
-                class="rounded-sm bg-[#F5F5F5]"
-            >
+    <div class="grid gap-2.5 relative max-sm:grid-cols-1 w-full max-w-[291px] {{ $attributes["class"] }}">
+        <div class="relative w-full rounded-sm bg-[#E9E9E9] shimmer">
+            <div class="relative after:content-[' '] after:block after:pb-[100%] "></div>
         </div>
 
         <div class="grid gap-2.5 content-start">
             <p class="w-[75%] h-[24px] bg-[#E9E9E9] shimmer"></p>
-
             <p class="w-[55%] h-[24px] bg-[#E9E9E9] shimmer"></p>
 
             <div class="flex gap-4 mt-[12px]">
