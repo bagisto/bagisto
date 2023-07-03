b59cea53e988796e61020bd4a92801927795945b
Jitendra <jitendra@webkul.in>
2023-07-03 14:22:34 +0530

    Fixed checkout shimmer issue

--- packages/Webkul/Shop/src/Resources/views/components/shimmer/checkout/onepage/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/shimmer/checkout/onepage/index.blade.php
@@ -1,16 +1,14 @@
-<div class="container px-[60px] max-lg:px-[30px] max-sm:px-[15px]">
-    <div class="grid grid-cols-[1fr_auto] gap-[30px] max-lg:grid-cols-[1fr]">
-        <div>
-            {{-- Billing Address --}}
-            <x-shop::shimmer.checkout.onepage.address></x-shop::shimmer.checkout.onepage.address>
+<div class="grid grid-cols-[1fr_auto] gap-[30px] max-lg:grid-cols-[1fr]">
+    <div>
+        {{-- Billing Address --}}
+        <x-shop::shimmer.checkout.onepage.address></x-shop::shimmer.checkout.onepage.address>
 
-            {{-- Shipping method --}}
-            <x-shop::shimmer.checkout.onepage.shipping-method></x-shop::shimmer.checkout.onepage.shipping-method>
+        {{-- Shipping method --}}
+        <x-shop::shimmer.checkout.onepage.shipping-method></x-shop::shimmer.checkout.onepage.shipping-method>
 
-            {{-- Payment method --}}
-            <x-shop::shimmer.checkout.onepage.payment-method></x-shop::shimmer.checkout.onepage.payment-method>
-        </div>
-    
-        <x-shop::shimmer.checkout.onepage.cart-summary></x-shop::shimmer.checkout.onepage.cart-summary>
+        {{-- Payment method --}}
+        <x-shop::shimmer.checkout.onepage.payment-method></x-shop::shimmer.checkout.onepage.payment-method>
     </div>
+
+    <x-shop::shimmer.checkout.onepage.cart-summary></x-shop::shimmer.checkout.onepage.cart-summary>
 </div>
