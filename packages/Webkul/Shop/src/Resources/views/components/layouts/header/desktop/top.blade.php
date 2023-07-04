58300d93ec2bc7dbb9e498d30ea75643619ea1ca
suraj kashyap <suraj.kashyap370@webkul.in>
2023-07-04 11:29:18 +0530

    Code refactor.

--- packages/Webkul/Shop/src/Resources/views/components/layouts/header/desktop/top.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/layouts/header/desktop/top.blade.php
@@ -27,16 +27,14 @@
     }
 @endphp
 
-<div
-    class="flex justify-between items-center w-full border border-t-0 border-b-[1px] border-l-0 border-r-0 py-[11px] px-16"
->
+<div class="flex justify-between items-center w-full border border-t-0 border-b-[1px] border-l-0 border-r-0 py-[11px] px-16">
     <x-shop::dropdown>
         <x-slot:toggle>
             <div class="flex">
                 <span class="cursor-pointer">{{ core()->getCurrentCurrency()->symbol }}</span>
 
                 <span class="ml-2 cursor-pointer">
-                    {{core()->getCurrentCurrencyCode()}}
+                    {{ core()->getCurrentCurrencyCode() }}
                 </span>
             </div>
         </x-slot:toggle>
@@ -60,10 +58,20 @@
     <x-shop::dropdown>
         <x-slot:toggle>
             <div class="flex">
-                @if(! empty(core()->getCurrentLocale()->image_url))
-                    <img src="{{ core()->getCurrentLocale()->image_url }}" alt="" width="20" height="20" />
+                @if (! empty(core()->getCurrentLocale()->image_url))
+                    <img 
+                        src="{{ core()->getCurrentLocale()->image_url }}"
+                        alt="Default locale"
+                        width="20"
+                        height="20"
+                    />
                 @else
-                    <img src="{{ asset('/themes/velocity/assets/images/flags/default-locale-image.png') }}" alt="" width="20" height="20" />
+                    <img 
+                        src="{{ asset('/themes/velocity/assets/images/flags/default-locale-image.png') }}"
+                        alt="Default locale" 
+                        width="20"
+                        height="20"
+                    />
                 @endif
                 
                 <span class="ml-2 cursor-pointer">
