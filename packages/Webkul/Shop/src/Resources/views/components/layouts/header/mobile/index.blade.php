1dd46b2668a58139cdc3dc4c7ea58dfaf43ac135
suraj kashyap <suraj.kashyap370@webkul.in>
2023-07-04 19:15:22 +0530

    Add lang and currency changer and remove lenght>0.

--- packages/Webkul/Shop/src/Resources/views/components/layouts/header/mobile/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/layouts/header/mobile/index.blade.php
@@ -61,8 +61,74 @@
                         </div>
                     </a>
 
+                    <div class="flex justify-between">
+                        <x-shop::dropdown>
+                            <x-slot:toggle>
+                                <div class="flex">
+                                    <span class="cursor-pointer">{{ core()->getCurrentCurrency()->symbol }}</span>
+    
+                                    <span class="ml-2 cursor-pointer">
+                                        {{ core()->getCurrentCurrencyCode() }}
+                                    </span>
+                                </div>
+                            </x-slot:toggle>
+    
+                            <x-slot:content class="!p-[0px]">
+                                <div class="grid gap-[4px] mt-[10px] pb-[10px]">
+                                    @foreach (core()->getCurrentChannel()->currencies as $currency)
+                                        <a 
+                                            class="text-[16px] px-5 py-2 cursor-pointer hover:bg-gray-100 @if($currency->code == core()->getCurrentCurrencyCode()) bg-gray-100 @endif"
+                                            href="?currency={{ $currency->code }}"
+                                        >
+                                            {{ $currency->code }}
+                                        </a>
+                                    @endforeach
+                                </div>
+                            </x-slot:content>
+                        </x-shop::dropdown>
+
+                        <x-shop::dropdown>
+                            <x-slot:toggle>
+                                <div class="flex">
+                                    @if (! empty(core()->getCurrentLocale()->image_url))
+                                        <img 
+                                            src="{{ core()->getCurrentLocale()->image_url }}"
+                                            alt="Default locale"
+                                            width="20"
+                                            height="20"
+                                        />
+                                    @else
+                                        <img 
+                                            src="{{ asset('/themes/velocity/assets/images/flags/default-locale-image.png') }}"
+                                            alt="Default locale" 
+                                            width="20"
+                                            height="20"
+                                        />
+                                    @endif
+                                    
+                                    <span class="ml-2 cursor-pointer">
+                                        {{ core()->getCurrentChannel()->locales()->orderBy('name')->where('code', app()->getLocale())->value('name') }}
+                                    </span>
+                                </div>
+                            </x-slot:toggle>
+                        
+                            <x-slot:content class="!p-[0px]">
+                                <div class="grid gap-[4px] mt-[10px] pb-[10px]">
+                                    @foreach (core()->getCurrentChannel()->locales()->orderBy('name')->get() as $locale)
+                                        <a 
+                                            class="text-[16px] px-5 py-2 cursor-pointer hover:bg-gray-100 @if($locale->code == app()->getLocale()) bg-gray-100 @endif"
+                                            href="?locale={{ $locale->code }}"
+                                        >
+                                            {{ $locale->name }}
+                                        </a>
+                                    @endforeach
+                                </div>
+                            </x-slot:content>
+                        </x-shop::dropdown>
+                    </div>
+                  
                     <v-mobile-category></v-mobile-category>
-                    
+
                 </x-slot:content>
 
                 <x-slot:footer></x-slot:footer>
@@ -121,7 +187,7 @@
                     class="grid gap-[8px]"
                     v-if="category.category_show"
                 >
-                    <ul v-if="category.children.length > 0">
+                    <ul v-if="category.children.length">
                         <li v-for="child in category.children">
                             <div class="flex justify-between items-center">
                                 <a
