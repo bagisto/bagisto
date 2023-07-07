c1676ffbab2bc0af2958ad088133ec3de53f314b
Jitendra <jitendra@webkul.in>
2023-07-07 11:35:51 +0530

    Addes review shimmer and fixed shimmer ui issues

--- packages/Webkul/Shop/src/Resources/views/components/shimmer/categories/filters.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/shimmer/categories/filters.blade.php
@@ -1,14 +1,14 @@
 <div class="grid grid-cols-[1fr] panel-side max-w-[400px] gap-[20px] max-h-[1320px] overflow-y-auto overflow-x-hidden journal-scroll pr-[26px] min-w-[342px] max-xl:min-w-[270px] max-md:hidden">
     <div class="flex pb-[10px] border-b-[1px] border-[#E9E9E9] justify-between items-center h-[50px]">
-        <p class="w-[30%] h-[24px] bg-[#E9E9E9] shimmer"></p>
-        <p class="w-[20%] h-[20px] bg-[#E9E9E9] shimmer"></p>
+        <p class="w-[30%] h-[24px] shimmer"></p>
+        <p class="w-[20%] h-[20px] shimmer"></p>
     </div>
 
 
     <div class="border-b-[1px] border-[#E9E9E9]">
         <div class="flex pb-[10px] justify-between items-center">
-            <p class="h-[27px] w-[40%] bg-[#E9E9E9] shimmer"></p>
-            <span class="h-[24px] w-[24px] bg-[#E9E9E9] shimmer"></span>
+            <p class="h-[27px] w-[40%] shimmer"></p>
+            <span class="h-[24px] w-[24px] shimmer"></span>
         </div>
 
         <div class="z-10 bg-white rounded-lg">
@@ -18,19 +18,19 @@
 
     <div class="border-b-[1px] border-[#E9E9E9]">
         <div class="flex pb-[10px] justify-between items-center">
-            <p class="h-[27px] w-[40%] bg-[#E9E9E9] shimmer"></p>
-            <span class="h-[24px] w-[24px] bg-[#E9E9E9] shimmer"></span>
+            <p class="h-[27px] w-[40%] shimmer"></p>
+            <span class="h-[24px] w-[24px] shimmer"></span>
         </div>
 
         <div class="z-10 bg-white rounded-lg">
-            <ul class="pb-3 text-sm text-gray-700 grid gap-[16px]">
-                <li class="h-[24px] w-[60%] bg-[#E9E9E9] shimmer"></li>
-                <li class="h-[24px] w-[60%] bg-[#E9E9E9] shimmer"></li>
-                <li class="h-[24px] w-[60%] bg-[#E9E9E9] shimmer"></li>
-                <li class="h-[24px] w-[60%] bg-[#E9E9E9] shimmer"></li>
-                <li class="h-[24px] w-[60%] bg-[#E9E9E9] shimmer"></li>
-                <li class="h-[24px] w-[60%] bg-[#E9E9E9] shimmer"></li>
-                <li class="h-[24px] w-[60%] bg-[#E9E9E9] shimmer"></li>
+            <ul class="pb-3 grid gap-[16px]">
+                <li class="h-[24px] w-[60%] shimmer"></li>
+                <li class="h-[24px] w-[60%] shimmer"></li>
+                <li class="h-[24px] w-[60%] shimmer"></li>
+                <li class="h-[24px] w-[60%] shimmer"></li>
+                <li class="h-[24px] w-[60%] shimmer"></li>
+                <li class="h-[24px] w-[60%] shimmer"></li>
+                <li class="h-[24px] w-[60%] shimmer"></li>
             </ul>
         </div>
     </div>
