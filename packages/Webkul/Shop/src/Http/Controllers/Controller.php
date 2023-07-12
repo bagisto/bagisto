d6152b594658de15c1781d34f1dd4d399cc31b7f
amit kumar laravel <amitkumar.laravel358@webkul.in>
2023-07-12 13:36:18 +0530

    One more changes

--- packages/Webkul/Shop/src/Http/Controllers/Controller.php
+++ packages/Webkul/Shop/src/Http/Controllers/Controller.php
@@ -11,19 +11,11 @@ class Controller extends BaseController
     use DispatchesJobs, ValidatesRequests;
 
     /**
-     * Contains route related configuration
-     *
-     * @var array
-     */
-    protected $_config;
-
-    /**
      * Create a new controller instance.
      *
      * @return void
      */
     public function __construct()
     {
-        $this->_config = request('_config');
     }
 }
