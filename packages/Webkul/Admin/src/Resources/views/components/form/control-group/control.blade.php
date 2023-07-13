3eb194952114d68b9a87c36bad5bc248b4232c22
Shivendra <shivendra.gupta423@webkul.in>
2023-07-13 12:30:26 +0530

    manage sequence

--- packages/Webkul/Admin/src/Resources/views/components/form/control-group/control.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/form/control-group/control.blade.php
@@ -17,7 +17,7 @@
                 type="{{ $type }}"
                 :class="[errors['{{ $name }}'] ? 'border border-red-500' : '']"
                 v-bind="field"
-                {{ $attributes->except(['value'])->merge(['class' => 'text-[14px] text-gray-600 appearance-none border rounded-[6px] w-full py-2 px-3 transition-all hover:border-gray-400']) }}
+                {{ $attributes->except(['value'])->merge(['class' => 'border rounded-[6px] py-2 px-3 text-[14px] text-gray-600 appearance-none w-full transition-all hover:border-gray-400']) }}
             >
         </v-field>
 
@@ -74,7 +74,7 @@
             <select
                 v-bind="field"
                 :class="[errors['{{ $name }}'] ? 'border border-red-500' : '']"
-                {{ $attributes->except(['value'])->merge(['class' => 'custom-select text-[14px] text-gray-600 appearance-none border rounded-[6px] w-full py-2 px-3 transition-all hover:border-gray-400']) }}
+                {{ $attributes->except(['value'])->merge(['class' => 'border rounded-[6px] w-full py-2 px-3 text-[14px] text-gray-600 appearance-none transition-all hover:border-gray-400 custom-select']) }}
             >
                 {{ $slot }}
             </select>
