586f8c8c0e8b4c03209891cd7a04fecce1535748
amit kumar laravel <amitkumar.laravel358@webkul.in>
2023-07-19 19:43:14 +0530

    Update email marketing page with new tabs

--- packages/Webkul/Admin/src/Resources/views/marketing/email-marketing/templates/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/marketing/email-marketing/templates/index.blade.php
@@ -1,55 +1,24 @@
 <x-admin::layouts>
-    <x-admin::tabs 
-        position="left" 
-        class="!text-[12px]"
-    >
-        {{-- Email Template Tab --}}
-        <x-admin::tabs.item
-            class=""
-            :title="trans('admin::app.marketing.email-marketing.templates.title')"
-            :is-selected="true"
-        >
-            <div class="flex gap-[16px] justify-between items-center max-sm:flex-wrap">
-                <p class="text-[20px] text-gray-800 font-bold">
-                    @lang('admin::app.marketing.email-marketing.templates.title')
-                </p>
+    @include ('admin::layouts.tabs')
 
-                <div class="flex gap-x-[10px] items-center">
-                    <a href="{{ route('admin.email_templates.create') }}">
-                        <div class="px-[12px] py-[6px] bg-blue-600 border border-blue-700 rounded-[6px] text-gray-50 font-semibold cursor-pointer">
-                            @lang('admin::app.marketing.email-marketing.templates.create.title')
-                        </div>
-                    </a>
-                </div>
+    {{-- Title of the page --}}
+    <x-slot:title>
+        @lang('admin::app.marketing.email-marketing.templates.title')
+    </x-slot:title>
+
+    <div class="mt-5">
+        <div class="flex gap-[16px] justify-between max-sm:flex-wrap">
+            <p class="text-[20px] text-gray-800 font-bold">
+                @lang('admin::app.marketing.email-marketing.templates.title')
+            </p>
+
+            <div class="flex gap-x-[10px] items-center">
+                <a href="{{ route('admin.email_templates.create') }}">
+                    <div class="px-[12px] py-[6px] bg-blue-600 border border-blue-700 rounded-[6px] text-gray-50 font-semibold cursor-pointer">
+                        @lang('admin::app.marketing.email-marketing.templates.create.title')
+                    </div>
+                </a>
             </div>
-        </x-admin::tabs.item>
-
-        {{-- Events Tab --}}
-        <x-admin::tabs.item
-            class=""
-            :title="trans('admin::app.marketing.email-marketing.templates.events')"
-            :is-selected="false"
-        >
-            @include('admin::marketing.email-marketing.events.index')
-        </x-admin::tabs.item>
-
-        {{-- Campaigns Tab --}}
-        <x-admin::tabs.item
-            class=""
-            :title="trans('admin::app.marketing.email-marketing.campaigns.title')"
-            :is-selected="false"
-        >
-            @include('admin::marketing.email-marketing.campaigns.index')
-        </x-admin::tabs.item>
-
-        {{-- News Letter Subscription Tab --}}
-        <x-admin::tabs.item
-            class=""
-            :title="trans('admin::app.marketing.email-marketing.newsletters.title')"
-            :is-selected="false"
-        >
-            @include('admin::marketing.email-marketing.subscribers.index')
-        </x-admin::tabs.item>
-    </x-admin::tabs>
-
+        </div>
+    </div>
 </x-admin::layouts>
