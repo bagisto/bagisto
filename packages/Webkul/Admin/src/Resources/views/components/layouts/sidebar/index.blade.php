72460223d642389c8e40a90aa36102cc39b3d6fd
suraj kashyap <suraj.kashyap370@webkul.in>
2023-07-25 19:06:24 +0530

    Minor changes.

--- packages/Webkul/Admin/src/Resources/views/components/layouts/sidebar/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/layouts/sidebar/index.blade.php
@@ -1,5 +1,4 @@
 @php
-
     $tree = \Webkul\Core\Tree::create();
 
     foreach (config('core') as $item) {
@@ -10,8 +9,8 @@
 <div class="fixed top-[57px] h-full bg-white px-[16px] pt-[8px] max-w-[269px] shadow-[0px_8px_10px_0px_rgba(0,_0,_0,_0.2)] max-lg:hidden">
     <div class="h-[calc(100vh-100px)] overflow-auto journal-scroll">
         <nav class="grid w-[222px]">
-            {{-- Navigation Content --}}
-            <div class="">
+            <div>
+                {{-- Navigation Menu --}}
                 @foreach ($menu->items as $menuItem)
                     <a href="{{ $menuItem['url'] }}">
                         <div class="accordian-toggle flex gap-[10px] p-[6px] items-center cursor-pointer hover:bg-gray-100 hover:rounded-[8px]">
@@ -22,7 +21,6 @@
                         </div>
                     </a>
 
-                    {{-- Sub Navigation Content --}}
                     @if ($menuItem['key'] != 'configuration')
                         <div class="grid pb-[7px]">
                             @foreach ($menuItem['children'] as $subMenuItem)
@@ -34,7 +32,6 @@
                             @endforeach
                         </div>
                     @else 
-                    {{-- Navigation for configurations --}}
                         <div class="grid pb-[7px]">
                             @foreach (core()->sortItems($tree->items) as $key => $item)
                                 <a href="{{ route('admin.configuration.index', $item['key']) }}">
@@ -51,6 +48,7 @@
         </nav>
     </div>
 
+    {{-- Collapse menu --}}
     <div class="bg-white mt-[60px] fixed w-full max-w-[221px] bottom-0">
         <div class="flex gap-[10px] p-[6px] items-center">
             <span class="icon-arrow-right text-[24px]"></span>
