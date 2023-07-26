bfd73aa240e247f22f9f3119e013a4bbbb35a04c
Shivendra <shivendra.gupta423@webkul.in>
2023-07-26 15:03:40 +0530

    create customer notes functionality

---
+++ packages/Webkul/Customer/src/Models/CustomerNote.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Webkul\Customer\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Webkul\Customer\Contracts\CustomerNote as CustomerNoteContract;
+
+class CustomerNote extends Model implements CustomerNoteContract
+{
+    protected $fillable = [
+        'note',
+        'customer_id',
+    ];
+
+    /**
+     * Get the order record associated with the order comment.
+     */
+    public function customer()
+    {
+        return $this->belongsTo(CustomerProxy::modelClass());
+    }
+}
