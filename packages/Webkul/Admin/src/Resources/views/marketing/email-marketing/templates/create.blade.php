c18cb670daa7e243249afb8a358c273d4515c6b7
Shivendra <shivendra.gupta423@webkul.in>
2023-08-10 14:51:37 +0530

    updated email template create page

--- packages/Webkul/Admin/src/Resources/views/marketing/email-marketing/templates/create.blade.php
+++ packages/Webkul/Admin/src/Resources/views/marketing/email-marketing/templates/create.blade.php
@@ -28,73 +28,19 @@
             </div>
         </div>
 
-        {{-- Informations --}}
-        <div class="flex gap-[10px] mt-[28px] mb-2">
-            <div class="flex flex-col gap-[8px] flex-1">
-                {{-- General Section --}}
+        {{-- body content --}}
+        <div class="flex gap-[10px] mt-[14px] max-xl:flex-wrap">
+            {{-- Left sub-component --}}
+            <div class=" flex flex-col gap-[8px] flex-1 max-xl:flex-auto">
+                {{--Content --}}
                 <div class="p-[16px] bg-white rounded-[4px] box-shadow">
-                    <p class="mb-[16px] text-[16px] text-gray-800 font-semibold">
-                        @lang('admin::app.marketing.email-marketing.templates.create.general')
+                    <p class="required text-[16px] text-gray-800 font-semibold mb-[16px]">
+                        @lang('admin::app.marketing.email-marketing.templates.create.content')
                     </p>
 
                     <div class="mb-[10px]">
-                        {{-- Template Name --}}
-                        <x-admin::form.control-group class="w-full mb-[10px]">
-                            <x-admin::form.control-group.label class="required !mt-0">
-                                @lang('admin::app.marketing.email-marketing.templates.create.name')
-                            </x-admin::form.control-group.label>
-
-                            <x-admin::form.control-group.control
-                                type="text"
-                                name="name"
-                                :value="old('name')"
-                                rules="required"
-                                label="{{ trans('admin::app.marketing.email-marketing.templates.create.name') }}"
-                                placeholder="{{ trans('admin::app.marketing.email-marketing.templates.create.name') }}"
-                            >
-                            </x-admin::form.control-group.control>
-
-                            <x-admin::form.control-group.error
-                                control-name="name"
-                            >
-                            </x-admin::form.control-group.error>
-                        </x-admin::form.control-group>
-
-                        {{-- Template Status --}}
-                        <x-admin::form.control-group class="w-full mb-[10px]">
-                            <x-admin::form.control-group.label class="required">
-                                @lang('admin::app.marketing.email-marketing.templates.create.status')
-                            </x-admin::form.control-group.label>
-
-                            <x-admin::form.control-group.control
-                                type="select"
-                                name="status"
-                                class="cursor-pointer mb-1"
-                                rules="required"
-                                label="{{ trans('admin::app.marketing.email-marketing.templates.create.status') }}"
-                            >
-                                @foreach (['active', 'inactive', 'draft'] as $state)
-                                    <option
-                                        value="{{ $state }}" 
-                                        {{ old('status') == $state ? 'selected' : '' }}
-                                    >
-                                        @lang('admin::app.marketing.email-marketing.templates.create.' . $state)
-                                    </option>
-                                @endforeach
-                            </x-admin::form.control-group.control>
-
-                            <x-admin::form.control-group.error
-                                control-name="status"
-                            >
-                            </x-admin::form.control-group.error>
-                        </x-admin::form.control-group>
-
                         {{-- Template Textarea --}}
                         <x-admin::form.control-group class="mb-[10px]">
-                            <x-admin::form.control-group.label class="required">
-                                @lang('admin::app.marketing.email-marketing.templates.create.content')
-                            </x-admin::form.control-group.label>
-
                             <x-admin::form.control-group.control
                                 type="tinymce"
                                 name="content"
@@ -112,6 +58,76 @@
                             </x-admin::form.control-group.error>
                         </x-admin::form.control-group>
                     </div>
+                </div>
+            </div>
+
+            {{-- Right sub-component --}}
+            <div class="flex flex-col gap-[8px] w-[360px] max-w-full max-sm:w-full">
+                {{-- General --}}
+                <div class="bg-white rounded-[4px] box-shadow">
+                    <x-admin::accordion>
+                        <x-slot:header>
+                            <div class="flex items-center justify-between">
+                                <p class="p-[10px] text-gray-600 text-[16px] font-semibold">
+                                    @lang('admin::app.users.roles.create.general')
+                                </p>
+                            </div>
+                        </x-slot:header>
+                
+                        <x-slot:content>
+                            <div class="mb-[10px]">
+                                {{-- Template Name --}}
+                                <x-admin::form.control-group class="w-full mb-[10px]">
+                                    <x-admin::form.control-group.label class="required">
+                                        @lang('admin::app.marketing.email-marketing.templates.create.name')
+                                    </x-admin::form.control-group.label>
+
+                                    <x-admin::form.control-group.control
+                                        type="text"
+                                        name="name"
+                                        :value="old('name')"
+                                        rules="required"
+                                        :label="trans('admin::app.marketing.email-marketing.templates.create.name')"
+                                        :placeholder="trans('admin::app.marketing.email-marketing.templates.create.name')"
+                                    >
+                                    </x-admin::form.control-group.control>
+
+                                    <x-admin::form.control-group.error
+                                        control-name="name"
+                                    >
+                                    </x-admin::form.control-group.error>
+                                </x-admin::form.control-group>
+
+                                {{-- Template Status --}}
+                                <x-admin::form.control-group class="w-full mb-[10px]">
+                                    <x-admin::form.control-group.label class="required">
+                                        @lang('admin::app.marketing.email-marketing.templates.create.status')
+                                    </x-admin::form.control-group.label>
+
+                                    <x-admin::form.control-group.control
+                                        type="select"
+                                        name="status"
+                                        rules="required"
+                                        label="{{ trans('admin::app.marketing.email-marketing.templates.create.status') }}"
+                                    >
+                                        @foreach (['active', 'inactive', 'draft'] as $state)
+                                            <option
+                                                value="{{ $state }}" 
+                                                {{ old('status') == $state ? 'selected' : '' }}
+                                            >
+                                                @lang('admin::app.marketing.email-marketing.templates.create.' . $state)
+                                            </option>
+                                        @endforeach
+                                    </x-admin::form.control-group.control>
+
+                                    <x-admin::form.control-group.error
+                                        control-name="status"
+                                    >
+                                    </x-admin::form.control-group.error>
+                                </x-admin::form.control-group>
+                            </div>
+                        </x-slot:content>
+                    </x-admin::accordion>
                 </div>
             </div>
         </div>
