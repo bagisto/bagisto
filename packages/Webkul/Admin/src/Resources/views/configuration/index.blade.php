534a12a6188da06e85c04cf048e892918dde64c3
Shivendra <shivendra.gupta423@webkul.in>
2023-08-29 20:05:37 +0530

    fixed config lang

--- packages/Webkul/Admin/src/Resources/views/configuration/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/configuration/index.blade.php
@@ -1,13 +1,13 @@
 <x-admin::layouts>
     {{-- Title of the page. --}}
     <x-slot:title>
-        @lang('admin::app.configuration.title')
+        @lang('admin::app.configuration.index.title')
     </x-slot:title>
 
     {{-- Heading of the page --}}
     <div class="flex justify-between items-center mb-[26px]">
         <p class="text-[20px] text-gray-800 font-bold">
-            @lang('admin::app.configuration.title')
+            @lang('admin::app.configuration.index.title')
         </p>
 
         <div class="flex gap-x-[10px] items-center">
