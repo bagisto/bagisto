0122eef9ab0ba2d2de8a4923d7f605e02a144ea0
Jitendra <jitendra@webkul.in>
2023-08-29 13:27:04 +0530

    Fixed datagrid action icon hover ui issue

--- packages/Webkul/Admin/src/Resources/views/settings/locales/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/settings/locales/index.blade.php
@@ -105,7 +105,7 @@
                             <a @click="id=1; editModal(record.id)">
                                 <span
                                     :class="record.actions['0'].icon"
-                                    class="cursor-pointer rounded-[6px] p-[6px] text-[24px] transition-all hover:bg-gray-100 max-sm:place-self-center"
+                                    class="cursor-pointer rounded-[6px] p-[6px] text-[24px] transition-all hover:bg-gray-200 max-sm:place-self-center"
                                     :title="record.actions['0'].title"
                                 >
                                 </span>
@@ -114,7 +114,7 @@
                             <a @click="deleteModal(record.actions['1']?.url)">
                                 <span
                                     :class="record.actions['1'].icon"
-                                    class="cursor-pointer rounded-[6px] p-[6px] text-[24px] transition-all hover:bg-gray-100 max-sm:place-self-center"
+                                    class="cursor-pointer rounded-[6px] p-[6px] text-[24px] transition-all hover:bg-gray-200 max-sm:place-self-center"
                                     :title="record.actions['1'].title"
                                 >
                                 </span>
