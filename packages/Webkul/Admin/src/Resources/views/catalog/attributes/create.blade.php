4514957d1e6d4236ff6921a596ad1c79024413f6
suraj kashyap <suraj.kashyap370@webkul.in>
2023-09-14 12:12:12 +0530

    Minor changes.

--- packages/Webkul/Admin/src/Resources/views/catalog/attributes/create.blade.php
+++ packages/Webkul/Admin/src/Resources/views/catalog/attributes/create.blade.php
@@ -401,7 +401,7 @@
                                             :class="[errors['{{ 'code' }}'] ? 'border border-red-600 hover:border-red-600' : '']"
                                             class="flex w-full min-h-[39px] py-2 px-3 border rounded-[6px] text-[14px] text-gray-600 transition-all hover:border-gray-400 focus:border-gray-400"
                                             placeholder="{{ trans('admin::app.catalog.attributes.create.code') }}"
-                                            v-code:slug="setValues"
+                                            v-code
                                         >
                                     </v-field>
 
