1cb14b67f01494d03413726003971fb690776e4c
Shivendra <shivendra.gupta423@webkul.in>
2023-10-11 11:03:52 +0530

    fixed root category delete message issue

--- packages/Webkul/Admin/src/Http/Controllers/Catalog/CategoryController.php
+++ packages/Webkul/Admin/src/Http/Controllers/Catalog/CategoryController.php
@@ -193,7 +193,7 @@ class CategoryController extends Controller
                 if (! $this->isCategoryDeletable($category)) {
                     $suppressFlash = false;
 
-                    session()->flash('warning', trans('admin::app.response.delete-category-root', ['name' => 'admin::app.catalog.categories.category']));
+                    return new JsonResponse(['message' => trans('admin::app.catalog.categories.delete-category-root')], 400);
                 } else {
                     try {
                         $suppressFlash = true;
