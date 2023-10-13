c396989341473447dc3afb811c131aa6e6f9a115
Devansh Bawari <devansh.bawari419@webkul.in>
2023-10-13 15:57:30 +0530

    fix: fixed smart button issues and transactions

--- packages/Webkul/Sales/src/Repositories/OrderTransactionRepository.php
+++ packages/Webkul/Sales/src/Repositories/OrderTransactionRepository.php
@@ -3,22 +3,15 @@
 namespace Webkul\Sales\Repositories;
 
 use Webkul\Core\Eloquent\Repository;
+use Webkul\Sales\Contracts\OrderTransaction;
 
-/**
- * Order Transaction Repository
- *
- * @author    Jitendra Singh <jitendra@webkul.com>
- * @copyright 2018 Webkul Software Pvt Ltd (http://www.webkul.com)
- */
 class OrderTransactionRepository extends Repository
 {
     /**
-     * Specify Model class name
-     *
-     * @return string
+     * Specify model class name.
      */
-    function model(): string
+    public function model(): string
     {
-        return 'Webkul\Sales\Contracts\OrderTransaction';
+        return OrderTransaction::class;
     }
 }
