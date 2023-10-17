cfff61e6089c8d64c899ca766b6da23d91013f62
Shivendra <shivendra.gupta423@webkul.in>
2023-10-17 19:32:53 +0530

    removed unnecessary classes

--- packages/Webkul/Shop/src/Resources/views/components/shimmer/customers/account/wishlist/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/shimmer/customers/account/wishlist/index.blade.php
@@ -13,38 +13,38 @@
         <div class="flex flex-wrap gap-[75px] mt-[30px] max-1060:flex-col">
             <div class="grid gap-y-[25px] flex-1">
                 <!-- Single card -->
-                <div class="flex justify-between gap-x-[10px] pb-[18px] border-b-[1px] border-[#E9E9E9]">
+                <div class="flex justify-between gap-x-[10px] pb-[18px] border-b-[1px]">
                     <div class="flex gap-x-[20px]">
                         <div class="">
-                            <div class="shimmer w-[110px] h-[110px] bg-[#E9E9E9] rounded-[12px]"></div>
+                            <div class="shimmer w-[110px] h-[110px] rounded-[12px]"></div>
                         </div>
 
                         <div class="grid gap-y-[10px]">
-                            <div class="shimmer w-[200px] h-[21px] bg-[#E9E9E9]"></div>
+                            <div class="shimmer w-[200px] h-[21px]"></div>
                             <div class="flex gap-x-[10px] gap-y-[6px] flex-wrap">
                                 <div class="grid gap-[8px]">
                                 </div>
                             </div>
 
-                            <div class="shimmer w-[100px] h-[21px] bg-[#E9E9E9]"></div>
+                            <div class="shimmer w-[100px] h-[21px]"></div>
                             
                             <div class="hidden gap-[10px] place-content-start max-sm:grid">
-                                <div class="shimmer w-[100px] h-[27px] bg-[#E9E9E9]"></div>
+                                <div class="shimmer w-[100px] h-[27px]"></div>
 
-                                <div class="shimmer w-[100px] h-[24px] bg-[#E9E9E9]"></div>
+                                <div class="shimmer w-[100px] h-[24px]"></div>
                             </div>
 
                             <div class="flex gap-[20px] flex-wrap">
-                                <div class="shimmer w-[110px] h-[40px] rounded-[54px] bg-[#E9E9E9]"></div>
-                                <div class="shimmer w-[158px] h-[40px] rounded-[18px] bg-[#E9E9E9]"></div>
+                                <div class="shimmer w-[110px] h-[40px] rounded-[54px]"></div>
+                                <div class="shimmer w-[158px] h-[40px] rounded-[18px]"></div>
                             </div>
                         </div>
                     </div>
 
                     <div class="grid gap-[10px] place-content-start max-sm:hidden">
-                        <div class="shimmer w-[100px] h-[27px] bg-[#E9E9E9]"></div>
+                        <div class="shimmer w-[100px] h-[27px]"></div>
                         
-                        <div class="shimmer w-[100px] h-[24px] bg-[#E9E9E9]"></div>
+                        <div class="shimmer w-[100px] h-[24px]"></div>
                     </div>
                 </div>
             </div>
