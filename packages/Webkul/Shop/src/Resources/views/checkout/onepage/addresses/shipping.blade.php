750dbd48d6b947773483fada039f3f082cbe0330
junaidjaved248 <junaidjaved8461@gmail.com>
2023-10-18 14:30:01 +0500

    Removed Validation Must be Integer (From Shipping Blade File)

--- packages/Webkul/Shop/src/Resources/views/checkout/onepage/addresses/shipping.blade.php
+++ packages/Webkul/Shop/src/Resources/views/checkout/onepage/addresses/shipping.blade.php
@@ -383,7 +383,7 @@
                                 <x-shop::form.control-group.control
                                     type="text"
                                     name="shipping[postcode]"
-                                    rules="required|integer"
+                                    rules="required"
                                     :label="trans('shop::app.checkout.onepage.addresses.shipping.postcode')"
                                     :placeholder="trans('shop::app.checkout.onepage.addresses.shipping.postcode')"
                                     v-model="forms.shipping.address.postcode"
