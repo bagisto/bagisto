098a4f09475cea2313292223b0fa6a53279b5a37
junaidjaved248 <junaidjaved8461@gmail.com>
2023-10-18 15:04:31 +0500

    Removed Must be Number (numeric) Validation Rule From Customer/AddressRequest Validation

--- packages/Webkul/Shop/src/Http/Requests/Customer/AddressRequest.php
+++ packages/Webkul/Shop/src/Http/Requests/Customer/AddressRequest.php
@@ -36,7 +36,7 @@ class AddressRequest extends FormRequest
             'country'      => [new AlphaNumericSpace],
             'state'        => [new AlphaNumericSpace],
             'city'         => ['required', 'string'],
-            'postcode'     => ['required', 'numeric'],
+            'postcode'     => ['required'],
             'phone'        => ['required', new PhoneNumber],
             'vat_id'       => [new VatIdRule()],
         ];
