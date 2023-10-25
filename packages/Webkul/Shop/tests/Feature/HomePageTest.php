38e9e0a889b7471a12815e66c56b7877c3f557be
Devansh Bawari <devansh.bawari419@webkul.in>
2023-10-25 19:57:27 +0530

    seperate api test

--- packages/Webkul/Shop/tests/Feature/HomePageTest.php
+++ packages/Webkul/Shop/tests/Feature/HomePageTest.php
@@ -1,83 +1,9 @@
 <?php
 
-use Pest\Expectation;
-use Webkul\Faker\Helpers\Product;
-use Webkul\Product\Models\Product as ProductModel;
-
 use function Pest\Laravel\get;
-use function Pest\Laravel\getJson;
-
-afterEach(function () {
-    /**
-     * Cleaning up rows which are created.
-     */
-    ProductModel::query()->delete();
-});
 
 it('returns a successful response', function () {
     // Act & Assert
     get(route('shop.home.index'))
         ->assertOk();
-});
-
-it('returns a new products listing', function () {
-    // Prepare
-    $newProductOptions = [
-        'attributes' => [
-            5 => 'new',
-        ],
-
-        'attribute_value' => [
-            'new' => [
-                'boolean_value' => true,
-            ],
-        ],
-    ];
-
-    (new Product($newProductOptions))->create(1, 'simple');
-
-    // Act
-    $response = getJson(route('shop.api.products.index', ['new' => 1]))->collect();
-
-    // Assert
-    expect($response['data'])->each(function (Expectation $product) {
-        return $product->is_new->toBeTrue();
-    });
-});
-
-it('returns a featured products listing', function () {
-    // Prepare
-    $featuredProductOptions = [
-        'attributes' => [
-            6 => 'featured',
-        ],
-
-        'attribute_value' => [
-            'featured' => [
-                'boolean_value' => true,
-            ],
-        ],
-    ];
-
-    (new Product($featuredProductOptions))->create(1, 'simple');
-
-    // Act
-    $response = getJson(route('shop.api.products.index', ['featured' => 1]))->collect();
-
-    // Assert
-    expect($response['data'])->each(function (Expectation $product) {
-        return $product->is_featured->toBeTrue();
-    });
-});
-
-it('returns all products listing', function () {
-    // Prepare
-    $product = (new Product())->create(1, 'simple')->first();
-
-    // Act & Assert
-    getJson(route('shop.api.products.index'))
-        ->assertJsonIsArray('data')
-        ->assertJsonFragment([
-            'id' => $product->id,
-        ]);
 });
