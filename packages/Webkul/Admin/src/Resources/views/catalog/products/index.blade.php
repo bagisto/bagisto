d63ec8bd253e1b7e64446992ff348aa0d599c4bc
amit kumar laravel <amitkumar.laravel358@webkul.in>
2023-10-25 18:17:44 +0530

    Remove Unnecessary Added export setting icon

--- packages/Webkul/Admin/src/Resources/views/catalog/products/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/catalog/products/index.blade.php
@@ -9,31 +9,6 @@
         </p>
 
         <div class="flex gap-x-[10px] items-center">
-            <!-- Dropdown -->
-            <x-admin::dropdown position="bottom-right">
-                <x-slot:toggle>
-                    <span class="flex icon-setting p-[6px] rounded-[6px] text-[24px] cursor-pointer transition-all hover:bg-gray-200 dark:hover:bg-gray-800 "></span>
-                </x-slot:toggle>
-
-                <x-slot:content class="w-[174px] max-w-full !p-[8PX] border dark:border-gray-800 rounded-[4px] z-10 bg-white dark:bg-gray-900  shadow-[0px_8px_10px_0px_rgba(0,_0,_0,_0.2)]">
-                    <div class="grid gap-[2px]">
-                        <!-- Current Channel -->
-                        <div class="p-[6px] items-center cursor-pointer transition-all hover:bg-gray-100 dark:hover:bg-gray-950  hover:rounded-[6px]">
-                            <p class="text-gray-600 dark:text-gray-300  font-semibold leading-[24px]">
-                                Channel - {{ core()->getCurrentChannel()->name }}
-                            </p>
-                        </div>
-
-                        <!-- Current Locale -->
-                        <div class="p-[6px] items-center cursor-pointer transition-all hover:bg-gray-100 dark:hover:bg-gray-950  hover:rounded-[6px]">
-                            <p class="text-gray-600 dark:text-gray-300  font-semibold leading-[24px]">
-                                Language - {{ core()->getCurrentLocale()->name }}
-                            </p>
-                        </div>
-                    </div>
-                </x-slot:content>
-            </x-admin::dropdown>
-
             <!-- Export Modal -->
             <x-admin::datagrid.export src="{{ route('admin.catalog.products.index') }}"></x-admin::datagrid.export>
 
