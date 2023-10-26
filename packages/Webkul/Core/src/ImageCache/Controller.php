a6be69de531756ed1b8e61ab5fdd71b567a4e6bb
Jitendra <jitendra@webkul.in>
2023-10-26 11:16:37 +0530

    Fixed issue

--- packages/Webkul/Core/src/ImageCache/Controller.php
+++ packages/Webkul/Core/src/ImageCache/Controller.php
@@ -4,9 +4,24 @@ namespace Webkul\Core\ImageCache;
 
 use Config;
 use Intervention\Image\ImageCacheController;
+use Illuminate\Http\Response as IlluminateResponse;
 
 class Controller extends ImageCacheController
 {
+    /**
+     * Cache template
+     *
+     * @var string
+     */
+    protected $template;
+
+    /**
+     * Logo
+     *
+     * @var string
+     */
+    const BAGISTO_LOGO = 'https://updates.bagisto.com/bagisto.png';
+
     /**
      * Get HTTP response of either original image file or
      * template applied file.
@@ -38,41 +53,84 @@ class Controller extends ImageCacheController
      */
     public function getImage($template, $filename)
     {
-        $cacheTime = config('imagecache.lifetime');
+        $this->template = $template;
+
+        $cacheTime = $template == 'logo' ? 10080 : config('imagecache.lifetime');
 
         if ($template == 'logo') {
-            $cacheTime = 10080;
-
-            $path = 'https://updates.bagisto.com/bagisto.png';
+            $path = self::BAGISTO_LOGO;
         } else {
             $template = $this->getTemplate($template);
+
             $path = $this->getImagePath($filename);
         }
 
-        // image manipulation based on callback
+        /**
+         * Image manipulation based on callback
+         */
         $manager = new ImageManager(Config::get('image'));
 
         try {
             $content = $manager->cache(function ($image) use ($template, $path) {
-
                 if ($template instanceof Closure) {
-                    // build from closure callback template
+                    /**
+                     * Build from closure callback template
+                     */
                     $template($image->make($path));
                 } elseif (is_object($template)) {
-                    // build from filter template
+                    /**
+                     * Build from filter template
+                     */
                     $image->make($path)->filter($template);
                 } else {
                     $image->make($path);
                 }
             }, $cacheTime);
         } catch (\Exception $e) {
-            if ($template == 'logo') {
-                $content = '';
-            } else {
+            if ($template != 'logo') {
                 abort(404);
             }
+            
+            $content = '';
         }
 
         return $this->buildResponse($content);
+    }
+
+    /**
+     * Builds HTTP response from given image data
+     *
+     * @param  string $content
+     * @return Illuminate\Http\Response
+     */
+    protected function buildResponse($content)
+    {
+        /**
+         * Define mime type
+         */
+        $mime = finfo_buffer(finfo_open(FILEINFO_MIME_TYPE), $content);
+
+        /**
+         * Respond with 304 not modified if browser has the image cached
+         */
+        $eTag = md5($content);
+
+        $notModified = isset($_SERVER['HTTP_IF_NONE_MATCH']) && $_SERVER['HTTP_IF_NONE_MATCH'] == $eTag;
+
+        $content = $notModified ? null : $content;
+
+        $statusCode = $notModified ? 304 : 200;
+
+        $maxAge = ($this->template == 'logo' ? 10080 : config('imagecache.lifetime')) * 60;
+
+        /**
+         * Return http response
+         */
+        return new IlluminateResponse($content, $statusCode, [
+            'Content-Type'   => $mime,
+            'Cache-Control'  => 'max-age=' . $maxAge . ', public',
+            'Content-Length' => strlen($content),
+            'Etag'           => $eTag
+        ]);
     }
 }
