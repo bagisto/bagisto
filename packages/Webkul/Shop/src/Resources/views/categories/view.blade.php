eed54825caa47ee127fdd12a38d791d0a9825c76
amit kumar laravel <amitkumar.laravel358@webkul.in>
2023-10-27 18:47:59 +0530

    Move spinner from blade to image

--- packages/Webkul/Shop/src/Resources/views/categories/view.blade.php
+++ packages/Webkul/Shop/src/Resources/views/categories/view.blade.php
@@ -148,34 +148,21 @@
                         <button
                             class="secondary-button block mx-auto w-max py-[11px] mt-[60px] px-[43px] rounded-[18px] text-base text-center"
                             @click="loadMoreProducts"
-                            v-if="links.next && ! loader"
+                            v-if="links.next &&  loader"
                         >
                             @lang('shop::app.categories.view.load-more')
                         </button>
 
                         <button
                             v-else-if="links.next"
-                            class="secondary-button block mx-auto w-max py-[13px] mt-[60px] px-[74.5px] rounded-[18px] text-base text-center"
+                            class="secondary-button block w-max mx-auto py-[13px] mt-[60px] px-[74.5px] rounded-[18px] text-base text-center"
                         >
                             <!-- Spinner -->
-                            <svg class="animate-spin h-5 w-5 text-navyBlue" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                <circle
-                                    class="opacity-25"
-                                    cx="12"
-                                    cy="12"
-                                    r="10"
-                                    stroke="currentColor"
-                                    stroke-width="4"
-                                >
-                                </circle>
-
-                                <path
-                                    class="opacity-75"
-                                    fill="currentColor"
-                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                >
-                                </path>
-                            </svg>
+                            <img
+                                class="animate-spin h-5 w-5 text-navyBlue"
+                                src="{{ bagisto_asset('images/spinner.svg') }}"
+                                alt="Loading"
+                            />
                         </button>
                     </div>
                 </div>
