07b6535b67852d5b8cdcbbfd99e5e9e6228b69c0
amit kumar laravel <amitkumar.laravel358@webkul.in>
2023-10-27 16:58:54 +0530

    FIxed Issue #8826

--- packages/Webkul/Shop/src/Resources/views/categories/view.blade.php
+++ packages/Webkul/Shop/src/Resources/views/categories/view.blade.php
@@ -142,9 +142,34 @@
                         <button
                             class="secondary-button block mx-auto w-max py-[11px] mt-[60px] px-[43px] rounded-[18px] text-base text-center"
                             @click="loadMoreProducts"
-                            v-if="links.next"
+                            v-if="links.next && ! loader"
                         >
                             @lang('shop::app.categories.view.load-more')
+                        </button>
+
+                        <button
+                            v-else-if="links.next"
+                            class="secondary-button block mx-auto w-max py-[13px] mt-[60px] px-[74.5px] rounded-[18px] text-base text-center"
+                        >
+                            <!-- Spinner -->
+                            <svg class="animate-spin h-5 w-5 text-navyBlue" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                <circle
+                                    class="opacity-25"
+                                    cx="12"
+                                    cy="12"
+                                    r="10"
+                                    stroke="currentColor"
+                                    stroke-width="4"
+                                >
+                                </circle>
+
+                                <path
+                                    class="opacity-75"
+                                    fill="currentColor"
+                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                >
+                                </path>
+                            </svg>
                         </button>
                     </div>
                 </div>
@@ -176,6 +201,8 @@
                         products: [],
 
                         links: {},
+
+                        loader: false,
                     }
                 },
 
@@ -233,13 +260,18 @@
 
                     loadMoreProducts() {
                         if (this.links.next) {
-                            this.$axios.get(this.links.next).then(response => {
-                                this.products = [...this.products, ...response.data.data];
+                            this.loader = true;
 
-                                this.links = response.data.links;
-                            }).catch(error => {
-                                console.log(error);
-                            });
+                            this.$axios.get(this.links.next)
+                                .then(response => {
+                                    this.loader = false;
+
+                                    this.products = [...this.products, ...response.data.data];
+
+                                    this.links = response.data.links;
+                                }).catch(error => {
+                                    console.log(error);
+                                });
                         }
                     },
 
