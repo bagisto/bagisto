3ac48a8652f2915dc7721ad442942ffb933886dd
Jitendra <jitendra@webkul.in>
2023-10-27 14:06:00 +0530

    Home page image alt attribute added

--- packages/Webkul/Shop/src/Resources/views/components/carousel/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/carousel/index.blade.php
@@ -21,7 +21,7 @@
                     ::lazy="false"
                     ::src="image.image"
                     ::srcset="image.image + ' 1920w, ' + image.image.replace('storage', 'cache/large') + ' 1280w,' + image.image.replace('storage', 'cache/medium') + ' 1024w, ' + image.image.replace('storage', 'cache/small') + ' 525w'"
-                    alt="Slide Image"
+                    ::alt="image?.title"
                 ></x-shop::media.images.lazy>
             </a>
 
