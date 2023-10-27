f62b3250a407e744bcdc246be12c7396a819f4a1
Jitendra <jitendra@webkul.in>
2023-10-27 14:20:09 +0530

    Remove https force

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
-        \URL::forceScheme('https');
     }
 
     /**
