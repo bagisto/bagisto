038b926debdce4933093cb83539680a51e744179
Khushi-sunarc <khushi.soni@sunarctechnologies.com>
2023-11-06 13:52:18 +0530

    Fixed #8893 - Develop a mailable for sending customer registration emails to admin and create a Blade file for the email template.

---
+++ packages/Webkul/Admin/src/Mail/Customer/RegistrationNotification.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Webkul\Admin\Mail\Customer;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class RegistrationNotification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new mailable instance.
+     *
+     * @param  \Webkul\Customer\Contracts\Customer  $customer
+     * @return void
+     */
+    public function __construct(public $customer)
+    {
+    }
+
+    public function build()
+    {
+        return $this->from(core()->getSenderEmailDetails()['email'], core()->getSenderEmailDetails()['name'])
+            ->to(core()->getAdminEmailDetails()['email'], core()->getAdminEmailDetails()['name'])
+            ->subject(trans('admin::app.emails.customers.registration.subject'))
+            ->view('admin::emails.customers.registration');
+    }
+}
