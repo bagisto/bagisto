0839d2def0f19786c1b440826e49b781ae148d16
Shivendra <shivendra.gupta423@webkul.in>
2023-11-07 13:05:07 +0530

    fixed check fail

--- packages/Webkul/Core/src/Rules/Address.php
+++ packages/Webkul/Core/src/Rules/Address.php
@@ -12,7 +12,7 @@ class Address implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        if (! preg_match("/^[a-zA-Z0-9 ,()-]+$/", $value)) {
+        if (! preg_match('/^[a-zA-Z0-9 ,()-]+$/', $value)) {
             $fail('core::validation.address')->translate();
         }
     }
