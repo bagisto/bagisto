067fa838145b3063a42f585209ed923a169f0f7d
Shivendra <shivendra.gupta423@webkul.in>
2023-11-07 15:53:48 +0530

    fixed status issue

--- packages/Webkul/Shop/src/Resources/views/components/layouts/services.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/layouts/services.blade.php
@@ -1,5 +1,9 @@
 {!! view_render_event('bagisto.shop.layout.features.before') !!}
 
+<!--
+    The ThemeCustomizationRepository repository is injected directly here because there is no way
+    to retrieve it from the view composer, as this is an anonymous component.
+-->
 @inject('themeCustomizationRepository', 'Webkul\Shop\Repositories\ThemeCustomizationRepository')
 
 @php
@@ -11,7 +15,7 @@
 @endphp
 
 <!-- Features -->
-@if ($customization->options)
+@if ($customization)
     <div class="container mt-20 max-lg:px-[30px] max-sm:mt-[30px]">
         <div class="flex gap-[25px] justify-center max-lg:flex-wrap">
             @foreach ($customization->options['services'] as $service)
@@ -19,6 +23,7 @@
                     <span
                         class="flex items-center justify-center w-[60px] h-[60px] bg-white border border-black rounded-full text-navyBlue p-[10px]"
                     >
+                        <!-- Service Image -->
                         <img 
                             class="min-w-[42px] min-h-[42px]" 
                             src="{{ $service['image'] }}" 
@@ -29,8 +34,10 @@
                     </span>
         
                     <div class="">
+                        <!-- Service Title -->
                         <p class="text-[16px] font-medium font-dmserif">{{$service['title']}}</p>
         
+                        <!-- Service Description -->
                         <p class="text-[14px] font-medium mt-[10px] text-[#6E6E6E] max-w-[217px]">
                             {{$service['description']}}
                         </p>
