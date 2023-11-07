cb6d21bfaa126b7300c79e9347c4a1116485e4e1
Jitendra <jitendra@webkul.in>
2023-11-07 18:44:41 +0530

    Removed test code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(BagistoDatabaseSeeder::class, false);
+        $this->call(BagistoDatabaseSeeder::class);
     }
 }
