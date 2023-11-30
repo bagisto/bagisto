93dcc599992cdee151288218387fb0569b5a286a
Shivendra <shivendra.gupta423@webkul.in>
2023-11-30 18:27:07 +0530

    made configuration validation dyanamic

--- packages/Webkul/Admin/src/Http/Requests/ConfigurationForm.php
+++ packages/Webkul/Admin/src/Http/Requests/ConfigurationForm.php
@@ -3,7 +3,6 @@
 namespace Webkul\Admin\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Webkul\Core\Rules\CommaSeparatedInteger;
 
 class ConfigurationForm extends FormRequest
 {
@@ -24,62 +23,32 @@ class ConfigurationForm extends FormRequest
      */
     public function rules()
     {
-        $this->rules = [];
+        $keys = request()->input('keys');
 
-        $request = request();
+        $this->rules = collect($keys)->mapWithKeys(function ($item) {
+            $data = json_decode($item);
 
-        if (
-            request()->has('catalog.products.storefront.products_per_page')
-            && ! empty(request()->input('catalog.products.storefront.products_per_page'))
-        ) {
-            $this->rules = [
-                'catalog.products.storefront.products_per_page' => [new CommaSeparatedInteger],
-            ];
-        }
+            $validationRules = collect($data->fields)->mapWithKeys(function ($field) use ($data) {
+                $key = $data->key . '.' . $field->name;
 
-        $imageFields = [
-            'general.design.admin_logo.logo_image',
-            'general.design.admin_logo.favicon',
-            'sales.invoice_settings.invoice_slip_design.logo',
-            'sales.payment_methods.cashondelivery.image',
-            'sales.payment_methods.moneytransfer.image',
-            'sales.payment_methods.paypal_standard.image',
-            'sales.payment_methods.paypal_smart_button.image',
-        ];
+                // Check delete key exist in the request
+                if (! $this->has($key . '.delete')) {
+                    $validation = isset($field->validation) ? $field->validation : '';
 
-        foreach ($imageFields as $field) {
-            if ($request->has($field) && ! $request->input("$field.delete")) {
-                $this->rules = array_merge($this->rules, [
-                    $field => 'required|mimes:bmp,jpeg,jpg,png,webp|max:5000',
-                ]);
-            }
-        }
+                    return [$key =>$validation];
+                }
+
+                return [];
+
+                // Check if validation information is available in the field
+                $validation = isset($field->validation) ? $field->validation : '';
+
+                return [$key => $validation];
+            })->toArray();
+
+            return $validationRules;
+        })->toArray();
 
         return $this->rules;
-    }
-
-    /**
-     * Get the error messages for the defined validation rules.
-     *
-     * @return array
-     */
-    public function messages()
-    {
-        return [
-            'general.design.admin_logo.logo_image.mimes' => 'Invalid file format. Use only bmp, jpeg, jpg, png and webp.',
-        ];
-    }
-
-    /**
-     * Set the attribute name.
-     */
-    public function attributes()
-    {
-        return [
-            'general.design.admin_logo.logo_image'             => 'Logo Image',
-            'general.design.admin_logo.favicon'                => 'Favicon Image',
-            'sales.invoice_settings.invoice_slip_design.logo'  => 'Invoice Logo',
-            'catalog.products.storefront.products_per_page'    => 'Product Per Page',
-        ];
     }
 }
