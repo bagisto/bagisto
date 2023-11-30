0da5a88fca59d787a40f04484e395ba2bd561e0a
Shivendra <shivendra.gupta423@webkul.in>
2023-11-30 20:21:33 +0530

    convert key as a array

--- packages/Webkul/Admin/src/Http/Requests/ConfigurationForm.php
+++ packages/Webkul/Admin/src/Http/Requests/ConfigurationForm.php
@@ -7,6 +7,13 @@ use Illuminate\Foundation\Http\FormRequest;
 class ConfigurationForm extends FormRequest
 {
     /**
+     * Rules of the configuration
+     *
+     * @var array
+     */
+    public $rules = [];
+
+    /**
      * Determine if the Configuration is authorized to make this request.
      *
      * @return bool
@@ -26,24 +33,19 @@ class ConfigurationForm extends FormRequest
         $keys = request()->input('keys');
 
         $this->rules = collect($keys)->mapWithKeys(function ($item) {
-            $data = json_decode($item);
+            $data = json_decode($item, true);
 
-            $validationRules = collect($data->fields)->mapWithKeys(function ($field) use ($data) {
-                $key = $data->key . '.' . $field->name;
+            $validationRules = collect($data['fields'])->mapWithKeys(function ($field) use ($data) {
+                $key = $data['key'] . '.' . $field['name'];
 
                 // Check delete key exist in the request
                 if (! $this->has($key . '.delete')) {
-                    $validation = isset($field->validation) ? $field->validation : '';
+                    $validation = isset($field['validation']) ? $field['validation'] : '';
 
-                    return [$key =>$validation];
+                    return [$key => $validation];
                 }
 
                 return [];
-
-                // Check if validation information is available in the field
-                $validation = isset($field->validation) ? $field->validation : '';
-
-                return [$key => $validation];
             })->toArray();
 
             return $validationRules;
