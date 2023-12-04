8885d54ebff6baeab74341f0e264ec55f752f1ee
amit kumar laravel <amitkumar.laravel358@webkul.in>
2023-12-04 15:43:36 +0530

    Fixed Issue #9077

--- packages/Webkul/Admin/src/Resources/views/components/tinymce/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/tinymce/index.blade.php
@@ -267,27 +267,29 @@
                         directionality : "{{ core()->getCurrentLocale()->direction }}",
 
                         setup: editor => {
-                            editor.ui.registry.addIcon('magic', '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"> <g clip-path="url(#clip0_3148_2242)"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12.1484 9.31989L9.31995 12.1483L19.9265 22.7549L22.755 19.9265L12.1484 9.31989ZM12.1484 10.7341L10.7342 12.1483L13.5626 14.9767L14.9768 13.5625L12.1484 10.7341Z" fill="#2563EB"/> <path d="M11.0877 3.30949L13.5625 4.44748L16.0374 3.30949L14.8994 5.78436L16.0374 8.25924L13.5625 7.12124L11.0877 8.25924L12.2257 5.78436L11.0877 3.30949Z" fill="#2563EB"/> <path d="M2.39219 2.39217L5.78438 3.95197L9.17656 2.39217L7.61677 5.78436L9.17656 9.17655L5.78438 7.61676L2.39219 9.17655L3.95198 5.78436L2.39219 2.39217Z" fill="#2563EB"/> <path d="M3.30947 11.0877L5.78434 12.2257L8.25922 11.0877L7.12122 13.5626L8.25922 16.0374L5.78434 14.8994L3.30947 16.0374L4.44746 13.5626L3.30947 11.0877Z" fill="#2563EB"/> </g> <defs> <clipPath id="clip0_3148_2242"> <rect width="24" height="24" fill="white"/> </clipPath> </defs> </svg>');
+                            <?php if (core()->getConfigData('general.magic_ai.settings.enabled')): ?>
+                                editor.ui.registry.addIcon('magic', '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"> <g clip-path="url(#clip0_3148_2242)"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12.1484 9.31989L9.31995 12.1483L19.9265 22.7549L22.755 19.9265L12.1484 9.31989ZM12.1484 10.7341L10.7342 12.1483L13.5626 14.9767L14.9768 13.5625L12.1484 10.7341Z" fill="#2563EB"/> <path d="M11.0877 3.30949L13.5625 4.44748L16.0374 3.30949L14.8994 5.78436L16.0374 8.25924L13.5625 7.12124L11.0877 8.25924L12.2257 5.78436L11.0877 3.30949Z" fill="#2563EB"/> <path d="M2.39219 2.39217L5.78438 3.95197L9.17656 2.39217L7.61677 5.78436L9.17656 9.17655L5.78438 7.61676L2.39219 9.17655L3.95198 5.78436L2.39219 2.39217Z" fill="#2563EB"/> <path d="M3.30947 11.0877L5.78434 12.2257L8.25922 11.0877L7.12122 13.5626L8.25922 16.0374L5.78434 14.8994L3.30947 16.0374L4.44746 13.5626L3.30947 11.0877Z" fill="#2563EB"/> </g> <defs> <clipPath id="clip0_3148_2242"> <rect width="24" height="24" fill="white"/> </clipPath> </defs> </svg>');
 
-                            editor.ui.registry.addButton('aibutton', {
-                                text: "@lang('admin::app.components.tinymce.ai-btn-tile')",
-                                icon: 'magic',
-                                enabled: self.ai.enabled,
+                                editor.ui.registry.addButton('aibutton', {
+                                    text: "@lang('admin::app.components.tinymce.ai-btn-tile')",
+                                    icon: 'magic',
+                                    enabled: self.ai.enabled,
 
-                                onAction: function () {
-                                    self.ai = {
-                                        prompt: self.prompt,
+                                    onAction: function () {
+                                        self.ai = {
+                                            prompt: self.prompt,
 
-                                        content: null,
-                                    };
+                                            content: null,
+                                        };
 
-                                    self.$refs.magicAIModal.toggle()
-                                }
-                            });
+                                        self.$refs.magicAIModal.toggle()
+                                    }
+                                });
 
-                            editor.on('keyup', () => {
-                                this.field.onInput(editor.getContent());
-                            });
+                                editor.on('keyup', () => {
+                                    this.field.onInput(editor.getContent());
+                                });
+                            <?php endif; ?>
                         },
                     });
                 },
