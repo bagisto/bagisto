83d873fdde4aea6bdf4c1c6bb98a1c4403dc74dd
Jitendra <jitendra@webkul.in>
2023-12-05 12:00:39 +0530

    Fixed accessibility issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+
+        \URL::forceScheme('https');
     }
 
     /**
