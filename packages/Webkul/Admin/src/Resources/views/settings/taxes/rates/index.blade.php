7a9bf464a3d15923f733fd2b5cc0b3bfb64abed1
Shivendra <shivendra.gupta423@webkul.in>
2023-12-26 12:52:30 +0530

    fixed import button

--- packages/Webkul/Admin/src/Resources/views/settings/taxes/rates/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/settings/taxes/rates/index.blade.php
@@ -4,14 +4,14 @@
         @lang('admin::app.settings.taxes.rates.index.title')
     </x-slot:title>
 
-    <div class="flex justify-between items-center">
+    <div class="flex gap-4 justify-between items-center max-sm:flex-wrap">
         <p class="text-xl text-gray-800 dark:text-white font-bold">
             @lang('admin::app.settings.taxes.rates.index.title')
         </p>
-        
+
         <div class="flex gap-x-2.5 items-center">
-            <!-- Tax rate import -->
-            <v-tax-rates-import>
+                <!-- Tax rate import -->
+                <v-tax-rates-import>
                 <button
                     class="transparent-button hover:bg-gray-200 dark:hover:bg-gray-800 dark:text-white"
                 >
@@ -21,7 +21,7 @@
 
             <!-- Tax Rate Export -->
             <x-admin::datagrid.export src="{{ route('admin.settings.taxes.rates.index') }}"></x-admin::datagrid.export>
-
+            
             <!-- Create New Pages Button -->
             @if (bouncer()->hasPermission('settings.taxes.tax-rates.create'))
                 <a 
@@ -42,67 +42,63 @@
 
     @pushOnce('scripts')
         <script type="text/x-template" id="v-tax-rates-import-template">
-            <div class="flex justify-between items-center">
-                <div class="flex gap-x-2.5 items-center">
-                    <div class="flex gap-x-2.5 items-center">
-                        <!-- Import Button -->
-                        <button
-                            href="{{ route('admin.settings.taxes.rates.import') }}"
-                            class="transparent-button hover:bg-gray-200 dark:hover:bg-gray-800 dark:text-white"
-                            @click="$refs.importTaxRates.toggle()"
-                        >
-                            @lang('Import')
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Model Form -->
-            <x-admin::form
-                v-slot="{ meta, errors, handleSubmit }"
-                as="div"
-            >
-                <form
-                    @submit="handleSubmit($event, create)"
-                    ref="importTaxForm"
+            <div>
+                <!-- Import Button -->
+                <button
+                    href="{{ route('admin.settings.taxes.rates.import') }}"
+                    class="transparent-button hover:bg-gray-200 dark:hover:bg-gray-800 dark:text-white"
+                    @click="$refs.importTaxRates.toggle()"
                 >
-                    <x-admin::modal ref="importTaxRates">
-                        <!-- Modal Header -->
-                        <x-slot:header>
-                            <p class="text-lg text-gray-800 dark:text-white font-bold">
-                                @lang('admin::app.export.download')
-                            </p>
-                        </x-slot:header>
+                    @lang('Import')
+                </button>
 
-                        <!-- Modal Content -->
-                        <x-slot:content>
-                            <x-admin::form action="">
-                                <x-admin::form.control-group>
-                                    <x-admin::form.control-group.control
-                                        type="file"
-                                        id="file"
-                                        name="file"
-                                        ref="importFile"
+                <!-- Modal Form -->
+                <x-admin::form
+                    v-slot="{ meta, errors, handleSubmit }"
+                    as="div"
+                >
+                    <form
+                        @submit="handleSubmit($event, create)"
+                        ref="importTaxForm"
+                    >
+                        <x-admin::modal ref="importTaxRates">
+                            <!-- Modal Header -->
+                            <x-slot:header>
+                                <p class="text-lg text-gray-800 dark:text-white font-bold">
+                                    @lang('admin::app.export.download')
+                                </p>
+                            </x-slot:header>
+
+                            <!-- Modal Content -->
+                            <x-slot:content>
+                                <x-admin::form action="">
+                                    <x-admin::form.control-group>
+                                        <x-admin::form.control-group.control
+                                            type="file"
+                                            id="file"
+                                            name="file"
+                                            ref="importFile"
+                                        >
+                                        </x-admin::form.control-group.control>
+                                    </x-admin::form.control-group>
+                                </x-admin::form>
+                            </x-slot:content>
+
+                            <!-- Modal Footer -->
+                            <x-slot:footer>
+                                <div class="flex gap-x-2.5 items-center">
+                                    <button
+                                        type="submit"
+                                        class="primary-button"
                                     >
-                                    </x-admin::form.control-group.control>
-                                </x-admin::form.control-group>
-                            </x-admin::form>
-                        </x-slot:content>
-
-                        <!-- Modal Footer -->
-                        <x-slot:footer>
-                            <div class="flex gap-x-2.5 items-center">
-                                <button
-                                    type="submit"
-                                    class="primary-button"
-                                >
-                                    @lang('admin::app.settings.taxes.categories.index.create.save-btn')
-                                </button>
-                            </div>
-                        </x-slot:footer>
-                    </x-admin::modal>
-                </form>
-            </x-admin::form>
+                                        @lang('admin::app.settings.taxes.categories.index.create.save-btn')
+                                    </button>
+                                </div>
+                            </x-slot:footer>
+                        </x-admin::modal>
+                    </form>
+                </x-admin::form>
+            </div>
         </script>
 
         <script type="module">
