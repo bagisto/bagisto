80dd765077e5a553dd2007cb7c4c4390be080328
suraj kashyap <suraj.kashyap370@webkul.in>
2023-12-28 18:18:49 +0530

    Dashboard test cases and come code refactor.

--- packages/Webkul/Admin/tests/Feature/Dashboard/DashboardTest.php
+++ packages/Webkul/Admin/tests/Feature/Dashboard/DashboardTest.php
@@ -1,6 +1,31 @@
 <?php
 
+use Webkul\Checkout\Models\Cart;
+use Webkul\Checkout\Models\CartItem;
+use Webkul\Customer\Models\Customer;
+use Webkul\Faker\Helpers\Product as ProductFaker;
+use Webkul\Product\Models\Product;
+use Webkul\Sales\Models\Invoice;
+use Webkul\Sales\Models\Order;
+use Webkul\Sales\Models\OrderAddress;
+use Webkul\Sales\Models\OrderItem;
+use Webkul\Sales\Models\OrderPayment;
+
 use function Pest\Laravel\get;
+
+afterEach(function () {
+    /**
+     * Cleaning up rows which are created.
+     */
+    Customer::query()->delete();
+    OrderAddress::query()->delete();
+    Order::query()->delete();
+    OrderPayment::query()->delete();
+    CartItem::query()->delete();
+    Cart::query()->delete();
+    Invoice::query()->delete();
+    Product::query()->delete();
+});
 
 it('should return the dashboard index page', function () {
     // Act and Assert
@@ -15,4 +40,247 @@ it('should return the dashboard index page', function () {
         ->assertSeeText(trans('admin::app.dashboard.index.today-sales'));
 });
 
-it('should ')
+it('should show the dashboard over all stats', function () {
+    // Arrange
+    $product = (new ProductFaker([
+        'attributes' => [
+            5 => 'new',
+        ],
+
+        'attribute_value' => [
+            'new' => [
+                'boolean_value' => true,
+            ],
+        ],
+    ]))
+        ->getSimpleProductFactory()
+        ->create();
+
+    $order = Order::factory()->create([
+        'cart_id' => $cartId = CartItem::factory()->create([
+            'product_id' => $product->id,
+        ])->id,
+    ]);
+
+    OrderItem::factory()->create([
+        'product_id' => $product->id,
+        'order_id'   => $order->id,
+    ]);
+
+    OrderPayment::factory()->create([
+        'order_id' => $order->id,
+    ]);
+
+    OrderAddress::factory()->create([
+        'order_id'     => $order->id,
+        'cart_id'      => $cartId,
+        'address_type' => OrderAddress::ADDRESS_TYPE_BILLING,
+    ]);
+
+    Invoice::factory([
+        'order_id' => $order->id,
+        'state'    => 'paid',
+    ])->create();
+
+    // Act and Assert
+    $this->loginAsAdmin();
+
+    get(route('admin.dashboard.stats', [
+        'type' => 'over-all',
+    ]))
+        ->assertOk()
+        ->assertJsonPath('statistics.total_customers.current', 3)
+        ->assertJsonPath('statistics.total_orders.current', 1)
+        ->assertJsonPath('statistics.total_sales.current', $order->grand_total);
+});
+
+it('should show the dashboard today stats', function () {
+    // Arrange
+    $product = (new ProductFaker([
+        'attributes' => [
+            5 => 'new',
+        ],
+
+        'attribute_value' => [
+            'new' => [
+                'boolean_value' => true,
+            ],
+        ],
+    ]))
+        ->getSimpleProductFactory()
+        ->create();
+
+    $order = Order::factory()->create([
+        'cart_id' => $cartId = CartItem::factory()->create([
+            'product_id' => $product->id,
+        ])->id,
+    ]);
+
+    OrderItem::factory()->create([
+        'product_id' => $product->id,
+        'order_id'   => $order->id,
+    ]);
+
+    OrderPayment::factory()->create([
+        'order_id' => $order->id,
+    ]);
+
+    OrderAddress::factory()->create([
+        'order_id'     => $order->id,
+        'cart_id'      => $cartId,
+        'address_type' => OrderAddress::ADDRESS_TYPE_BILLING,
+    ]);
+
+    Invoice::factory([
+        'order_id' => $order->id,
+        'state'    => 'paid',
+    ])->create();
+
+    // Act and Assert
+    $this->loginAsAdmin();
+
+    get(route('admin.dashboard.stats', [
+        'type' => 'today',
+    ]))
+        ->assertOk()
+        ->assertJsonPath('statistics.total_customers.current', 3)
+        ->assertJsonPath('statistics.total_orders.current', 1)
+        ->assertJsonPath('statistics.total_sales.current', $order->grand_total)
+        ->assertJsonPath('statistics.orders.0.id', $order->id)
+        ->assertJsonPath('statistics.orders.0.status', $order->status)
+        ->assertJsonPath('statistics.orders.0.customer_email', $order->customer->email);
+});
+
+it('should show the dashboard stock threshold products stats', function () {
+    // Arrange
+    $product = (new ProductFaker([
+        'attributes' => [
+            5 => 'new',
+        ],
+
+        'attribute_value' => [
+            'new' => [
+                'boolean_value' => true,
+            ],
+        ],
+    ]))
+        ->getSimpleProductFactory()
+        ->create();
+
+    $order = Order::factory()->create([
+        'cart_id' => $cartId = CartItem::factory()->create([
+            'product_id' => $product->id,
+        ])->id,
+    ]);
+
+    OrderItem::factory()->create([
+        'product_id' => $product->id,
+        'order_id'   => $order->id,
+    ]);
+
+    OrderPayment::factory()->create([
+        'order_id' => $order->id,
+    ]);
+
+    OrderAddress::factory()->create([
+        'order_id'     => $order->id,
+        'cart_id'      => $cartId,
+        'address_type' => OrderAddress::ADDRESS_TYPE_BILLING,
+    ]);
+
+    Invoice::factory([
+        'order_id' => $order->id,
+        'state'    => 'paid',
+    ])->create();
+
+    // Act and Assert
+    $this->loginAsAdmin();
+
+    get(route('admin.dashboard.stats', [
+        'type' => 'stock-threshold-products',
+    ]))
+        ->assertOk()
+        ->assertJsonPath('statistics.0.id', $product->id)
+        ->assertJsonPath('statistics.0.sku', $product->sku);
+});
+
+it('should show the dashboard total sales stats', function () {
+    // Arrange
+    $product = (new ProductFaker([
+        'attributes' => [
+            5 => 'new',
+        ],
+
+        'attribute_value' => [
+            'new' => [
+                'boolean_value' => true,
+            ],
+        ],
+    ]))
+        ->getSimpleProductFactory()
+        ->create();
+
+    $order = Order::factory()->create([
+        'cart_id' => $cartId = CartItem::factory()->create([
+            'product_id' => $product->id,
+        ])->id,
+    ]);
+
+    OrderItem::factory()->create([
+        'product_id' => $product->id,
+        'order_id'   => $order->id,
+    ]);
+
+    OrderPayment::factory()->create([
+        'order_id' => $order->id,
+    ]);
+
+    OrderAddress::factory()->create([
+        'order_id'     => $order->id,
+        'cart_id'      => $cartId,
+        'address_type' => OrderAddress::ADDRESS_TYPE_BILLING,
+    ]);
+
+    Invoice::factory([
+        'order_id' => $order->id,
+        'state'    => 'paid',
+    ])->create();
+
+    // Act and Assert
+    $this->loginAsAdmin();
+
+    get(route('admin.dashboard.stats', [
+        'type' => 'total-sales',
+    ]))
+        ->assertOk()
+        ->assertJsonPath('statistics.total_orders.current', 1)
+        ->assertJsonPath('statistics.total_sales.current', $order->grand_total);
+});
+
+it('should show the dashboard total visitors stats', function () {
+    // Arrange
+    $product = (new ProductFaker([
+        'attributes' => [
+            5 => 'new',
+        ],
+
+        'attribute_value' => [
+            'new' => [
+                'boolean_value' => true,
+            ],
+        ],
+    ]))
+        ->getSimpleProductFactory()
+        ->create();
+
+    visitor()->visit();
+
+    $this->loginAsAdmin();
+
+    get(route('admin.dashboard.stats', [
+        'type' => 'total-visitors',
+    ]))
+        ->assertOk()
+        ->assertJsonPath('statistics.total.current', 1)
+        ->assertJsonPath('statistics.unique.current', 1);
+});
