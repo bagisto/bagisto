23e5849c60ebe4cfadcf297c21295135f6109bf2
Jitendra <jitendra@webkul.in>
2023-12-29 12:07:45 +0530

    Fixed linting issue

--- config/products.php
+++ config/products.php
@@ -3,9 +3,9 @@
 return [
     /**
      * Skip attribute during product copy.
-     * 
+     *
      * Supported Relations: ['categories', 'inventories', 'customer_group_prices', 'images', 'videos', 'product_relations']
-     * 
+     *
      * Support Attributes: All Attributes (Example: 'sku', 'product_number', etc)
      */
     'copy' => [
