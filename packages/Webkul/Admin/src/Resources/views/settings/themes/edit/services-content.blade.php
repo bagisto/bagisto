f04540725f677759dd79ffcedea80027fd4a968f
suraj kashyap <suraj.kashyap370@webkul.in>
2023-12-29 10:33:04 +0530

    Theme datagrid filterable issue fix and show proper illustration for empty data.

--- packages/Webkul/Admin/src/Resources/views/settings/themes/edit/services-content.blade.php
+++ packages/Webkul/Admin/src/Resources/views/settings/themes/edit/services-content.blade.php
@@ -135,12 +135,12 @@
                             class="grid gap-3.5 justify-center justify-items-center py-10 px-2.5"
                             v-else
                         >
-                            <img    
-                                class="w-[120px] h-[120px] border border-dashed border-gray-300 dark:border-gray-800 rounded dark:invert dark:mix-blend-exclusion"
-                                src="{{ bagisto_asset('images/product-placeholders/front.svg') }}"
-                                alt="add-product-to-store"
+                            <img
+                                class="w-[120px] h-[120px] p-2 dark:invert dark:mix-blend-exclusion"
+                                src="{{ bagisto_asset('images/empty-placeholders/default.svg') }}"
+                                alt="@lang('admin::app.settings.themes.edit.services-content.services')"
                             >
-            
+
                             <div class="flex flex-col gap-1.5 items-center">
                                 <p class="text-base text-gray-400 font-semibold">
                                     @lang('admin::app.settings.themes.edit.services-content.add-btn')
