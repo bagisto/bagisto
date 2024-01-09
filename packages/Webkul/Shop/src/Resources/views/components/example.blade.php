a5273e30586f68631541bd09c84538e7b7738147
Devansh <68321766+devansh-webkul@users.noreply.github.com>
2024-01-09 15:44:20 +0530

    Update example.blade.php

--- packages/Webkul/Shop/src/Resources/views/components/example.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/example.blade.php
@@ -348,17 +348,10 @@
 </x-shop::layouts>
 
 <!-- label class -->
-
 <div class="label-canceled"></div>
-
 <div class="label-info"></div>
-
 <div class="label-completed"></div>
-
 <div class="label-closed"></div>
-
 <div class="label-processing"></div>
-
 <div class="label-pending"></div>
-
 <div class="label-canceled"></div>
