a74e9a235d7387637f7bc9f640092cd96ed8d89b
Devansh Bawari <devansh.bawari419@webkul.in>
2024-01-23 14:11:27 +0530

    feat: core assertions added

---
+++ packages/Webkul/Core/tests/Concerns/CoreAssertions.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Webkul\Core\Tests\Concerns;
+
+trait CoreAssertions
+{
+    /**
+     * Assert model wise.
+     */
+    public function assertModelWise(array $modelWiseAssertions): void
+    {
+        foreach ($modelWiseAssertions as $modelClassName => $modelAssertions) {
+            foreach ($modelAssertions as $assertion) {
+                $this->assertDatabaseHas(app($modelClassName)->getTable(), $assertion);
+            }
+        }
+    }
+}
