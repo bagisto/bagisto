a6dd597d63ab56132a14bd3e12e3ca5b932bf1ff
suraj kashyap <suraj.kashyap370@webkul.in>
2024-02-01 17:34:33 +0530

    test: admin package validation error test case has been done.

--- packages/Webkul/Admin/tests/Feature/Sales/TransactionTest.php
+++ packages/Webkul/Admin/tests/Feature/Sales/TransactionTest.php
@@ -25,6 +25,92 @@ it('should return the index page of transactions', function () {
         ->assertSeeText(trans('admin::app.sales.transactions.index.title'));
 });
 
+it('should faild the validation error when store the transaction when certain inputs not provided', function () {
+    // Arrange
+    $product = (new ProductFaker([
+        'attributes' => [
+            5 => 'new',
+        ],
+
+        'attribute_value' => [
+            'new' => [
+                'boolean_value' => true,
+            ],
+        ],
+    ]))
+        ->getSimpleProductFactory()
+        ->create();
+
+    $customer = Customer::factory()->create();
+
+    CartItem::factory()->create([
+        'product_id' => $product->id,
+        'sku'        => $product->sku,
+        'type'       => $product->type,
+        'name'       => $product->name,
+        'cart_id'    => $cartId = Cart::factory()->create([
+            'customer_id'         => $customer->id,
+            'customer_email'      => $customer->email,
+            'customer_first_name' => $customer->first_name,
+            'customer_last_name'  => $customer->last_name,
+        ])->id,
+    ]);
+
+    $order = Order::factory()->create([
+        'cart_id'             => $cartId,
+        'customer_id'         => $customer->id,
+        'customer_email'      => $customer->email,
+        'customer_first_name' => $customer->first_name,
+        'customer_last_name'  => $customer->last_name,
+    ]);
+
+    OrderItem::factory()->create([
+        'product_id' => $product->id,
+        'order_id'   => $order->id,
+        'sku'        => $product->sku,
+        'type'       => $product->type,
+        'name'       => $product->name,
+    ]);
+
+    $payment = OrderPayment::factory()->create([
+        'order_id' => $order->id,
+    ]);
+
+    $customerAddress = CustomerAddress::factory()->create([
+        'customer_id' => $customer->id,
+    ]);
+
+    OrderAddress::factory()->create([
+        'order_id'     => $order->id,
+        'cart_id'      => $cartId,
+        'address1'     => $customerAddress->address1,
+        'country'      => $customerAddress->country,
+        'state'        => $customerAddress->state,
+        'city'         => $customerAddress->city,
+        'postcode'     => $customerAddress->postcode,
+        'phone'        => $customerAddress->phone,
+        'address_type' => OrderAddress::ADDRESS_TYPE_BILLING,
+    ]);
+
+    $invoice = Invoice::factory([
+        'order_id'         => $order->id,
+        'sub_total'        => $order->grand_total,
+        'base_sub_total'   => $order->grand_total,
+        'grand_total'      => $order->grand_total,
+        'base_grand_total' => $order->grand_total,
+        'increment_id'     => app(InvoiceSequencer::class)->resolveGeneratorClass(),
+    ])->create();
+
+    // Act and Assert
+    $this->loginAsAdmin();
+
+    postJson(route('admin.sales.transactions.store'))
+        ->assertJsonValidationErrorFor('invoice_id')
+        ->assertJsonValidationErrorFor('payment_method')
+        ->assertJsonValidationErrorFor('amount')
+        ->assertUnprocessable();
+});
+
 it('should store the order transaction', function () {
     // Arrange
     $product = (new ProductFaker([
