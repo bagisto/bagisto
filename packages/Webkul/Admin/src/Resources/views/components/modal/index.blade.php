6af3556a6210a7e001ae85a7df2f0afae819bd5a
suraj kashyap <suraj.kashyap370@webkul.in>
2024-02-13 16:09:28 +0530

    revert some changes

--- packages/Webkul/Admin/src/Resources/views/components/modal/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/modal/index.blade.php
@@ -123,6 +123,8 @@
 
                     if (this.isOpen) {
                         document.body.style.overflow = 'hidden';
+                    } else {
+                        document.body.style.overflow ='scroll';
                     }
 
                     this.$emit('toggle', { isActive: this.isOpen });
