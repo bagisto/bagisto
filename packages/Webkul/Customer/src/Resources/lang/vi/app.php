f08633ad0fb7013a9922a6badf6d0d647e31091e
Phan Xuân Dũng <dungpx@vaixgroup.com>
2024-02-15 15:34:51 +0700

    [Language] Init Vietnamese Translate

---
+++ packages/Webkul/Customer/src/Resources/lang/vi/app.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'validations' => [
+        'captcha' => [
+            'captcha'  => 'Something went wrong! Please try again.',
+            'required' => 'Please select CAPTCHA',
+        ],
+
+        'vat-id' => [
+            'invalid-format' => 'Invalid Vat Format',
+        ],
+    ],
+];
