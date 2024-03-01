fb55c0731059d1eaedb0bff7eff3f6e39aad57de
suraj kashyap <suraj.kashyap370@webkul.in>
2024-03-01 15:46:23 +0530

    feat: add server exception handler.

--- packages/Webkul/Core/src/Exceptions/Handler.php
+++ packages/Webkul/Core/src/Exceptions/Handler.php
@@ -6,6 +6,7 @@ use App\Exceptions\Handler as BaseHandler;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Throwable;
 
 class Handler extends BaseHandler
 {
@@ -21,6 +22,8 @@ class Handler extends BaseHandler
         $this->handleAuthenticationException();
 
         $this->handleHttpException();
+
+        $this->handleServerException();
     }
 
     /**
@@ -54,6 +57,27 @@ class Handler extends BaseHandler
             $errorCode = in_array($exception->getStatusCode(), [401, 403, 404, 503])
                 ? $exception->getStatusCode()
                 : 500;
+
+            if ($request->wantsJson()) {
+                return response()->json([
+                    'error'       => trans("{$path}::app.errors.{$errorCode}.title"),
+                    'description' => trans("{$path}::app.shop.errors.{$errorCode}.description"),
+                ], $errorCode);
+            }
+
+            return response()->view("{$path}::errors.index", compact('errorCode'));
+        });
+    }
+
+    /**
+     * Handle the server exceptions.
+     */
+    private function handleServerException(): void
+    {
+        $this->renderable(function (Throwable $throwable, Request $request) {
+            $path = $request->is(config('app.admin_url').'/*') ? 'admin' : 'shop';
+
+            $errorCode = 500;
 
             if ($request->wantsJson()) {
                 return response()->json([
