d7c85ae08e830b6370988e721cb4c1d2382d82b1
Jitendra <jitendra@webkul.in>
2024-03-11 16:47:57 +0530

    Fixed failing test

--- packages/Webkul/Shop/src/Routes/customer-routes.php
+++ packages/Webkul/Shop/src/Routes/customer-routes.php
@@ -39,7 +39,7 @@ Route::group(['middleware' => ['locale', 'theme', 'currency']], function () {
         Route::controller(SessionController::class)->prefix('login')->group(function () {
             Route::get('', 'index')->name('shop.customer.session.index');
 
-            Route::post('', 'create')->name('shop.customer.session.create');
+            Route::post('', 'store')->name('shop.customer.session.create');
         });
 
         /**
