94cafbd24babbaa4444ea30ca89402a228f96143
suraj kashyap <suraj.kashyap370@webkul.in>
2024-05-16 18:36:38 +0530

    remove admin dependency from core.

--- packages/Webkul/Core/tests/CoreTestCase.php
+++ packages/Webkul/Core/tests/CoreTestCase.php
@@ -3,10 +3,9 @@
 namespace Webkul\Core\Tests;
 
 use Tests\TestCase;
-use Webkul\Admin\Tests\Concerns\AdminTestBench;
 use Webkul\Core\Tests\Concerns\CoreAssertions;
 
 class CoreTestCase extends TestCase
 {
-    use AdminTestBench, CoreAssertions;
+    use CoreAssertions;
 }
