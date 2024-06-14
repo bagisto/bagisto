6b8ece158f520c19f8a88d43a7fb5467102035be
Amit Kumar <amitkumar.laravel358@webkul.in>
2024-06-14 15:53:31 +0530

    Fix some chnages

--- packages/Webkul/Shop/src/Resources/views/components/layouts/footer/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/layouts/footer/index.blade.php
@@ -114,7 +114,7 @@
     
                             <button
                                 type="submit"
-                                class="absolute top-2 flex w-max items-center rounded-xl bg-white px-7 py-3.5 font-medium hover:bg-zinc-100 max-md:px-5 max-md:py-2.5 max-md:text-xs max-sm:-mt-1 max-sm:rounded-lg max-sm:px-4 max-sm:py-2 ltr:right-2 rtl:left-2"
+                                class="absolute top-1.5 flex w-max items-center rounded-xl bg-white px-7 py-2.5 font-medium hover:bg-zinc-100 max-md:px-5 max-md:text-xs max-sm:-mt-1 max-sm:rounded-lg max-sm:px-4 max-sm:py-2 ltr:right-2 rtl:left-2"
                             >
                                 @lang('shop::app.components.layouts.footer.subscribe')
                             </button>
