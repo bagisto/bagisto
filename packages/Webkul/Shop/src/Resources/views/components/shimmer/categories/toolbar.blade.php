2b7a8a6c5b91f79f5233c46ebada98bbb2f9fa20
Amit Kumar <amitkumar.laravel358@webkul.in>
2024-06-17 11:43:50 +0530

    Fix shimmer on category page toolbar section.

--- packages/Webkul/Shop/src/Resources/views/components/shimmer/categories/toolbar.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/shimmer/categories/toolbar.blade.php
@@ -1,7 +1,7 @@
 <div class="flex justify-between max-md:hidden max-md:items-center">
     <div class="hidden max-md:block"></div>
 
-    <div class="shimmer h-[54px] w-[143px] rounded-lg"></div>
+    <div class="shimmer h-[54px] w-[185px] rounded-lg"></div>
 
     <div class="flex items-center gap-10 max-md:hidden">
         <div class="shimmer h-[54px] w-[84px] rounded-lg"></div>
