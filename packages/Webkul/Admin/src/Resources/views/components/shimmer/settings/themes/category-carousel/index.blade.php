dc6c5a97535aacbf09fb2cb846ecacbdcab166e3
Shivendra <shivendra.gupta423@webkul.in>
2024-06-17 10:40:03 +0530

    refactor theme customization

--- packages/Webkul/Admin/src/Resources/views/components/shimmer/settings/themes/category-carousel/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/shimmer/settings/themes/category-carousel/index.blade.php
@@ -50,29 +50,6 @@
             </div>
         </div>
     </div>
-            
-    <!-- General -->
-    <div class="flex w-[360px] max-w-full flex-col gap-2 max-sm:w-full">
-        <div class="box-shadow rounded bg-white px-4 dark:bg-gray-900">
-            <div class="flex items-center justify-between py-4">
-                <div class="shimmer h-5 w-20"></div>
 
-                <div class="shimmer h-5 w-6"></div>
-            </div>
-
-            @for ($i = 0; $i < 3; $i++)    
-                <div class="mb-4">
-                    <div class="shimmer mb-2 flex h-3.5 w-16 items-center gap-1"></div>
-
-                    <div class="shimmer flex h-10 w-full rounded-md py-px"></div>
-                </div>
-            @endfor
-
-            <div class="mb-5 grid grid-cols-1 gap-2">
-                <div class="shimmer h-3.5 w-16"></div>
-
-                <div class="shimmer h-5 w-10 rounded-full"></div>
-            </div>
-        </div>
-    </div>
+    {{-- <x-admin::shimmer.settings.themes.general-content /> --}}
 </div>
--- packages/Webkul/Admin/src/Resources/views/components/shimmer/settings/themes/static-content/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/shimmer/settings/themes/static-content/index.blade.php
@@ -30,29 +30,6 @@
             <div class="shimmer h-[214px] w-full"></div>
         </div>
     </div>
-            
-    <!-- General -->
-    <div class="flex w-[360px] max-w-full flex-col gap-2 max-sm:w-full">
-        <div class="box-shadow rounded bg-white px-4 dark:bg-gray-900">
-            <div class="flex items-center justify-between py-4">
-                <div class="shimmer h-4 w-20"></div>
 
-                <div class="shimmer h-4 w-4"></div>
-            </div>
-
-            @for ($i = 1; $i < 4; $i++)    
-                <div class="mb-4">
-                    <div class="shimmer mb-1.5 flex h-4 w-16 items-center gap-1"></div>
-
-                    <div class="flex h-10 w-full rounded-md border px-3 py-2 transition-all dark:border-gray-800 dark:bg-gray-900"></div>
-                </div>
-            @endfor
-
-            <div class="mb-5 grid grid-cols-1 gap-2">
-                <div class="shimmer h-4 w-12"></div>
-
-                <div class="shimmer h-5 w-10 rounded-full"></div>
-            </div>
-        </div>
-    </div>
+    <x-admin::shimmer.settings.themes.general-content />
 </div>
