47939b0d627251fcdedf648b9fda400fcaaf820d
amit kumar laravel <amitkumar.laravel358@webkul.in>
2024-07-31 18:18:03 +0530

    Added Missing Price class on diffrent type products.

--- packages/Webkul/Shop/src/Resources/views/products/prices/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/prices/index.blade.php
@@ -1,6 +1,6 @@
 @if ($prices['final']['price'] < $prices['regular']['price'])
     <p
-        class="font-medium text-zinc-500 line-through max-sm:leading-4"
+        class="final-price font-medium text-zinc-500 line-through max-sm:leading-4"
         aria-label="{{ $prices['regular']['formatted_price'] }}"
     >
         {{ $prices['regular']['formatted_price'] }}
@@ -10,7 +10,7 @@
         {{ $prices['final']['formatted_price'] }}
     </p>
 @else
-    <p class="font-semibold max-sm:leading-4">
+    <p class="final-price font-semibold max-sm:leading-4">
         {{ $prices['regular']['formatted_price'] }}
     </p>
 @endif
