f6ea2153727f524273aae399bf45fcf088bc6811
amit kumar laravel <amitkumar.laravel358@webkul.in>
2024-09-10 15:09:17 +0530

    Remove un used code .

--- packages/Webkul/Shop/src/Http/Controllers/Controller.php
+++ packages/Webkul/Shop/src/Http/Controllers/Controller.php
@@ -9,21 +9,4 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use DispatchesJobs, ValidatesRequests;
-
-    /**
-     * Sanitizes the input recursively.
-     */
-    public function sanitizeArray(mixed $input): mixed
-    {
-        if (is_array($input)) {
-            // Recursively sanitize array elements
-            return array_map([$this, 'sanitizeArray'], $input);
-        }
-
-        // Sanitize string values
-        return htmlspecialchars(
-            str_replace(['(', ')'], ['&#x28;', '&#x29;'], preg_replace('/\{\{(.*?)\}\}/', '$1', (string) $input)),
-            ENT_QUOTES, 'UTF-8'
-        );
-    }
 }
