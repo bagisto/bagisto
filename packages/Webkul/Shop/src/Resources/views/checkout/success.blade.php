1979f9a1e7d225a6b90d7b436d1d12b7d8c46ff1
amit kumar laravel <amitkumar.laravel358@webkul.in>
2024-11-14 14:07:11 +0530

    Added Improvement

--- packages/Webkul/Shop/src/Resources/views/checkout/success.blade.php
+++ packages/Webkul/Shop/src/Resources/views/checkout/success.blade.php
@@ -1,40 +1,12 @@
 <x-shop::layouts
-	:has-header="false"
+	:has-header="true"
 	:has-feature="false"
-	:has-footer="false"
+	:has-footer="true"
 >
     <!-- Page Title -->
     <x-slot:title>
 		@lang('shop::app.checkout.success.thanks')
     </x-slot>
-
-	{!! view_render_event('bagisto.shop.checkout.onepage.header.before') !!}
-
-    <!-- Page Header -->
-    <div class="flex-wrap">
-        <div class="flex w-full justify-between border border-b border-l-0 border-r-0 border-t-0 px-[60px] py-4 max-lg:px-8 max-md:px-4">
-            <div class="flex items-center gap-x-14 max-[1180px]:gap-x-9">
-                <a
-                    href="{{ route('shop.home.index') }}"
-                    class="flex min-h-[30px]"
-                    aria-label="@lang('shop::checkout.onepage.index.bagisto')"
-                >
-                    <img
-                        src="{{ core()->getCurrentChannel()->logo_url ?? bagisto_asset('images/logo.svg') }}"
-                        alt="{{ config('app.name') }}"
-                        width="131"
-                        height="29"
-                    >
-                </a>
-            </div>
-
-            @guest('customer')
-                @include('shop::checkout.login')
-            @endguest
-        </div>
-    </div>
-
-    {!! view_render_event('bagisto.shop.checkout.onepage.header.after') !!}
 
 	<!-- Page content -->
 	<div class="container mt-8 px-[60px] max-lg:px-8">
@@ -44,8 +16,8 @@
 			<img 
 				class="max-md:h-[100px] max-md:w-[100px]"
 				src="{{ bagisto_asset('images/thank-you.png') }}" 
-				alt="thankyou" 
-				title=""
+				alt="@lang('shop::app.checkout.success.thanks')" 
+				title="@lang('shop::app.checkout.success.thanks')"
 			>
 
 			{{ view_render_event('bagisto.shop.checkout.success.image.after', ['order' => $order]) }}
@@ -53,14 +25,14 @@
 			<p class="text-xl max-md:text-sm">
 				@if (auth()->guard('customer')->user())
 					@lang('shop::app.checkout.success.order-id-info', [
-						'order_id' => '<a class="text-blue-700" href="' . route('shop.customers.account.orders.view', $order->id) . '">' . $order->increment_id . '</a>'
+						'order_id' => '<a class="text-blue-700" href="'.route('shop.customers.account.orders.view', $order->id).'">'.$order->increment_id.'</a>'
 					])
 				@else
 					@lang('shop::app.checkout.success.order-id-info', ['order_id' => $order->increment_id]) 
 				@endif
 			</p>
 
-			<p class="text-2xl font-medium max-md:text-base">
+			<p class="font-medium md:text-2xl">
 				@lang('shop::app.checkout.success.thanks')
 			</p>
 			
@@ -75,7 +47,7 @@
 			{{ view_render_event('bagisto.shop.checkout.success.continue-shopping.before', ['order' => $order]) }}
 
 			<a href="{{ route('shop.home.index') }}">
-				<div class="m-auto mx-auto block w-max cursor-pointer rounded-2xl bg-navyBlue px-11 py-3 text-center text-base font-medium text-white max-md:rounded-lg max-md:px-6 max-md:py-1.5">
+				<div class="w-max cursor-pointer rounded-2xl bg-navyBlue px-11 py-3 text-center text-base font-medium text-white max-md:rounded-lg max-md:px-6 max-md:py-1.5">
              		@lang('shop::app.checkout.cart.index.continue-shopping')
 				</div> 
 			</a>
