2635a859c3c28acb0e8b42e11168aea6b802bdf8
amit kumar laravel <amitkumar.laravel358@webkul.in>
2024-11-20 15:31:09 +0530

    Fixed All types template injection in search bar.

--- packages/Webkul/Shop/src/Resources/views/search/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/search/index.blade.php
@@ -1,9 +1,7 @@
 @php
-    if (request()->has('query')) {
-        $title = trans('shop::app.search.title', ['query' => request()->query('query')]);
-    } else {
-        $title = trans('shop::app.search.results');
-    }
+    $title = request()->has('query')
+            ? trans('shop::app.search.title', ['query' => request()->query('query')])
+            : trans('shop::app.search.results');
 @endphp
 
 <!-- SEO Meta Content -->
@@ -33,7 +31,7 @@
         <div class="mt-8 flex items-center justify-between max-md:mt-5">
             <h1
                 class="text-2xl font-medium max-sm:text-base"
-                v-text="'{{ trim($title, '\'') }}'"
+                v-text="'{{ preg_replace('/[,\\"\\\']+/', '', $title) }}'"
             >
             </h1>
         </div>
