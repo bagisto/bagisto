6576372e3977e2a3c7b5e73bf9e06d44a6ef1af4
amit kumar laravel <amitkumar.laravel358@webkul.in>
2024-12-16 10:52:55 +0530

    Fix translations for pt_BR

--- packages/Webkul/Admin/src/Resources/lang/pt_BR/app.php
+++ packages/Webkul/Admin/src/Resources/lang/pt_BR/app.php
@@ -34,7 +34,7 @@ return [
 
     'notifications' => [
         'description-text' => 'Listar todas as notificações',
-        'marked-success'   => 'All notifications have been marked as read',
+        'marked-success'   => 'Todas as notificações foram marcadas como lidas',
         'no-record'        => 'Nenhum Registro Encontrado',
         'of'               => 'de',
         'per-page'         => 'Por página',
