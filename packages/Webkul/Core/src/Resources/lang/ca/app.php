61b12a9ab9d4e5810c1e68a68fb317564b486c75
Albert Arques <albertarques@gmail.com>
2025-01-10 16:42:22 +0100

    Core literals translation

---
+++ packages/Webkul/Core/src/Resources/lang/ca/app.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'path-hint' => [
+        'template' => 'Plantilla',
+        'parents'  => 'Pares',
+    ],
+
+    'currency-position' => [
+        'options' => [
+            'left'             => 'Esquerra',
+            'left-with-space'  => 'Esquerra amb Espai',
+            'right'            => 'Dreta',
+            'right-with-space' => 'Dreta amb Espai',
+        ],
+    ],
+];
