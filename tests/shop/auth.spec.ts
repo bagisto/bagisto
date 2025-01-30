2e28959f3fc2905ac31c94da9b9445efc0017a28
amit kumar laravel <amitkumar.laravel358@webkul.in>
2025-01-30 16:52:43 +0530

    refactor: update registration test to use dynamic email and improve error handling

--- tests/shop/auth.spec.ts
+++ tests/shop/auth.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect, config } from '../utils/setup';
 
 test('register', async ({ page }) => {
+    const randomEmail = Math.random().toString(36).substring(2, 11) + '@gmail.com';
     await page.goto(`${config.baseUrl}`);
     await page.getByLabel('Profile').click();
     await page.getByRole('link', { name: 'Sign Up' }).click();
@@ -9,28 +10,22 @@ test('register', async ({ page }) => {
     await page.getByPlaceholder('Last Name').click();
     await page.getByPlaceholder('Last Name').fill('Demo');
     await page.getByPlaceholder('email@example.com').click();
-    await page.getByPlaceholder('email@example.com').fill('testUser@gmail.com');
+    await page.getByPlaceholder('email@example.com').fill(randomEmail);
     await page.getByPlaceholder('Password', { exact: true }).click();
     await page.getByPlaceholder('Password', { exact: true }).fill('testUser@123');
     await page.getByPlaceholder('Confirm Password').click();
     await page.getByPlaceholder('Confirm Password').fill('testUser@123');
     await page.locator('#main form div').filter({ hasText: 'Subscribe to newsletter' }).locator('label').first().click();
     await page.getByRole('button', { name: 'Register' }).click();
-    await page.getByText('Account created successfully.').first().click();
 
     try {
-        const getError = await page.waitForSelector('.text-red-500.text-xs.italic', { timeout: 2000 }).catch(() => null);
+        const successMessage = await page.waitForSelector('text=Account created successfully, an e-mail has been sent for verification', { timeout: 5000 }).catch(() => null);
 
-        if (getError) {
+        if (successMessage) {
+            const message = await successMessage.innerText();
+        } else {
             const errors = await page.$$eval('.text-red-500.text-xs.italic', els => els.map(el => el.innerText));
             errors.forEach(message => console.log(message));
-        } else {
-            await page.waitForSelector('.icon-toast-done', { timeout: 5000 });
-
-            const message = await page.$eval('.icon-toast-done', el => el.parentNode.innerText);
-            await page.click('.cursor-pointer.icon-cancel');
-
-            console.log(message);
         }
     } catch(e) {
         console.log(page.url());
@@ -47,22 +42,10 @@ test('login', async ({ page }) => {
     await page.getByPlaceholder('Password').fill('testUser@123');
     await page.getByRole('button', { name: 'Sign In' }).click();
 
-    try {
-        const getError = await page.waitForSelector('.text-red-500.text-xs.italic', { timeout: 2000 }).catch(() => null);
+    const errors = await page.$$eval('.text-red-500.text-xs.italic', els => els.map(el => el.innerText));
 
-        if (getError) {
-            const errors = await page.$$eval('.text-red-500.text-xs.italic', els => els.map(el => el.innerText));
-            errors.forEach(message => console.log(message));
-        } else {
-            await page.waitForSelector('.icon-toast-done', { timeout: 5000 });
-
-            const message = await page.$eval('.icon-toast-done', el => el.parentNode.innerText);
-            await page.click('.cursor-pointer.icon-cancel');
-
-            console.log(message);
-        }
-    } catch(e) {
-        console.log(page.url());
+    if (errors.length > 0) {
+        errors.forEach(message => console.log(message));
     }
 });
 
@@ -77,15 +60,4 @@ test('logout', async ({ page }) => {
     await page.getByRole('button', { name: 'Sign In' }).click();
     await page.getByLabel('Profile').click();
     await page.getByRole('link', { name: 'Logout' }).click();
-
-    try {
-        await page.waitForSelector('.icon-toast-done', { timeout: 5000 });
-
-        const message = await page.$eval('.icon-toast-done', el => el.parentNode.innerText);
-        await page.click('.cursor-pointer.icon-cancel');
-
-        console.log(message);
-    } catch(e) {
-        console.log(page.url());
-    }
 });
