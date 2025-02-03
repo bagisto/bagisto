b23921d85d9394385e9b5de19f54db9c81e0f8f2
Devansh Bawari <devansh.bawari419@webkul.in>
2025-02-03 16:40:22 +0530

    chore: come cleanup done

--- tests/utils/admin/loginHelper.ts
+++ tests/utils/admin/loginHelper.ts
@@ -1,22 +1,14 @@
-import config from '../../../config/setup';
+import { config } from '../setup';
 import * as forms from '../admin/formHelper';
 
 const baseUrl = config.baseUrl;
 
 const logIn = async (page) => {
     await page.goto(`${baseUrl}/admin/login`);
-
     await page.fill('input[name="email"]', config.adminEmail);
     await page.fill('input[name="password"]', config.adminPassword);
-
     await page.press('input[name="password"]', 'Enter');
-
-    if (await forms.testForm(page)) {
-        console.log('Successfully login');
-        return 'Successfully login';
-    } else {
-        return null;
-    }
+    await forms.testForm(page);
 }
 
 export default logIn;
