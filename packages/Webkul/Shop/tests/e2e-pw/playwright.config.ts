4bdf145728abfd66f7ccc2d2670bb47542bdcbfa
Devansh Bawari <devansh.bawari419@webkul.in>
2025-02-06 14:39:07 +0530

    fix: moved both folders in theire respective package and updated the workflow

--- packages/Webkul/Shop/tests/e2e-pw/playwright.config.ts
+++ packages/Webkul/Shop/tests/e2e-pw/playwright.config.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+dotenv.config({ path: path.resolve(__dirname, "../../../../../.env") });
 
 export default defineConfig({
     testDir: "./tests",
