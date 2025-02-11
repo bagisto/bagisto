790834c972abdce59ecbf71436cb333ea1b4544e
Devansh Bawari <devansh.bawari419@webkul.in>
2025-02-11 14:10:50 +0530

    test: fixed some more test cases

--- packages/Webkul/Shop/tests/e2e-pw/tests/auth.spec.ts
+++ packages/Webkul/Shop/tests/e2e-pw/tests/auth.spec.ts
@@ -1,31 +1,5 @@
 import { test, expect, config } from '../setup';
-
-async function register(page) {
-    const credentials = {
-        email: `testUser${Date.now()}@gmail.com`,
-        password: 'testUser@123'
-    };
-
-    await page.goto(`${config.baseUrl}`);
-    await page.getByLabel('Profile').click();
-    await page.getByRole('link', { name: 'Sign Up' }).click();
-    await page.getByPlaceholder('First Name').click();
-    await page.getByPlaceholder('First Name').fill('testUser');
-    await page.getByPlaceholder('Last Name').click();
-    await page.getByPlaceholder('Last Name').fill('Demo');
-    await page.getByPlaceholder('email@example.com').click();
-    await page.getByPlaceholder('email@example.com').fill(credentials.email);
-    await page.getByPlaceholder('Password', { exact: true }).click();
-    await page.getByPlaceholder('Password', { exact: true }).fill(credentials.password);
-    await page.getByPlaceholder('Confirm Password').click();
-    await page.getByPlaceholder('Confirm Password').fill(credentials.password);
-    await page.locator('#main form div').filter({ hasText: 'Subscribe to newsletter' }).locator('label').first().click();
-    await page.getByRole('button', { name: 'Register' }).click();
-
-    await expect(page.getByText('Account created successfully, an e-mail has been sent for verification.').first()).toBeVisible();
-
-    return credentials;
-}
+import { register } from '../utils/customer';
 
 test('register', async ({ page }) => {
     await register(page);
@@ -58,12 +32,12 @@ test('logout', async ({ page }) => {
     await page.getByPlaceholder('Password').click();
     await page.getByPlaceholder('Password').fill(credentials.password);
     await page.getByRole('button', { name: 'Sign In' }).click();
-    await page.waitForTimeout(5000);
 
+    await page.getByLabel('Profile').waitFor({ state: 'visible' });
     await page.getByLabel('Profile').click();
     await page.getByRole('link', { name: 'Logout' }).click();
-    await page.waitForTimeout(5000);
 
+    await page.getByLabel('Profile').waitFor({ state: 'visible' });
     await page.getByLabel('Profile').click();
     await expect(page.getByText('Welcome Guest').first()).toBeVisible();
 });
--- packages/Webkul/Shop/tests/e2e-pw/tests/wishlist.spec.ts
+++ packages/Webkul/Shop/tests/e2e-pw/tests/wishlist.spec.ts
@@ -1,31 +1,26 @@
-import { test, expect, config } from '../setup';
+import { test, expect } from "../setup";
+import { addWishlist, loginAsCustomer } from "../utils/customer";
 
-test('Add To Wishlist', async ({page}) => {
-    await page.goto(`${config.baseUrl}`);
-    await page.getByLabel('Profile').click();
-    await page.getByRole('link', { name: 'Sign In' }).click();
-    await page.getByPlaceholder('email@example.com').click();
-    await page.getByPlaceholder('email@example.com').fill('testUser@gmail.com');
-    await page.getByPlaceholder('email@example.com').press('Tab');
-    await page.getByPlaceholder('Password').fill('testUser@123');
-    await page.getByRole('button', { name: 'Sign In' }).click();
-    await page.locator('.action-items > span').first().click();
-    await page.locator('div:nth-child(9) > div:nth-child(2) > div > .-mt-9 > .action-items > span').first().click();
+test("should add wishlist to the cart", async ({ page }) => {
+    await loginAsCustomer(page);
 
-    await expect(page.getByText('Item Successfully Added To Wishlist').first()).toBeVisible();
+    await addWishlist(page);
 });
 
-test('Remove from Wishlist', async ({page}) => {
-    await page.goto(`${config.baseUrl}`);
-    await page.getByLabel('Profile').click();
-    await page.getByRole('link', { name: 'Sign In' }).click();
-    await page.getByPlaceholder('email@example.com').click();
-    await page.getByPlaceholder('email@example.com').fill('testUser@gmail.com');
-    await page.getByPlaceholder('email@example.com').press('Tab');
-    await page.getByPlaceholder('Password').fill('testUser@123');
-    await page.getByRole('button', { name: 'Sign In' }).click();
-    await page.locator('.action-items > span').first().click();
-    await page.locator('div:nth-child(9) > div:nth-child(2) > div > .-mt-9 > .action-items > span').first().click();
+test("should remove wishlist from the cart", async ({ page }) => {
+    await loginAsCustomer(page);
 
-    await expect(page.getByText('Item Successfully Removed From Wishlist').first()).toBeVisible();
+    await addWishlist(page);
+
+    await page.locator(".action-items > span").first().click();
+    await page
+        .locator(
+            "div:nth-child(9) > div:nth-child(2) > div > .-mt-9 > .action-items > span"
+        )
+        .first()
+        .click();
+
+    await expect(
+        page.getByText("Item Successfully Removed From Wishlist").first()
+    ).toBeVisible();
 });
