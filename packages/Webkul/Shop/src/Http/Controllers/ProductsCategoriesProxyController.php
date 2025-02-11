7e5507786b48440cd46743258631e515d9544f3a
amit-webkul <121479029+amit-webkul@users.noreply.github.com>
2025-02-11 14:04:34 +0000

    chore: applied pint changes

--- packages/Webkul/Shop/src/Http/Controllers/ProductsCategoriesProxyController.php
+++ packages/Webkul/Shop/src/Http/Controllers/ProductsCategoriesProxyController.php
@@ -70,7 +70,7 @@ class ProductsCategoriesProxyController extends Controller
         if (core()->getConfigData('catalog.products.search.engine') == 'elastic') {
             $searchEngine = core()->getConfigData('catalog.products.search.storefront_mode');
         }
-            
+
         $product = $this->productRepository
             ->setSearchEngine($searchEngine ?? 'database')
             ->findBySlug($slugOrURLKey);
