631c206d851c7ee1d5a7b448ee75634005d9bdec
Devansh Bawari <devansh.bawari419@webkul.in>
2025-03-11 12:36:10 +0530

    test: fixed open ai configuration message

--- packages/Webkul/Admin/tests/e2e-pw/tests/configuration/general/magic-ai.spec.ts
+++ packages/Webkul/Admin/tests/e2e-pw/tests/configuration/general/magic-ai.spec.ts
@@ -1,114 +1,212 @@
-import { test, expect } from '../../../setup';
-import { generateRandomStringWithSpaces, generateRandomUrl } from '../../../utils/form';
-import { generateName, generateDescription } from '../../../utils/faker';
+import { test, expect } from "../../../setup";
+import {
+    generateRandomStringWithSpaces,
+    generateRandomUrl,
+} from "../../../utils/form";
+import { generateName, generateDescription } from "../../../utils/faker";
 
-
-test.describe('Magic AI Configuration', () => {
-    /**
-     * Navigate to the configuration page.
-     */
+test.describe("magic ai configuration", () => {
     test.beforeEach(async ({ adminPage }) => {
-        await adminPage.goto('admin/configuration/general/magic_ai');
+        await adminPage.goto("admin/configuration/general/magic_ai");
     });
 
-    /**
-     * Update the Open AI General Settings.
-     */
-    test('should update openAI credential', async ({ adminPage }) => {
-        await adminPage.click('label[for="general[magic_ai][settings][enabled]"]');
-        // const toggleInput = await adminPage.locator('input[name="general[magic_ai][settings][enabled]"]');
-        
-        // Ensure the state is correctly toggled
-        // await expect(toggleInput).toBeChecked();
+    test("should update the openai credential", async ({ adminPage }) => {
+        const isChecked = await adminPage
+            .locator(
+                'input[type="checkbox"][name="general[magic_ai][settings][enabled]"]'
+            )
+            .isChecked();
 
-        await adminPage.locator('input[name="general[magic_ai][settings][api_key]"]').fill(generateRandomStringWithSpaces(100));
-        await adminPage.locator('input[name="general[magic_ai][settings][organization]"]').fill(generateName());
-        await adminPage.locator('input[name="general[magic_ai][settings][api_domain]"]').fill(generateRandomUrl());
+        if (!isChecked) {
+            await adminPage.click(
+                'label[for="general[magic_ai][settings][enabled]"]'
+            );
+        }
+
+        /**
+         * Fill the Open AI credentials.
+         */
+        await adminPage
+            .locator('input[name="general[magic_ai][settings][api_key]"]')
+            .fill(generateRandomStringWithSpaces(100));
+        await adminPage
+            .locator('input[name="general[magic_ai][settings][organization]"]')
+            .fill(generateName());
+        await adminPage
+            .locator('input[name="general[magic_ai][settings][api_domain]"]')
+            .fill(generateRandomUrl());
+
+        /**
+         * Save the configuration.
+         */
         await adminPage.click('button[type="submit"].primary-button:visible');
 
         /**
          * Verify the change is saved.
          */
-        await expect(adminPage.getByText('Configuration saved successfully')).toBeVisible();
+        await expect(
+            adminPage.getByText("Configuration saved successfully")
+        ).toBeVisible();
+
+        await expect(
+            await adminPage.locator(
+                'input[type="checkbox"][name="general[magic_ai][settings][enabled]"]'
+            )
+        ).toBeChecked();
     });
 
-    /**
-     * Update the Open AI Content Generation.
-     */
-    test('should mange content using AI', async ({ adminPage }) => {
-        await adminPage.click('label[for="general[magic_ai][content_generation][enabled]"]');
-        // const toggleInput = await adminPage.locator('input[name="general[magic_ai][content_generation][enabled]"]');
+    test("should manage content using ai", async ({ adminPage }) => {
+        const isChecked = await adminPage
+            .locator(
+                'input[type="checkbox"][name="general[magic_ai][content_generation][enabled]"]'
+            )
+            .isChecked();
 
-        // Ensure the state is correctly toggled
-        // await expect(toggleInput).toBeChecked();
+        if (!isChecked) {
+            await adminPage.click(
+                'label[for="general[magic_ai][content_generation][enabled]"]'
+            );
+        }
 
-        await adminPage.locator('textarea[name="general[magic_ai][content_generation][product_short_description_prompt]"]').fill(generateDescription(100));
-        await adminPage.locator('textarea[name="general[magic_ai][content_generation][product_description_prompt]"]').fill(generateDescription(100));
-        await adminPage.locator('textarea[name="general[magic_ai][content_generation][category_description_prompt]"]').fill(generateDescription(100));
-        await adminPage.locator('textarea[name="general[magic_ai][content_generation][cms_page_content_prompt]"]').fill(generateDescription(100));
+        await adminPage
+            .locator(
+                'textarea[name="general[magic_ai][content_generation][product_short_description_prompt]"]'
+            )
+            .fill(generateDescription(100));
+        await adminPage
+            .locator(
+                'textarea[name="general[magic_ai][content_generation][product_description_prompt]"]'
+            )
+            .fill(generateDescription(100));
+        await adminPage
+            .locator(
+                'textarea[name="general[magic_ai][content_generation][category_description_prompt]"]'
+            )
+            .fill(generateDescription(100));
+        await adminPage
+            .locator(
+                'textarea[name="general[magic_ai][content_generation][cms_page_content_prompt]"]'
+            )
+            .fill(generateDescription(100));
+
         await adminPage.click('button[type="submit"].primary-button:visible');
 
         /**
          * Verify the change is saved.
          */
-        await expect(adminPage.getByText('Configuration saved successfully')).toBeVisible();
+        await expect(
+            adminPage.getByText("Configuration saved successfully")
+        ).toBeVisible();
     });
 
-    /**
-     * Update the Open AI Image Generation.
-     */
-    test('should enable image generation using AI', async ({ adminPage }) => {
-        await adminPage.click('label[for="general[magic_ai][image_generation][enabled]"]');
-        // const toggleInput = await adminPage.locator('input[name="general[magic_ai][image_generation][enabled]"]');
+    test("should enable image generation using ai", async ({ adminPage }) => {
+        const isChecked = await adminPage
+            .locator(
+                'input[type="checkbox"][name="general[magic_ai][image_generation][enabled]"]'
+            )
+            .isChecked();
 
-        // Ensure the state is correctly toggled
-        // await expect(toggleInput).toBeChecked();
+        if (!isChecked) {
+            await adminPage.click(
+                'label[for="general[magic_ai][image_generation][enabled]"]'
+            );
+        }
 
         await adminPage.click('button[type="submit"].primary-button:visible');
 
         /**
          * Verify the change is saved.
          */
-        await expect(adminPage.getByText('Configuration saved successfully')).toBeVisible();
+        await expect(
+            adminPage.getByText("Configuration saved successfully")
+        ).toBeVisible();
     });
 
-    /**
-     * Update the Open AI Review Translation.
-     */
-    test('should enable review translation using AI', async ({ adminPage }) => {
-        await adminPage.click('label[for="general[magic_ai][review_translation][enabled]"]');
-        // const toggleInput = await adminPage.locator('input[name="general[magic_ai][review_translation][enabled]"]');
+    test("should enable review translation using ai", async ({ adminPage }) => {
+        const isChecked = await adminPage
+            .locator(
+                'input[type="checkbox"][name="general[magic_ai][review_translation][enabled]"]'
+            )
+            .isChecked();
 
-        // Ensure the state is correctly toggled
-        // await expect(toggleInput).toBeChecked();
-        await adminPage.selectOption('select[name="general[magic_ai][review_translation][model]"]', 'llama2');
+        if (!isChecked) {
+            await adminPage.click(
+                'label[for="general[magic_ai][review_translation][enabled]"]'
+            );
+        }
+
+        /**
+         * Selecting the model.
+         */
+        await adminPage.selectOption(
+            'select[name="general[magic_ai][review_translation][model]"]',
+            "gemini-2.0-flash"
+        );
+
+        /**
+         * Saving configuration.
+         */
         await adminPage.click('button[type="submit"].primary-button:visible');
 
         /**
          * Verify the change is saved.
          */
-        await expect(adminPage.getByText('Configuration saved successfully')).toBeVisible();
+        await expect(
+            adminPage.getByText("Configuration saved successfully")
+        ).toBeVisible();
+
+        await expect(
+            await adminPage.locator(
+                'input[type="checkbox"][name="general[magic_ai][review_translation][enabled]"]'
+            )
+        ).toBeChecked();
     });
 
-    /**
-     * Update the Open AI Review Translation.
-     */
-    test('should craft a personalized checkout message for customers usin AI', async ({ adminPage }) => {
-        await adminPage.click('label[for="general[magic_ai][checkout_message][enabled]"]');
-        // const toggleInput = await adminPage.locator('input[name="general[magic_ai][review_translation][enabled]"]');
+    test("should craft a personalized checkout message for customers using AI", async ({
+        adminPage,
+    }) => {
+        const isChecked = await adminPage
+            .locator(
+                'input[type="checkbox"][name="general[magic_ai][checkout_message][enabled]"]'
+            )
+            .isChecked();
 
-        // Ensure the state is correctly toggled
-        // await expect(toggleInput).toBeChecked();
-        await adminPage.selectOption('select[name="general[magic_ai][checkout_message][model]"]', 'llama2');
-        const searchEngine = adminPage.locator('select[name="general[magic_ai][checkout_message][model]"]');
-        await expect(searchEngine).toHaveValue('llama2');
+        if (!isChecked) {
+            await adminPage.click(
+                'label[for="general[magic_ai][checkout_message][enabled]"]'
+            );
+        }
 
-        await adminPage.locator('textarea[name="general[magic_ai][checkout_message][prompt]"]').fill(generateDescription(100));
+        /**
+         * Fill the form.
+         */
+        await adminPage.selectOption(
+            'select[name="general[magic_ai][checkout_message][model]"]',
+            "gemini-2.0-flash"
+        );
+
+        await adminPage
+            .locator(
+                'textarea[name="general[magic_ai][checkout_message][prompt]"]'
+            )
+            .fill(generateDescription(100));
+
+        /**
+         * Save the configuration.
+         */
         await adminPage.click('button[type="submit"].primary-button:visible');
 
         /**
          * Verify the change is saved.
          */
-        await expect(adminPage.getByText('Configuration saved successfully')).toBeVisible();
+        await expect(
+            adminPage.getByText("Configuration saved successfully")
+        ).toBeVisible();
+
+        await expect(
+            adminPage.locator(
+                'select[name="general[magic_ai][checkout_message][model]"]'
+            )
+        ).toHaveValue("gemini-2.0-flash");
     });
 });
