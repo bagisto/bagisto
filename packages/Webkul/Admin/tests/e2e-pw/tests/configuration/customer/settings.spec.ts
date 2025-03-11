69f1493527c73eeefaf5a5b142ac9a0257fbed2c
Devansh Bawari <devansh.bawari419@webkul.in>
2025-03-11 18:53:58 +0530

    test: fixed gdpr testcases

--- packages/Webkul/Admin/tests/e2e-pw/tests/configuration/customer/settings.spec.ts
+++ packages/Webkul/Admin/tests/e2e-pw/tests/configuration/customer/settings.spec.ts
@@ -1,23 +1,17 @@
 import { test, expect } from "../../../setup";
-import { generateRandomStringWithSpaces } from "../../../utils/form";
 
-test.describe("Settings Configuration", () => {
-    /**
-     * Navigate to the configuration page.
-     */
+test.describe("settings configuration", () => {
     test.beforeEach(async ({ adminPage }) => {
+        /**
+         * Navigate to the configuration page.
+         */
         await adminPage.goto("admin/configuration/customer/settings");
     });
 
-    /**
-     * Update the Wishlist Configuration.
-     */
     test("should enable the wishlist feature", async ({ adminPage }) => {
         await adminPage.click(
             'label[for="customer[settings][wishlist][wishlist_option]"]'
         );
-        // const toggleInput = await adminPage.locator('input[name="customer[settings][wishlist][wishlist_option]"]');
-        // await expect(toggleInput).toBeChecked();
 
         await adminPage.click('button[type="submit"].primary-button:visible');
 
@@ -29,9 +23,6 @@ test.describe("Settings Configuration", () => {
         ).toBeVisible();
     });
 
-    /**
-     * Update Redirect Page Option Configuration.
-     */
     test("should update the redirect page option after the login", async ({
         adminPage,
     }) => {
@@ -54,9 +45,6 @@ test.describe("Settings Configuration", () => {
         ).toBeVisible();
     });
 
-    /**
-     * Update Default Customer Group and Newsletter Configuration During Signup.
-     */
     test("should update default customer group and enabling the newsletter subscription option during sign-up", async ({
         adminPage,
     }) => {
@@ -72,8 +60,6 @@ test.describe("Settings Configuration", () => {
         await adminPage.click(
             'label[for="customer[settings][create_new_account_options][news_letter]"]'
         );
-        // const toggleInput = await adminPage.locator('input[name="customer[settings][create_new_account_options][news_letter]"]');
-        // await expect(toggleInput).toBeChecked();
 
         await adminPage.click('button[type="submit"].primary-button:visible');
 
@@ -85,9 +71,6 @@ test.describe("Settings Configuration", () => {
         ).toBeVisible();
     });
 
-    /**
-     * Update Newsletter Configuration for Footer Section.
-     */
     test("should update the newsletter subscription option", async ({
         adminPage,
     }) => {
@@ -119,39 +102,26 @@ test.describe("Settings Configuration", () => {
         ).toBeChecked();
     });
 
-    /**
-     * Update Newsletter Configuration for Footer Section.
-     */
     test("should enable the social login option", async ({ adminPage }) => {
         await adminPage.click(
             'label[for="customer[settings][social_login][enable_facebook]"]'
         );
-        // const toggleInput = await adminPage.locator('input[name="customer[settings][social_login][enable_facebook]"]');
-        // await expect(toggleInput).toBeChecked();
 
         await adminPage.click(
             'label[for="customer[settings][social_login][enable_twitter]"]'
         );
-        // const toggleInput = await adminPage.locator('input[name="customer[settings][social_login][enable_twitter]"]');
-        // await expect(toggleInput).toBeChecked();
 
         await adminPage.click(
             'label[for="customer[settings][social_login][enable_google]"]'
         );
-        // const toggleInput = await adminPage.locator('input[name="customer[settings][social_login][enable_google]"]');
-        // await expect(toggleInput).toBeChecked();
 
         await adminPage.click(
             'label[for="customer[settings][social_login][enable_linkedin-openid]"]'
         );
-        // const toggleInput = await adminPage.locator('input[name="customer[settings][social_login][enable_linkedin-openid]"]');
-        // await expect(toggleInput).toBeChecked();
 
         await adminPage.click(
             'label[for="customer[settings][social_login][enable_github]"]'
         );
-        // const toggleInput = await adminPage.locator('input[name="customer[settings][social_login][enable_github]"]');
-        // await expect(toggleInput).toBeChecked();
 
         await adminPage.click('button[type="submit"].primary-button:visible');
 
