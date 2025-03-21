c5e05ae120aac79b9245c424d34dfc71b4652f98
Shivendra <shivendra.gupta423@webkul.in>
2025-03-21 15:22:17 +0530

    Update GDPR migration syntax

--- packages/Webkul/GDPR/src/Database/Migrations/2020_08_07_174804_create_gdpr_data_request_table.php
+++ packages/Webkul/GDPR/src/Database/Migrations/2020_08_07_174804_create_gdpr_data_request_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateGdprDataRequestTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
