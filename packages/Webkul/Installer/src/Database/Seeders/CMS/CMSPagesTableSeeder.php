bacd63c78a3305851082a94d890a653efab4b54e
Shivendra <shivendra.gupta423@webkul.in>
2025-04-11 17:48:46 +0530

    remove space

--- packages/Webkul/Installer/src/Database/Seeders/CMS/CMSPagesTableSeeder.php
+++ packages/Webkul/Installer/src/Database/Seeders/CMS/CMSPagesTableSeeder.php
@@ -165,15 +165,12 @@ class CMSPagesTableSeeder extends Seeder
         }
 
         $cmsPageIds = DB::table('cms_pages')->pluck('id');
-        
+
         foreach ($cmsPageIds as $pageId) {
             DB::table('cms_page_channels')->insert([
                 'cms_page_id' => $pageId,
                 'channel_id'  => 1,
             ]);
         }
-        
-
-
     }
 }
