b56b9f56f13edabd9a285135eb2a9682f893ddb2
Glenn Hermans <glhermans@gmail.com>
2025-05-18 16:26:56 +0200

    update component to match admin ui

--- packages/Webkul/Admin/src/Resources/views/components/tabs/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/tabs/index.blade.php
@@ -4,23 +4,23 @@
     position="{{ $position }}"
     {{ $attributes }}
 >
-    <x-admin::shimmer.tabs />
+
 </v-tabs>
 
 @pushOnce('scripts')
-    <script 
+    <script
         type="text/x-template"
         id="v-tabs-template"
     >
         <div>
             <div
-                class="flex justify-center gap-4 bg-neutral-100 pt-2 max-sm:hidden"
+                class="flex justify-center gap-4 bg-white dark:border-gray-800 dark:bg-gray-900 dark:text-gray-400 pt-2 max-sm:hidden"
                 :style="positionStyles"
             >
                 <div
                     v-for="tab in tabs"
-                    class="cursor-pointer px-2.5 pb-3.5 text-base font-medium text-gray-300"
-                    :class="{'border-navyBlue border-b-2 text-black transition': tab.isActive }"
+                    class="cursor-pointer px-2.5 pb-3.5 text-base font-medium text-gray-400"
+                    :class="{'border-blue-600 border-b-2 text-blue-600 transition': tab.isActive }"
                     @click="change(tab)"
                 >
                     @{{ tab.title }}
