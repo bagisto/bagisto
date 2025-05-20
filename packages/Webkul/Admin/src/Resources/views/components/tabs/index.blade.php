7b47e3b2f2dd05b3d7b130074175b6d27fdf211a
Glenn Hermans <glhermans@gmail.com>
2025-05-20 08:55:48 +0200

    Changed text-gray-400 to 300

--- packages/Webkul/Admin/src/Resources/views/components/tabs/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/tabs/index.blade.php
@@ -14,12 +14,12 @@
     >
         <div>
             <div
-                class="flex justify-center gap-4 bg-white dark:border-gray-800 dark:bg-gray-900 dark:text-gray-400 pt-2 max-sm:hidden"
+                class="flex justify-center gap-4 bg-white dark:border-gray-800 dark:bg-gray-900 dark:text-gray-300 pt-2 max-sm:hidden"
                 :style="positionStyles"
             >
                 <div
                     v-for="tab in tabs"
-                    class="cursor-pointer px-2.5 pb-3.5 text-base font-medium text-gray-400"
+                    class="cursor-pointer px-2.5 pb-3.5 text-base font-medium text-gray-300"
                     :class="{'border-blue-600 border-b-2 text-blue-600 transition': tab.isActive }"
                     @click="change(tab)"
                 >
