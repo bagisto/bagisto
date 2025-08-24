2a2b7278111ed2106b26f8273c5c622be9057b68
Glenn Hermans <glhermans@gmail.com>
2025-08-24 23:33:27 +0200

    fixed typo & rebuild assets

--- packages/Webkul/Shop/tailwind.config.js
+++ packages/Webkul/Shop/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["../../../resources/views/**/*.blade.php", "./src/Resources/**/*.blade.php", "./src/Resources/**/*.js"],
+    content: ["../../../resources/themes/**/*.blade.php", "./src/Resources/**/*.blade.php", "./src/Resources/**/*.js"],
 
     theme: {
         container: {
