ad498d65c19d9b59f67708d9ed150d0723592cee
chethan7032 <chethanreddy118@gmail.com>
2025-09-04 12:10:41 +0530

    chore: apply pint formatting

--- packages/Webkul/Admin/src/Resources/lang/ca/app.php
+++ packages/Webkul/Admin/src/Resources/lang/ca/app.php
@@ -1298,7 +1298,7 @@ return [
                         'validations' => [
                             'type-mismatch'      => 'El tipus de reserva no es pot canviar.',
                             'time-validation'    => "L'hora d'inici ha de ser menor que l'hora de finalització.",
-                            'overlap-validation' => "La franja horària se solapa amb una franja existent.",
+                            'overlap-validation' => 'La franja horària se solapa amb una franja existent.',
                         ],
                     ],
 
