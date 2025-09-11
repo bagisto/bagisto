9b6b9dcb49e09c3cb024ef15478ea2d1913a5620
ashif laravel <ashif.322@webkul.in>
2025-09-11 19:12:40 +0530

    Update system.php

--- packages/Webkul/Admin/src/Config/system.php
+++ packages/Webkul/Admin/src/Config/system.php
@@ -541,7 +541,32 @@ return [
                 'channel_based' => true,
             ],
         ],
+    ],
+
+    [
+        'key'  => 'general.two_factor_auth',
+        'name' => 'two_factor_auth::app.configuration.index.general.two_factor_auth.title',
+        'info' => 'two_factor_auth::app.configuration.index.general.two_factor_auth.info',
+        'icon' => 'settings/store.svg',
+        'sort' => 5,
     ], [
+        'key'    => 'general.two_factor_auth.settings',
+        'name'   => 'two_factor_auth::app.configuration.index.general.two_factor_auth.settings.title',
+        'info'   => 'two_factor_auth::app.configuration.index.general.two_factor_auth.settings.info',
+        'sort'   => 1,
+        'fields' => [
+            [
+                'name'          => 'enabled',
+                'title'         => 'two_factor_auth::app.configuration.index.general.two_factor_auth.settings.enabled',
+                'type'          => 'boolean',
+                'channel_based' => true,
+                'locale_based'  => true,
+                'default'       => false,
+            ],
+        ],
+    ],
+    
+    [
         'key'  => 'general.gdpr',
         'name' => 'admin::app.configuration.index.general.gdpr.title',
         'info' => 'admin::app.configuration.index.general.gdpr.info',
