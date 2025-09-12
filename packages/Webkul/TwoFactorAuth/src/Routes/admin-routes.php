a8d294f9528d2314bf6f0d159c77a09b88cfec9b
ashif laravel <ashif.322@webkul.in>
2025-09-12 18:19:11 +0530

    Update Admin routes

--- packages/Webkul/TwoFactorAuth/src/Routes/admin-routes.php
+++ packages/Webkul/TwoFactorAuth/src/Routes/admin-routes.php
@@ -1,14 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Webkul\Admin\Http\Controllers\User\SessionController;
 use Webkul\TwoFactorAuth\Http\Controllers\Admin\TwoFactorController;
 
 Route::group(['middleware' => ['web', 'admin', 'admin.2fa']], function () {
-    Route::get('/admin/two-factor/setup', [TwoFactorController::class, 'setup'])->name('admin.twofactor.setup');
 
-    Route::post('/admin/two-factor/enable', [TwoFactorController::class, 'enable'])->name('admin.twofactor.enable');
+    Route::group(['prefix' => 'admin/two-factor'], function () {
+    
+        Route::get('/setup', [TwoFactorController::class, 'setup'])->name('admin.twofactor.setup');
+        
+        Route::post('/enable', [TwoFactorController::class, 'enable'])->name('admin.twofactor.enable');
+        
+        Route::get('/remove', [TwoFactorController::class, 'remove'])->name('admin.twofactor.configuration.remove');
+        
+        Route::get('/back', [TwoFactorController::class, 'back'])->name('admin.twofactor.configuration.back');
+        
+        Route::get('/verify', [TwoFactorController::class, 'showVerifyForm'])->name('admin.twofactor.verify.form');
+        
+        Route::post('/verify', [TwoFactorController::class, 'verifyTwoFactorCode'])->name('admin.twofactor.verifyTwoFactorCode');
+    });
 
-    Route::get('/admin/two-factor/verify', [TwoFactorController::class, 'showVerifyForm'])->name('admin.twofactor.verify.form');
+    Route::get('/admin/logout', [SessionController::class, 'destroy'])->name('admin.twofactor.session.destroy');
 
-    Route::post('/admin/two-factor/verify', [TwoFactorController::class, 'verify'])->name('admin.twofactor.verify');
 });
