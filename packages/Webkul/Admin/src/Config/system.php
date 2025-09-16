43631f79368f2ea72f11364ae7279a93a56a1865
ashif laravel <ashif.322@webkul.in>
2025-09-16 18:21:03 +0530

    Refactor TwoFactorAuth : System Config

--- packages/Webkul/Admin/src/Config/system.php
+++ packages/Webkul/Admin/src/Config/system.php
@@ -696,27 +696,6 @@ return [
         ],
     ],
 
-    [
-        'key'  => 'general.two_factor_auth',
-        'name' => 'two_factor_auth::app.configuration.index.general.two_factor_auth.title',
-        'info' => 'two_factor_auth::app.configuration.index.general.two_factor_auth.info',
-        'icon' => 'settings/store.svg',
-        'sort' => 5,
-    ], [
-        'key'    => 'general.two_factor_auth.settings',
-        'name'   => 'two_factor_auth::app.configuration.index.general.two_factor_auth.settings.title',
-        'info'   => 'two_factor_auth::app.configuration.index.general.two_factor_auth.settings.info',
-        'sort'   => 1,
-        'fields' => [
-            [
-                'name'          => 'two_factor_auth_enable',
-                'title'         => 'admin::app.configuration.index.general.gdpr.agreement.checkbox-label',
-                'type'          => 'blade',
-                'path'          => 'two_factor_auth::admin.setup',
-            ],
-        ],
-    ],
-
     /**
      * Catalog.
      */
