e85359f8bb6e31d8c24147885ea4fca443f41030
dheeraj.qa322 <dheeraj.qa322@webkul.in>
2025-11-13 10:34:20 +0530

    correction shop/review testscript

--- packages/Webkul/Shop/tests/e2e-pw/tests/review.spec.ts
+++ packages/Webkul/Shop/tests/e2e-pw/tests/review.spec.ts
@@ -11,17 +11,13 @@ test("should review a product", async ({ page }) => {
         .getByLabel("Arctic Touchscreen Winter")
         .click();
     await page.getByRole("button", { name: "Reviews" }).click();
-    await page.waitForSelector("#review-tab");
-    await page.locator("#review-tab").getByText("Write a Review").click();
-    await page.locator("#review-tab span").nth(3).click();
-    await page.locator("#review-tab span").nth(4).click();
+    await page.waitForSelector("#review-tab-button");
+    await page.getByText('Write a Review').click();
     await page.getByPlaceholder("Title").click();
     await page.getByPlaceholder("Title").fill(generateName());
     await page.getByPlaceholder("Comment").click();
     await page.getByPlaceholder("Comment").fill(generateDescription());
     await page.getByRole("button", { name: "Submit Review" }).click();
 
-    await expect(
-        page.getByText("Review submitted successfully.").first()
-    ).toBeVisible();
+    await expect(page.getByRole('paragraph').filter({ hasText: 'Review submitted successfully.' })).toBeVisible();
 });
