5c759cccb73dc245f73cd2be409ece421bf4344d
Devansh Bawari <devansh.bawari419@webkul.in>
2025-12-24 12:02:16 +0000

    chore: changelog added and some changes done

--- packages/Webkul/Product/src/Exceptions/InsufficientProductInventoryException.php
+++ packages/Webkul/Product/src/Exceptions/InsufficientProductInventoryException.php
@@ -4,13 +4,4 @@ namespace Webkul\Product\Exceptions;
 
 use Exception;
 
-/**
- * Class InsufficientProductInventoryException
- *
- * Thrown when a product does not have sufficient inventory
- * to fulfill the requested quantity during cart or order operations.
- *
- * This exception is typically raised while validating product
- * availability before adding items to the cart or placing an order.
- */
 class InsufficientProductInventoryException extends Exception {}
