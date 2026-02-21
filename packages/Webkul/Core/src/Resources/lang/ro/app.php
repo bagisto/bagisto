216e57f60a29859b245d852414d49cc2a09c57f1
Bogdan-Adrian Burciu <bogdanadrian.burciu@yahoo.com>
2026-02-21 14:11:04 +0200

    feat: Add support for Romanian language
    Signed-off-by: Bogdan-Adrian Burciu <bogdanadrian.burciu@yahoo.com>

---
+++ packages/Webkul/Core/src/Resources/lang/ro/app.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'path-hint' => [
+        'template' => 'Șablon',
+        'parents' => 'Părinți',
+    ],
+
+    'currency-position' => [
+        'options' => [
+            'left' => 'Stânga',
+            'left-with-space' => 'Stânga cu spațiu',
+            'right' => 'Dreapta',
+            'right-with-space' => 'Dreapta cu spațiu',
+        ],
+    ],
+];
