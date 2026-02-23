94cb7632d7c95a4abfd859f0be55ad8a8db67f70
Aviral Updahyay <aviral.pba322@webkul.in>
2026-02-23 19:02:53 +0530

    Added all cart rule condition automation script

---
+++ packages/Webkul/Shop/tests/e2e-pw/tests/promotion/product-attributes/color.spec.ts
@@ -0,0 +1,82 @@
+import { expect, test } from "../../../setup";
+import { ProductCreation } from "../../../pages/product";
+import { CreateRules } from "../../../pages/rules";
+
+test.beforeEach("should create simple product", async ({ adminPage }) => {
+    const productCreation = new ProductCreation(adminPage);
+
+    await productCreation.createProduct({
+        type: "simple",
+        sku: `SKU-${Date.now()}`,
+        name: `Simple-${Date.now()}`,
+        shortDescription: "Short desc",
+        description: "Full desc",
+        price: 199,
+        weight: 1,
+        inventory: 100,
+    });
+});
+
+test.describe("cart rules", () => {
+    test.describe("product attribute conditions", () => {
+        test("should apply coupon when color condition is -> is equal to", async ({
+            page,
+        }) => {
+            const createRules = new CreateRules(page);
+            await createRules.adminlogin();
+            await createRules.cartRuleCreationFlow();
+            await createRules.addCondition({
+                attribute: "product|color",
+                operator: "==",
+                optionSelect: "1",
+            });
+            await createRules.saveCartRule();
+            await page.goto("admin/catalog/products");
+            await page
+                .locator("span.cursor-pointer.icon-sort-right")
+                .nth(1)
+                .click();
+            await page.waitForLoadState("networkidle");
+            await page.locator('select[name="color"]').first().selectOption("1");
+            await page
+                .locator('button:has-text("Save Product")')
+                .first()
+                .click();
+            await expect(
+                page.getByText("Product updated successfully").first(),
+            ).toBeVisible();
+            await createRules.applyCoupon();
+            await createRules.deleteRuleAndProduct();
+        });
+
+        test("should apply coupon when color condition is -> is not equal to", async ({
+            page,
+        }) => {
+            const createRules = new CreateRules(page);
+            await createRules.adminlogin();
+            await createRules.cartRuleCreationFlow();
+            await createRules.addCondition({
+                attribute: "product|color",
+                operator: "!=",
+                optionSelect: "1",
+            });
+            await createRules.saveCartRule();
+            await page.goto("admin/catalog/products");
+            await page
+                .locator("span.cursor-pointer.icon-sort-right")
+                .nth(1)
+                .click();
+            await page.waitForLoadState("networkidle");
+            await page.locator('select[name="color"]').first().selectOption("2");
+            await page
+                .locator('button:has-text("Save Product")')
+                .first()
+                .click();
+            await expect(
+                page.getByText("Product updated successfully").first(),
+            ).toBeVisible();
+            await createRules.applyCoupon();
+            await createRules.deleteRuleAndProduct();
+        });
+    });
+});
---
+++ packages/Webkul/Shop/tests/e2e-pw/tests/promotion/product-attributes/price.spec.ts
@@ -0,0 +1,118 @@
+import { test } from "../../../setup";
+import { ProductCreation } from "../../../pages/product";
+import { CreateRules } from "../../../pages/rules";
+
+test.beforeEach("should create simple product", async ({ adminPage }) => {
+    const productCreation = new ProductCreation(adminPage);
+
+    await productCreation.createProduct({
+        type: "simple",
+        sku: `SKU-${Date.now()}`,
+        name: `Simple-${Date.now()}`,
+        shortDescription: "Short desc",
+        description: "Full desc",
+        price: 199,
+        weight: 1,
+        inventory: 100,
+    });
+});
+
+test.describe("cart rules", () => {
+    test.describe("product attribute conditions", () => {
+        test("should apply coupon when price condition is -> is equal to", async ({
+            page,
+        }) => {
+            const createRules = new CreateRules(page);
+            await createRules.adminlogin();
+            await createRules.cartRuleCreationFlow();
+            await createRules.addCondition({
+                attribute: "product|price",
+                operator: "==",
+                value: "199",
+            });
+            await createRules.saveCartRule();
+            await createRules.applyCouponAtCheckout();
+            await createRules.deleteRuleAndProduct();
+        });
+
+        test("should apply coupon when price condition is -> is not equal to", async ({
+            page,
+        }) => {
+            const createRules = new CreateRules(page);
+            await createRules.adminlogin();
+            await createRules.cartRuleCreationFlow();
+            await createRules.addCondition({
+                attribute: "product|price",
+                operator: "!=",
+                value: "100",
+            });
+            await createRules.saveCartRule();
+            await createRules.applyCouponAtCheckout();
+            await createRules.deleteRuleAndProduct();
+        });
+
+        test("should apply coupon when price condition is -> equals or greater then", async ({
+            page,
+        }) => {
+            const createRules = new CreateRules(page);
+            await createRules.adminlogin();
+            await createRules.cartRuleCreationFlow();
+            await createRules.addCondition({
+                attribute: "product|price",
+                operator: ">=",
+                value: "199",
+            });
+            await createRules.saveCartRule();
+            await createRules.applyCoupon();
+            await createRules.deleteRuleAndProduct();
+        });
+
+        test("should apply coupon when price condition is -> equals or less than", async ({
+            page,
+        }) => {
+            const createRules = new CreateRules(page);
+            await createRules.adminlogin();
+            await createRules.cartRuleCreationFlow();
+            await createRules.addCondition({
+                attribute: "product|price",
+                operator: "<=",
+                value: "200",
+            });
+            await createRules.saveCartRule();
+            await createRules.applyCoupon();
+            await createRules.deleteRuleAndProduct();
+        });
+
+        test("should apply coupon when price condition is -> greater than", async ({
+            page,
+        }) => {
+            const createRules = new CreateRules(page);
+            await createRules.adminlogin();
+            await createRules.cartRuleCreationFlow();
+            await createRules.addCondition({
+                attribute: "product|price",
+                operator: ">",
+                value: "198",
+            });
+            await createRules.saveCartRule();
+            await createRules.applyCoupon();
+            await createRules.deleteRuleAndProduct();
+        });
+
+        test("should apply coupon when price condition is -> less than", async ({
+            page,
+        }) => {
+            const createRules = new CreateRules(page);
+            await createRules.adminlogin();
+            await createRules.cartRuleCreationFlow();
+            await createRules.addCondition({
+                attribute: "product|price",
+                operator: "<",
+                value: "200",
+            });
+            await createRules.saveCartRule();
+            await createRules.applyCoupon();
+            await createRules.deleteRuleAndProduct();
+        });
+    });
+});
