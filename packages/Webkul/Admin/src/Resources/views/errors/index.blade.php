b430dcec8664e3e08b6452cb9d57526c3afb8351
Shivendra <shivendra.gupta423@webkul.in>
2026-03-09 16:48:53 +0530

    fixed issue #10250

--- packages/Webkul/Admin/src/Resources/views/errors/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/errors/index.blade.php
@@ -56,7 +56,7 @@
                 <p class="text-sm text-gray-800">
                     @lang('admin::app.errors.support', [
                         'link'  => 'mailto:support@example.com',
-                        'email' => 'support@example.com',
+                        'email' => core()->getAdminEmailDetails()['email'] ?? 'support@example.com',
                         'class' => 'font-semibold text-blue-600 transition-all hover:underline',
                     ])
                 </p>
