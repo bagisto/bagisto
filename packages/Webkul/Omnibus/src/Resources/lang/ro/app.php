cb77a5c82e83f2e72e49d05aba26eeac7fe567eb
sopson <biuro@msobczak.pl>
2026-04-16 13:56:18 +0200

    feat: add new lang

---
+++ packages/Webkul/Omnibus/src/Resources/lang/ro/app.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'admin' => [
+        'system' => [
+            'omnibus' => 'Omnibus',
+            'omnibus-info' => 'Configurarea directivei Omnibus',
+            'is-enabled' => 'Activați Omnibus',
+        ],
+    ],
+
+    'shop' => [
+        'price-info' => 'Cel mai mic preț în 30 de zile înainte de reducere:',
+    ],
+];
