e61f1481c87602037a8207a2b547d39b103d8a5a
Devansh Bawari <devansh.bawari419@webkul.in>
2026-06-23 14:26:14 +0000

    fix: style issue fixed for tables

--- config/purify.php
+++ config/purify.php
@@ -43,9 +43,9 @@ return [
         'default' => [
             'Core.Encoding' => 'utf-8',
             'HTML.Doctype' => 'HTML 4.01 Transitional',
-            'HTML.Allowed' => 'h1,h2,h3,h4,h5,h6,b,u,strong,i,em,s,del,a[href|title],ul,ol,li,p[style],br,span[style],img[width|height|alt|src],blockquote',
+            'HTML.Allowed' => 'h1[class|style],h2[class|style],h3[class|style],h4[class|style],h5[class|style],h6[class|style],b,u,strong,i,em,s,del,a[href|title|class|style],ul[class|style],ol[class|style],li[class|style],p[class|style],br,hr,span[class|style],img[width|height|alt|src|class|style],blockquote[class|style],div[class|style],pre[class|style],code[class|style],table[class|style|border|cellpadding|cellspacing|width|summary],thead[class|style],tbody[class|style],tfoot[class|style],tr[class|style],td[class|style|colspan|rowspan|width|height|align|valign],th[class|style|colspan|rowspan|width|height|align|valign|scope],caption[class|style],colgroup[span|width],col[span|width]',
             'HTML.ForbiddenElements' => '',
-            'CSS.AllowedProperties' => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align',
+            'CSS.AllowedProperties' => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding,padding-left,padding-right,padding-top,padding-bottom,margin,margin-left,margin-right,margin-top,margin-bottom,color,background-color,text-align,vertical-align,width,height,border,border-collapse,border-spacing,border-color,border-style,border-width,border-top,border-right,border-bottom,border-left',
             'AutoFormat.AutoParagraph' => false,
             'AutoFormat.RemoveEmpty' => false,
         ],
