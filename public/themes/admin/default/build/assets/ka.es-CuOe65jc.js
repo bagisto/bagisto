d1e8261d0c3b6abb61d3f82690c59065f88e742c
Abreu Data Systems <abreudatasystems@gmail.com>
2026-06-27 13:50:01 +0100

    implementeacao para inciair

---
+++ public/themes/admin/default/build/assets/ka.es-CuOe65jc.js
@@ -0,0 +1,5 @@
+/**
+  * vue-cal v4.10.2
+  * (c) 2025 Antoni Andre <antoniandre.web@gmail.com>
+  * @license MIT
+  */const t=["ორშაბათი","სამშაბათი","ოთხშაბათი","ხუთშაბათი","პარასკევი","შაბათი","კვირა"],n=["იანვარი","თებერვალი","მარტი","აპრილი","მაისი","ივნისი","ივლისი","აგვისტო","სექტემბერი","ოქტომბერი","ნოემბერი","დეკემბერი"],o="წლები",s="წელი",m="თვე",e="კვირა",M="დღე",c="დღეს",a="ღონისძიება არ არის",d="მთელი დღე",y="წაშლა",r="შექმენით ღონისძიება",l="dddd D MMMM YYYY",Y={weekDays:t,months:n,years:o,year:s,month:"თვე",week:e,day:"დღე",today:c,noEvent:a,allDay:d,deleteEvent:y,createEvent:r,dateFormat:l};export{d as allDay,r as createEvent,l as dateFormat,M as day,Y as default,y as deleteEvent,m as month,n as months,a as noEvent,c as today,e as week,t as weekDays,s as year,o as years};
---
+++ public/themes/admin/default/build/assets/pl.es-BQ3a0ltx.js
@@ -0,0 +1,5 @@
+/**
+  * vue-cal v4.10.2
+  * (c) 2025 Antoni Andre <antoniandre.web@gmail.com>
+  * @license MIT
+  */const e=["Poniedziałek","Wtorek","Środa","Czwartek","Piątek","Sobota","Niedziela"],t=["Styczeń","Luty","Marzec","Kwiecień","Maj","Czerwiec","Lipiec","Sierpień","Wrzesień","Październik","Listopad","Grudzień"],a="Lata",k="Rok",o="Miesiąc",i="Tydzień",n="Dzień",s="Dzisiaj",c="Brak wydarzeń",d="Cały dzień",r="Usuń",z="Utwórz wydarzenie",y="dddd, D MMMM YYYY",w={weekDays:e,months:t,years:a,year:"Rok",month:o,week:i,day:n,today:s,noEvent:c,allDay:d,deleteEvent:r,createEvent:z,dateFormat:y};export{d as allDay,z as createEvent,y as dateFormat,n as day,w as default,r as deleteEvent,o as month,t as months,c as noEvent,s as today,i as week,e as weekDays,k as year,a as years};
