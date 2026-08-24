0a99d123ebd61965ba9c2f115684cc77e582ba7d
Devansh Bawari <devansh.bawari419@webkul.in>
2026-08-24 13:38:53 +0530

    test: fixed testcases and installation testcase added

---
+++ packages/Webkul/Installer/tests/e2e-pw/playwright.config.ts
@@ -0,0 +1,56 @@
+import { defineConfig, devices } from "@playwright/test";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const TESTS_ROOT_PATH = __dirname;
+
+dotenv.config({ path: path.resolve(__dirname, "../../../../../.env") });
+
+export default defineConfig({
+    testDir: "./tests",
+
+    timeout: 300 * 1000,
+
+    expect: { timeout: 30 * 1000 },
+
+    outputDir: "./test-results",
+
+    fullyParallel: false,
+
+    workers: 1,
+
+    forbidOnly: !!process.env.CI,
+
+    retries: 0,
+
+    reportSlowTests: null,
+
+    reporter: [
+        ["list"],
+
+        [
+            "html",
+            {
+                outputFolder: "./playwright-report",
+            },
+        ],
+    ],
+
+    use: {
+        baseURL: `${process.env.APP_URL}/`.replace(/\/+$/, "/"),
+        screenshot: { mode: "only-on-failure", fullPage: true },
+        video: "retain-on-failure",
+        trace: "retain-on-failure",
+    },
+
+    projects: [
+        {
+            name: "chromium",
+            use: { ...devices["Desktop Chrome"] },
+        },
+    ],
+});
