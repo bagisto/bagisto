be33be930b4553ac14dcaf61e44b7ced8640e726
Mohammad Salehi <144056438+mohammadsaleehi@users.noreply.github.com>
2026-09-14 14:50:22 +0330

    fix: get mysql engine from env

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'prefix' => env('DB_PREFIX', ''),
             'prefix_indexes' => true,
             'strict' => false,
-            'engine' => null,
+            'engine' => env('DB_ENGINE'),
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
